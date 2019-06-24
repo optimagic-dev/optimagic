e145a3b645bb9555a4c25325e92b051bba673fdb
MaxBlesch <s6mables@uni-bonn.de>
2019-06-24 15:59:50 +0200

    Added non richardson option for jacobain matrix. Adjusted tests.

--- estimagic/differentiation/jacobian.py
+++ estimagic/differentiation/jacobian.py
@@ -1,8 +1,20 @@
 import numdifftools as nd
+import numpy as np
 import pandas as pd
 
+from estimagic.differentiation.first_order_auxiliary import backward
+from estimagic.differentiation.first_order_auxiliary import central
+from estimagic.differentiation.first_order_auxiliary import forward
 
-def jacobian(func, params_sr, method="central", func_args=None, func_kwargs=None):
+
+def jacobian(
+    func,
+    params_sr,
+    method="central",
+    extrapolation="richardson",
+    func_args=None,
+    func_kwargs=None,
+):
     """
     Calculate the jacobian of *func*.
 
@@ -15,6 +27,8 @@ def jacobian(func, params_sr, method="central", func_args=None, func_kwargs=None
         func_kwargs (dict): additional positional arguments for func.
         method (string): The method for the computation of the derivative. Default is
                          central as it gives the highest accuracy.
+        extrapolation (string): This variable allows to specify the use of the
+        richardson extrapolation.
 
     Returns:
         DataFrame: If func returns a Series, the index is the index of this Series or
@@ -28,9 +42,28 @@ def jacobian(func, params_sr, method="central", func_args=None, func_kwargs=None
     func_args = [] if func_args is None else func_args
     func_kwargs = {} if func_kwargs is None else func_kwargs
     f_x0 = func(params_sr, *func_args, **func_kwargs)
-    jac_np = nd.Jacobian(func, method=method)(params_sr, *func_args, **func_kwargs)
-    if isinstance(f_x0, pd.Series):
-        jac = pd.DataFrame(index=f_x0.index, columns=params_sr.index, data=jac_np)
+    if extrapolation == "richardson":
+        # For the richardson extrapolation we use, the numdifftools library.
+        jac_np = nd.Jacobian(func, method=method)(params_sr, *func_args, **func_kwargs)
+        if isinstance(f_x0, pd.Series):
+            jac = pd.DataFrame(index=f_x0.index, columns=params_sr.index, data=jac_np)
+        else:
+            jac = pd.DataFrame(columns=params_sr.index, data=jac_np)
+        return jac
     else:
-        jac = pd.DataFrame(columns=params_sr.index, data=jac_np)
-    return jac
+        if method == "forward":
+            f = forward
+        elif method == "backward":
+            f = backward
+        else:
+            f = central
+        if isinstance(f_x0, pd.Series):
+            jac = pd.DataFrame(index=f_x0.index, columns=params_sr.index)
+        else:
+            jac = pd.DataFrame(columns=params_sr.index)
+        for var in jac.columns:
+            # The rule of thumb for the stepsize is implemented
+            h = (1 + abs(params_sr[var])) * np.sqrt(np.finfo(float).eps)
+            f_diff = f(func, f_x0, params_sr, var, h, *func_args, **func_kwargs)
+            jac[var] = f_diff / h
+        return jac
--- estimagic/tests/differentiation/test_jacobian.py
+++ estimagic/tests/differentiation/test_jacobian.py
@@ -24,6 +24,21 @@ def test_jacobian_central(statsmodels_fixtures):
             logit_loglikeobs,
             fix["params"],
             method="central",
+            extrapolation="",
+            func_args=[fix["y"], fix["x"]],
+        ),
+        fix["jacobian"],
+    )
+
+
+def test_jacobian_central_richardson(statsmodels_fixtures):
+    fix = statsmodels_fixtures
+    assert_frame_equal(
+        jacobian(
+            logit_loglikeobs,
+            fix["params"],
+            method="central",
+            extrapolation="richardson",
             func_args=[fix["y"], fix["x"]],
         ),
         fix["jacobian"],
@@ -37,6 +52,21 @@ def test_jacobian_forward(statsmodels_fixtures):
             logit_loglikeobs,
             fix["params"],
             method="forward",
+            extrapolation="",
+            func_args=[fix["y"], fix["x"]],
+        ),
+        fix["jacobian"],
+    )
+
+
+def test_jacobian_forward_richardson(statsmodels_fixtures):
+    fix = statsmodels_fixtures
+    assert_frame_equal(
+        jacobian(
+            logit_loglikeobs,
+            fix["params"],
+            method="forward",
+            extrapolation="richardson",
             func_args=[fix["y"], fix["x"]],
         ),
         fix["jacobian"],
@@ -50,6 +80,21 @@ def test_jacobian_backward(statsmodels_fixtures):
             logit_loglikeobs,
             fix["params"],
             method="backward",
+            extrapolation="",
+            func_args=[fix["y"], fix["x"]],
+        ),
+        fix["jacobian"],
+    )
+
+
+def test_jacobian_backward_richardson(statsmodels_fixtures):
+    fix = statsmodels_fixtures
+    assert_frame_equal(
+        jacobian(
+            logit_loglikeobs,
+            fix["params"],
+            method="backward",
+            extrapolation="richardson",
             func_args=[fix["y"], fix["x"]],
         ),
         fix["jacobian"],
