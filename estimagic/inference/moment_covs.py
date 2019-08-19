85c733a62c0a0dc6af425a6750305a5b50ffb132
MaxBlesch <s6mables@uni-bonn.de>
2019-08-19 16:07:59 +0200

    Added calculation for standard covariance matrix.

--- estimagic/inference/moment_covs.py
+++ estimagic/inference/moment_covs.py
@@ -14,3 +14,22 @@ def average_contribution(mom_cond):
     n = mom_cond.shape[0]
     mom_cond_mean = np.sum(mom_cond, axis=0) / n
     return mom_cond_mean
+
+
+def calc_standard_cov(mom_cond):
+    """
+    Calculates the standard covariance matrix Omega.
+    Args:
+        mom_cond (np.array): 3d array moment conditions values
+
+    Returns:
+        cov (np.array):
+    """
+    n = mom_cond.shape[0]
+    mom_cond_mean = average_contribution(mom_cond)
+    cov = np.zeros(shape=(mom_cond.shape[1:3]))
+    for i in range(n):
+        mom_cond_dev = mom_cond[i, :, :] - mom_cond_mean
+        cov = cov + np.dot(mom_cond_dev, mom_cond_dev.transpose())
+    cov = cov / n
+    return cov
--- estimagic/tests/inference/test_moment_covs.py
+++ estimagic/tests/inference/test_moment_covs.py
@@ -2,11 +2,12 @@ import numpy as np
 from numpy.testing import assert_array_almost_equal
 
 from estimagic.inference.moment_covs import average_contribution
+from estimagic.inference.moment_covs import calc_standard_cov
 
 
 def test_average_contribution():
     x, y, z = np.random.randint(1, 50, size=3)
-    moment_cond = np.array(range(x * y * z)).reshape(shape=(x, y, z))
+    moment_cond = np.reshape(np.arange(x * y * z), (x, y, z))
     control = np.empty(shape=(y, z))
     control_0_0 = np.sum(np.arange(0, x * y * z, y * z))
     start = -1
@@ -15,3 +16,9 @@ def test_average_contribution():
             start += 1
             control[i, j] = control_0_0 + start * x
     assert_array_almost_equal(average_contribution(moment_cond), control / x)
+
+
+def test_calc_standard_cov():
+    moment_cond = np.reshape(np.arange(12), (3, 4, 1))
+    control = np.full((4, 4), 32, dtype=np.float) / 3
+    assert_array_almost_equal(calc_standard_cov(moment_cond), control)
