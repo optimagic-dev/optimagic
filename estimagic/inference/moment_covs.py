428b7327286ab9c5676bfafa5c7e6f48418eb4e7
Hans-Martin von Gaudecker <hmgaudecker@gmail.com>
2019-08-20 11:12:20 +0200

    Make code a little prettier.

--- estimagic/inference/moment_covs.py
+++ estimagic/inference/moment_covs.py
@@ -1,73 +1,65 @@
 """
 This module provides functions for the calculation of standard errors for anon linear
 GMM estimator. The source of these calculations is
-Bruce E. Hansen - Econometrics.
+Bruce E. Hansen - Econometrics (https://www.ssc.wisc.edu/~bhansen/econometrics).
 We also adopted the notation, where
-    - n: number of observations
+    - nobs: number of observations
     - l: number of moment conditions
     - k: size of estimated parameter vector
 """
 import numpy as np
 
 
-def average_contribution(mom_cond):
-    """
-    Calculates the average condition value for l moment conditions of n
-    observations.
-    Args:
-        mom_cond (np.array):
-            3d array containing for each of n observations
-            either lx1 moment condition values or lxk moment condition derivatives
-
-    Returns:
-        mom_cond_mean (np.array):
-    """
-    n = mom_cond.shape[0]
-    mom_cond_mean = np.sum(mom_cond, axis=0) / n
-    return mom_cond_mean
-
 
 def covariance_moments(mom_cond):
     """
-    Calculates the standard covariance matrix Omega.
+    Calculate the standard covariance matrix Omega.
+
     Args:
-        mom_cond (np.array):
-            3d array containing for each of n observations lx1
-            moment condition values
+        mom_cond (np.array): 2d array matrix of the moment conditions of
+        dimension (nobs, nmoms).
 
     Returns:
-        cov (np.array):
+        cov (np.array): 2d array covariance matrix of the moments (nmoms, nmoms)
+
     """
-    n = mom_cond.shape[0]
-    mom_cond_mean = average_contribution(mom_cond)
-    cov = np.zeros(shape=(mom_cond.shape[1], mom_cond.shape[1]))
-    mom_cond_dev = mom_cond - mom_cond_mean
-    for i in range(n):
-        cov += np.dot(mom_cond_dev[i, :, :], mom_cond_dev[i, :, :].transpose())
-    cov = cov / n
+
+    dev = mom_cond - np.mean(mom_cond, axis=0)
+    cov = dev @ dev.T / mom_cond.shape[0]
+
     return cov
 
 
-def gmm_cov(mom_cond, mom_cond_diff, weight_m):
+def gmm_cov(mom_cond, mom_cond_jacob, mom_weight):
     """
 
     Args:
         mom_cond (np.array):
-            3d array containing for each of n observations
-            either lx1 moment condition values
+            n x l array containing for n individuals the values of the
+            l moment conditions.
 
-        mom_cond_diff (np.array):
-            3d array containing for each of n observations the lxk jacobian of the
-            moment conditions
+        mom_cond_jacob (np.array):
+            n x l x k array containing for n individuals the derivatives
+            of l moments with respect to k parameters.
 
-        weight_m (np.array):
-            A lxl weighting matrix.
+        mom_weight (np.array):
+            Weighting matrix for the moments of dimension (nmoms, nmoms)
 
     Returns:
 
+        sandwich (np.array):
+            Variance-covariance matrix of the GMM estimator of dimension
+            (nparams, nparams)
+
     """
-    omega = covariance_moments(mom_cond)
-    q = average_contribution(mom_cond_diff)
-    qwq_inv = np.linalg.inv(np.linalg.multi_dot([q.T, weight_m, q]))
-    aux = np.linalg.multi_dot([q.T, weight_m, omega, weight_m, q])
-    return np.linalg.multi_dot([qwq_inv, aux, qwq_inv])
+
+    # Use notation from Hansen book everywhere
+    W = mom_weight
+    Omega = covariance_moments(mom_cond)
+    Q = np.mean(mom_cond_jacob, axis=0)
+
+    bread = np.linalg.inv(Q.T @ W @ Q)
+    butter = Q.T @ W @ Omega @ W @ Q
+    sandwich = bread @ butter @ bread
+
+    return sandwich
