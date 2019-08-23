91bbe06ac0357b0641f36b0603ae766b4d3235c0
MaxBlesch <s6mables@uni-bonn.de>
2019-08-23 11:22:23 +0200

    Added unit test for gmm_cov.

--- estimagic/tests/inference/test_moment_covs.py
+++ estimagic/tests/inference/test_moment_covs.py
@@ -1,7 +1,9 @@
 import numpy as np
+import pytest
 from numpy.testing import assert_array_almost_equal
 
 from estimagic.inference.moment_covs import covariance_moments
+from estimagic.inference.moment_covs import gmm_cov
 
 
 def test_covariance_moments_random():
@@ -19,3 +21,23 @@ def test_covariance_moments_unit():
     moment_cond = np.reshape(np.arange(12), (3, 4))
     control = np.full((4, 4), 32, dtype=np.float) / 3
     assert_array_almost_equal(covariance_moments(moment_cond), control)
+
+
+@pytest.fixture
+def statsmodels_fixtures_gmm_cov():
+    fix = {}
+    fix["mom_cond"] = np.array([[0.1, 0.3], [0.7, 1.3]])
+    fix["mom_cond_jacob"] = np.array(
+        [[[3.0, 3.6], [4.0, 4.5]], [[3.2, 3.8], [4.2, 4.9]]]
+    )
+    fix["mom_weight"] = np.array([[13, 17], [23, 29]])
+    fix["cov_result"] = np.array([[0.26888897, -0.19555563], [-0.19555563, 0.14222229]])
+    return fix
+
+
+def test_gmm_cov(statsmodels_fixtures_gmm_cov):
+    fix = statsmodels_fixtures_gmm_cov
+    assert_array_almost_equal(
+        gmm_cov(fix["mom_cond"], fix["mom_cond_jacob"], fix["mom_weight"]),
+        fix["cov_result"],
+    )
