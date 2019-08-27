f3fcdc3874920b58cc0b0ba40c459fbcf0a8ce0a
mo2561057 <moritz.mendel12@gmail.com>
2019-08-27 21:24:11 +0200

    added solver and tests

---
+++ estimagic/optimization/solver_pounders.py
@@ -0,0 +1,212 @@
+"""
+This file contains the wrapper for pounders in the tao package 
+"""
+"""
+Define the algorithm required.
+"""
+from petsc4py import PETSc
+from functools import partial
+
+
+
+
+def solve(func,
+          x,
+          len_out,
+          bounds=None,
+          init_tr=None,
+          tol={"gatol": 0.00000001, "grtol": 0.00000001, "gttol": 0.0000000001},
+          max_iterations=None,
+          gatol=True,
+          grtol=True,
+          gttol=True
+          ):
+    """
+    Args:
+        func: function that takes a 1d numpy array and returns a 1d numpy array
+        x:np.array that contains the start values of the variables of interest
+        bounds: list or tuple of lists containing the bounds for the variable of interest
+                The first list contains the lower value for each param and the upper list the upper value
+        init_tr: Sets the radius for the initial trust region that the optimizer employs.
+        tol: Sets the tolerance for the three default stopping criteria. The routine will stop once the first is reached.
+             One can turn off specific criteria with other args. In this case their value in this dict does not matter.
+        max_iterations: Alternative Stopping criterion. If set the routine will stop after the number of specified
+                        iterations or after the step size is sufficiently small. If the variable is set the default
+                        criteria will all be ignored.
+        gatol: Boolean that indicates whether the gatol should be cosnidered.
+               Explicit description is in the documentation.
+        grtol: Boolean that indicates whether the grtol should be cosnidered.
+               Explicit description is in the documentation
+        gttol: Boolean that indicates whether the gttol should be cosnidered.
+               Explicit description is in the documentation
+    Returns:
+        out: dict with the following key value pairs:
+             "solution": solution vector as np.array,
+             "func values": np.array of value of the objective at the solution
+             "x": np.array of the start values
+             "conv": string indicating the termination reason
+             "sol": list containing: current iterate as int, current value of the objective as float, current value of
+                    the approximated jacobian as float, infeasability norm as float, step length as float and termination
+                    reason as int.
+
+
+    """
+    # we want to get containers for the func verctor and the paras
+    size_paras = len(x)
+    size_objective = len_out
+    paras, crit = _prep_args(size_paras, size_objective)
+
+    # Set the start value
+    paras[:] = x
+
+    def func_tao(tao, paras, f):
+        """
+        This function takes an input, calculates the value of the objective and
+        attaches it to an petsc object f thereafter.
+        func_tao puts the objective in a format that the optimizer requires.
+        Args:
+             tao: The tao object we created for the optimization task
+             paras: 1d np.array of the current values at which we want to evaluate the function.
+             f: Petsc object in which we save the current function value
+        """
+        dev = func(paras.array)
+        # Attach to PETSc object
+        f.array = dev
+
+    # Create the solver object
+    tao = PETSc.TAO().create(PETSc.COMM_WORLD)
+
+    # Set the solver type
+    tao.setType('pounders')
+
+    tao.setFromOptions()
+
+    # Set the procedure for calculating the objective
+    # This part has to be changed if we want more than pounders
+    tao.setResidual(func_tao, crit)
+
+    # We try to set user defined convergence tests
+    if init_tr is not None:
+        tao.setInitialTrustRegionRadius(init_tr)
+
+    # Change they need to be in a container
+    # Set the variable sounds if existing
+    if bounds is not None:
+        low, up = _prep_args(len(x), len(x))
+        low.array = bounds[0]
+        up.array = bounds[1]
+        tao.setVariableBounds([low, up])
+
+    # Set the container over which we optimize that already contians start values
+    tao.setInitial(paras)
+
+    # Obtain tolerances for the convergence criteria
+    # Since we can not create gttol manually we manually set gatol and or grtol to zero once a subset of these two is
+    # turned off and gttol is still turned on
+    tol_real = _get_tolerances(tol, gatol, grtol)
+
+    # Set tolerances for default convergence tests
+    tao.setTolerances(gatol=tol_real["gatol"], gttol=tol_real["gttol"], grtol=tol_real["grtol"])
+
+    # Set user defined convergence tests. Beware that specifiying multiple tests could overwrite others or lead to
+    # unclear behavior.
+    if max_iterations is not None:
+        tao.setConvergenceTest(partial(_max_iters, max_iterations))
+    elif gttol is False and gatol is False:
+        tao.setConvergenceTest(partial(_grtol_conv, tol["grtol"]))
+    elif grtol is False and gttol is False:
+        tao.setConvergenceTest(partial(_gatol_conv, tol["gatol"]))
+    elif gttol is False:
+        tao.setConvergenceTest(partial(_grtol_gatol_conv, tol["grtol"], tol["gatol"]))
+
+    # Run the problem
+    tao.solve()
+
+    # Create a dict that contains relevant information
+    out = dict()
+    out["solution"] = paras.array
+    out["func_values"] = crit.array
+    out["x"] = x
+    out["conv"] = _conv_reason[tao.getConvergedReason()]
+    out["sol"] = tao.getSolutionStatus()
+
+    # Destroy petsc objects for memory reasons
+    tao.destroy()
+    paras.destroy()
+    crit.destroy()
+
+    return out
+
+
+def _prep_args(size_paras, size_objective):
+    """
+    Args:
+        size_paras: int containing the size of the pram vector
+        size_prob: int containing the size of the
+    """
+    # create container for variable of interest
+    paras = PETSc.Vec().create(PETSc.COMM_WORLD)
+    paras.setSizes(size_paras)
+
+    # Create container for criterion function
+    crit = PETSc.Vec().create(PETSc.COMM_WORLD)
+    crit.setSizes(size_objective)
+
+    # Initialize
+    crit.setFromOptions()
+    paras.setFromOptions()
+
+    return paras, crit
+
+def _max_iters(max_iterations, tao):
+    if tao.getSolutionStatus()[0] < max_iterations:
+        return (0)
+    elif tao.getSolutionStatus()[0] >= max_iterations:
+        tao.setConvergedReason(8)
+
+def _gatol_conv(gatol,tao):
+    if tao.getSolutionStatus()[2] >= gatol:
+        return 0
+    elif tao.getSolutionStatus()[2] < gatol:
+        tao.setConvergedReason(3)
+
+def _grtol_conv(grtol, tao):
+    if tao.getSolutionStatus()[2]/tao.getSolutionStatus()[1] >= grtol:
+        return 0
+    elif tao.getSolutionStatus()[2]/tao.getSolutionStatus()[1] < grtol:
+        tao.setConvergedReason(4)
+
+def _grtol_gatol_conv(grtol, gatol, tao):
+    if tao.getSolutionStatus()[2]/tao.getSolutionStatus()[1] >= grtol:
+        return 0
+    elif tao.getSolutionStatus()[2]/tao.getSolutionStatus()[1] < grtol:
+        tao.setConvergedReason(4)
+
+    elif tao.getSolutionStatus()[2] < gatol:
+        tao.setConvergedReason(3)
+
+def _get_tolerances(tol, gatol, grtol):
+    out = tol.copy()
+    if gatol is False and grtol is False:
+        out["gatol"] = -1
+        out["grtol"] = -1
+    elif gatol is False:
+        out["gatol"] = -1
+    elif grtol is False:
+        out["grtol"] = -1
+    return out
+
+
+_conv_reason = {3: "gatol below critical value",
+               4: "grtol below critical value",
+               5: "gttol below critical value",
+               6: "step size small",
+               7: "objective below min value",
+               8: "user defined",
+               -2: "maxits reached",
+               -4: "numerical porblems",
+               -5: "max funcevals reached",
+               -6: "line search failure",
+               -7: "trust region failure",
+               -8: "user defined"
+               }
---
+++ estimagic/tests/optimization/test_pounders.py
@@ -0,0 +1,214 @@
+"""
+Testing the wrapper around pounders
+"""
+"""
+
+"""
+import numpy as np
+from functools import partial
+
+from estimagic.optimization.solver_pounders import solve
+
+
+
+def test_robustness_1():
+    # get random args
+    paras = np.random.uniform(size=3)
+    start = np.random.uniform(size=3)
+    num_agents = 10000
+    objective, x = _set_up_test_1(paras, start, num_agents)
+    len_out = len(objective(x))
+    out = solve(objective, x, len_out), start, paras
+
+    return out
+
+
+def test_robustness_2():
+    # get random args
+    paras = np.random.uniform(size=2)
+    start = np.random.uniform(size=2)
+
+    # Simulate a sample
+    num_agents = 10000
+    objective, x, exog, endog = _set_up_test_2_ols(paras, start, num_agents)
+
+    # Obtain result with Pounders
+    len_out = len(objective(x))
+    out = solve(objective, x, len_out), start, paras
+
+    # Obtain result via ols
+    X = np.concatenate((np.ones(len(exog)).reshape(len(exog), 1), exog.reshape(len(exog), 1)), axis=1).reshape(
+        len(exog), 2)
+    y = endog.reshape(len(endog), 1)
+    ols = np.linalg.lstsq(X, y)
+
+    # compare
+    np.testing.assert_almost_equal(ols[0], np.array(out[0]["solution"]).reshape(2, 1), decimal=1)
+
+    return out
+
+
+def test_box_constr():
+    paras = np.random.uniform(0.3, 0.4, size=2)
+    start = np.random.uniform(0.1, 0.2, size=2)
+    bounds = [[0, 0], [0.3, 0.3]]
+
+    num_agents = 10000
+    objective, x = _set_up_test_2(paras, start, num_agents)
+    len_out = len(objective(x))
+    out = solve(objective, x, len_out, bounds=bounds)
+    assert 0 <= out["solution"][0] <= 0.3
+    assert 0 <= out["solution"][1] <= 0.3
+
+
+def test_max_iters():
+    paras = np.random.uniform(0.3, 0.4, size=2)
+    start = np.random.uniform(0.1, 0.2, size=2)
+    bounds = [[0, 0], [0.3, 0.3]]
+    num_agents = 10000
+    objective, x = _set_up_test_2(paras, start, num_agents)
+    len_out = len(objective(x))
+    out = solve(objective, x, len_out, bounds=bounds, max_iterations=25)
+
+    assert (out["conv"] == "user defined" or out["conv"] == "step size small")
+    if out["conv"] == 8:
+        assert (out["sol"][0] == 25)
+
+
+def test_grtol():
+    paras = np.random.uniform(0.3, 0.4, size=2)
+    start = np.random.uniform(0.1, 0.2, size=2)
+    bounds = [[0, 0], [0.3, 0.3]]
+
+    num_agents = 10000
+    objective, x = _set_up_test_2(paras, start, num_agents)
+    len_out = len(objective(x))
+    out = solve(objective,
+                x,
+                len_out,
+                tol={"grtol": 10, "gatol": 1, "gttol": 1},
+                bounds=bounds,
+                gatol=False,
+                gttol=False
+                )
+
+    assert (out["conv"] == "grtol below critical value" or out["conv"] == "step size small")
+
+    if out["conv"] == 4:
+        assert (out["sol"][2] / out["sol"][1] < 10)
+
+
+def test_gatol():
+    paras = np.random.uniform(0.3, 0.4, size=2)
+    start = np.random.uniform(0.1, 0.2, size=2)
+    bounds = [[0, 0], [0.3, 0.3]]
+
+    num_agents = 10000
+    objective, x = _set_up_test_2(paras, start, num_agents)
+    len_out = len(objective(x))
+    out = solve(objective,
+                x,
+                len_out,
+                tol={"grtol": 1, "gatol": 0.00001, "gttol": 1},
+                bounds=bounds,
+                grtol=False,
+                gttol=False
+                )
+    assert (out["conv"] == "gatol below critical value" or out["conv"] == "step size small")
+
+    if out["conv"] == 3:
+        assert (out["sol"][2] < 0.00001)
+
+
+def test_gttol():
+    paras = np.random.uniform(0.3, 0.4, size=2)
+    start = np.random.uniform(0.1, 0.2, size=2)
+    bounds = [[0, 0], [0.3, 0.3]]
+
+    num_agents = 10000
+    objective, x = _set_up_test_2(paras, start, num_agents)
+    len_out = len(objective(x))
+    out = solve(objective,
+                x,
+                len_out,
+                tol={"grtol": 1, "gatol": 1, "gttol": 1},
+                bounds=bounds,
+                grtol=False,
+                gatol=False
+                )
+    assert (out["conv"] == "gttol below critical value" or out["conv"] == "step size small")
+
+    if out["conv"] == 5:
+        assert (out["sol"][2] < 1)
+
+
+def test_tol():
+    paras = np.random.uniform(0.3, 0.4, size=2)
+    start = np.random.uniform(0.1, 0.2, size=2)
+    bounds = [[0, 0], [0.3, 0.3]]
+
+    num_agents = 10000
+    objective, x = _set_up_test_2(paras, start, num_agents)
+    len_out = len(objective(x))
+    out = solve(objective, x, len_out, bounds=bounds,
+                tol={"gatol": 0.00000001, "grtol": 0.00000001, "gttol": 0.0000000001})
+
+    if out["conv"] == 3:
+        assert (out["sol"][2] < 0.00000001)
+    elif out["conv"] == 4:
+        assert (out["sol"][2] / out["sol"][1] < 0.00000001)
+
+
+def _set_up_test_1(paras, start, num_agents):
+    """
+    This is a bit ad hoc
+
+    """
+    # Simulate values
+    exog, endog = _simulate_sample(num_agents, paras)
+    # Initialize class container
+    func = _return_obj_func(_return_dev, endog, exog)
+    return func, start
+
+
+def _set_up_test_2(paras, start, num_agents):
+    """
+    """
+    exog, endog = _simulate_ols_sample(num_agents, paras)
+    func = _return_obj_func(_return_dev_ols, endog, exog)
+    return func, start
+
+
+def _set_up_test_2_ols(paras, start, num_agents):
+    """
+    """
+    exog, endog = _simulate_ols_sample(num_agents, paras)
+    func = _return_obj_func(_return_dev_ols, endog, exog)
+    return func, start, exog, endog
+
+def _return_dev(endog,exog,x):
+    dev = (endog - np.exp(-x[0]*exog)/(x[1] + x[2]*exog))**2
+    return dev
+
+
+def _return_obj_func(func,endog,exog):
+    out = partial(func,endog,exog)
+    return out
+
+def _return_dev_ols(endog,exog,x):
+    dev = (endog - x[0] - x[1] * exog)**2
+    return dev
+
+
+def _simulate_sample(num_agents,paras):
+    exog = np.random.uniform(0,1,num_agents)
+    error_term = np.random.normal(0,0.5,num_agents)
+    endog = np.exp(-paras[0]*exog)/(paras[1]+paras[2]*exog)+error_term
+    return exog,endog
+
+def _simulate_ols_sample(num_agents,paras):
+    exog = np.random.uniform(-5,5,num_agents)
+    error_term = np.random.normal(0,1,num_agents)
+    endog = paras[0] + paras[1]*exog +error_term
+    return exog,endog
+
