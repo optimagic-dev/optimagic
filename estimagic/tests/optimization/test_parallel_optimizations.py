905b506027ab697f88d4e3d9ba17cf5aa2caf500
Christian Zimpelmann <zimpelmann.christian@gmail.com>
2019-09-30 10:25:16 +0200

    test for parallel optimization implemented. Fails as expected.

--- estimagic/tests/optimization/test_parallel_optimizations.py
+++ estimagic/tests/optimization/test_parallel_optimizations.py
@@ -7,23 +7,25 @@ from numpy.testing import assert_array_almost_equal
 from estimagic.optimization.optimize import minimize
 
 
-def test_replace_pairwise_equality_by_equality():
+def rosen(x):
+    """The Rosenbrock function
+
+    Args:
+        x (pd.Series): Series with the parameters.
+
+    """
+    return np_rosen(x["value"].to_numpy())
+
+
+def np_rosen(x):
+
+    return sum(100.0 * (x[1:] - x[:-1] ** 2.0) ** 2.0 + (1 - x[:-1]) ** 2.0)
+
+
+def test_single_optimization():
     """
     Test an easy single optimization.
     """
-
-    def rosen(x):
-        """The Rosenbrock function
-
-        Args:
-            x (pd.Series): Series with the parameters.
-
-        """
-        return np_rosen(x["value"].to_numpy())
-
-    def np_rosen(x):
-
-        return sum(100.0 * (x[1:] - x[:-1] ** 2.0) ** 2.0 + (1 - x[:-1]) ** 2.0)
 
     params = pd.DataFrame()
     params["value"] = np.array([1.3, 0.7, 1.0, 1.9, 1.2])
@@ -35,3 +37,25 @@ def test_replace_pairwise_equality_by_equality():
     expected_result = [1, 1, 1, 1, 1]
 
     assert_array_almost_equal(result, expected_result)
+
+
+def test_multiple_opt_same_size():
+    """
+    Test a parallel optimization: All inputs are a list of the same length.
+    """
+
+    params = pd.DataFrame()
+    params["value"] = np.array([1.3, 0.7, 1.0, 1.9, 1.2])
+    params["fixed"] = [False, False, True, False, False]
+    params["lower"] = [-1, -1, -1, -1, -1]
+    params["upper"] = [5, 5, 5, 5, 5]
+
+    result = minimize(
+        [rosen, rosen], [params, params], ["nlopt_neldermead", "scipy_L-BFGS-B"]
+    )
+    result_neldermead = [0][0]["internal_x"]
+    result_BFGS = [1][0]["internal_x"]
+    expected_result = [1, 1, 1, 1, 1]
+
+    assert_array_almost_equal(result_neldermead, expected_result)
+    assert_array_almost_equal(result_BFGS, expected_result)
