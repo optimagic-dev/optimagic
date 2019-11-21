55c2d990e0e0206005880eaeb47a7f10cd25a46f
Tobias Raabe <tobiasraabe@users.noreply.github.com>
2019-11-21 21:43:18 +0100

    Move estimagic to OSE on Anaconda and add new release.py. (#95)
    * Various cahnges.
    
    * more changes.
    
    * Move to ose.
    
    * Correct download source on Anaconda.
    
    * small change.
    
    * Update release.py.
    
    * Update index.rst.
    
    * Added force flag to upload.
    
    * Added bump2version and corrected version strings.

--- release.py
+++ release.py
@@ -5,16 +5,81 @@ before you run the script. The steps for this are explained here:
 https://conda.io/docs/user-guide/tutorials/build-pkgs.html
 
 """
+import shutil
+import subprocess
 from os.path import join
 from os.path import split
-from subprocess import run
+from pathlib import Path
 
+import click
 from conda_build.api import build
 from conda_build.api import convert
 
 
-if __name__ == "__main__":
-    platforms = ["osx-64", "linux-64", "win-32", "win-64"]
+CONTEXT_SETTINGS = {"help_option_names": ["-h", "--help"]}
+
+TEMPORARY_FOLDERS = [
+    Path("documentation", "_build"),
+    Path("documentation", "_generated"),
+] + list(Path(".").glob("**/__pycache__"))
+
+
+@click.group(context_settings=CONTEXT_SETTINGS, chain=True, invoke_without_command=True)
+@click.pass_context
+def cli(ctx):
+    """Build, convert and upload a conda package."""
+    ctx.invoke(clean)
+    ctx.invoke(build_convert_upload)
+
+
+@cli.command()
+def clean():
+    """Clean the package folder from temporary files and folders."""
+    click.secho("-" * 88)
+    click.secho("Cleaning - Start\n")
+    click.secho(
+        "Unnecessary files in the repository can cause errors while building the\n"
+        "building the package. Check for some known issues.\n"
+    )
+
+    # Check for environments in .tox.
+    tox_envs = list(Path(".", ".tox").glob("*"))
+    if tox_envs and click.confirm(
+        "Do you want to remove all tests environments under .tox?"
+    ):
+        for path in tox_envs:
+            subprocess.run(f"conda env remove -p {path}", shell=True)
+        shutil.rmtree(".tox")
+
+    # Check for temporary files and folders which can be deleted.
+    for path in TEMPORARY_FOLDERS:
+        if path.is_dir():
+            shutil.rmtree(path)
+        elif path.is_file():
+            path.unlink()
+        else:
+            pass
+
+    # Check for uncommitted and untracked files.
+    files = subprocess.run(
+        "git status --porcelain", shell=True, stdout=subprocess.PIPE
+    ).stdout.decode("utf-8")
+    if files:
+        click.secho(
+            "There are some uncommitted or untracked files. Please manually clean \n"
+            "them up or move them somewhere else before proceeding.\n",
+            color="yellow",
+        )
+        click.secho(files, color="yellow")
+        raise click.Abort
+
+    click.secho("Cleaning - End")
+    click.secho("-" * 88 + "\n")
+
+
+@cli.command()
+def build_convert_upload():
+    platforms = ["osx-64", "linux-32", "linux-64", "win-32", "win-64"]
     built_packages = build(".", need_source_download=False)
     converted_packages = []
 
@@ -23,16 +88,16 @@ if __name__ == "__main__":
         out_root, os = split(helper)
         pfs = [pf for pf in platforms if pf != os]
         convert(path, output_dir=out_root, platforms=pfs)
-        print(
-            "\n{} was converted to the following platforms: {}\n".format(
-                package_name, pfs
-            )
-        )
         for pf in pfs:
             converted_packages.append(join(out_root, pf, package_name))
 
     all_packages = built_packages + converted_packages
     for package in all_packages:
         _, package_name = split(package)
-        run(["anaconda", "upload", package])
-        print("\n{} was uploaded to anaconda.org".format(package_name))
+        subprocess.run(
+            ["anaconda", "upload", "--force", "--user", "OpenSourceEconomics", package]
+        )
+
+
+if __name__ == "__main__":
+    cli()
