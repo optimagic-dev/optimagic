b82cc8c7adc0aad2464d598f9f4b79fdfcc1c4b3
Annica Gehlen <ama.gehlen@gmail.com>
2020-02-10 15:39:40 +0100

    Deleted whitespace in links.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -140,9 +140,7 @@ nbsphinx_prolog = r"""
 .. only:: html
 
     .. nbinfo::
-        Download the notebook :download:`here
-        <https://nbviewer.jupyter.org/github/OpenSourceEconomics/estimagic/blob/master/
-        {{ docname }}>`!
+        Download the notebook :download:`here <https://nbviewer.jupyter.org/github/OpenSourceEconomics/estimagic/blob/master/{{ docname }}>`!
 """
 
 # -- Options for HTML output ----------------------------------------------
