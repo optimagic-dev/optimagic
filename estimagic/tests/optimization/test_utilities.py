350550044a63eb36460ed280c22578b135b6516b
timmens <tim@mensinger96.com>
2020-02-11 17:11:37 +0100

    Updates test for `robust_cholesky`.
    This commit updates the tests for the `robust_cholesky` function
    and fixes a bug in `random_cov`.

--- estimagic/tests/optimization/test_utilities.py
+++ estimagic/tests/optimization/test_utilities.py
@@ -118,14 +118,16 @@ def random_cov(dim, seed):
     return cov
 
 
-seeds = [58822, 3181, 98855, 44002, 47631, 97741, 10655, 4600, 1151, 58189]
+seeds = [58822, 3182, 98855, 44002, 47631, 97741, 10655, 4600, 1151, 58189]
 dims = [8] * 6 + [10, 12, 15, 20]
+# Tests only run with larger threshold.
+thresh = -4 * np.finfo(float).eps
 
 
 @pytest.mark.parametrize("dim, seed", zip(dims, seeds))
 def test_robust_cholesky_with_zero_variance(dim, seed):
     cov = random_cov(dim, seed)
-    chol = robust_cholesky(cov)
+    chol = robust_cholesky(cov, thresh)
     aaae(chol.dot(chol.T), cov)
     assert (chol[np.triu_indices(len(cov), k=1)] == 0).all()
 
