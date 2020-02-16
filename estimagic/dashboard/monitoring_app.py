ad2ff973be048550dc7b063b8c4964d2e5081156
Klara Roehrl <klara.roehrl@gmail.com>
2020-02-16 11:33:01 +0100

    Improve the criterion convergence plot.

--- estimagic/dashboard/monitoring_app.py
+++ estimagic/dashboard/monitoring_app.py
@@ -1,10 +1,12 @@
 """Show the development of one optimization's criterion and parameters over time."""
 from bokeh.layouts import Column
 from bokeh.layouts import Row
+from bokeh.models import HoverTool
 from bokeh.models import Panel
 from bokeh.models import Tabs
 
-from estimagic.dashboard_old.plotting_functions import plot_time_series
+from estimagic.dashboard_old.plotting_functions import create_wide_figure
+from estimagic.dashboard_old.plotting_functions import get_color_palette
 from estimagic.logging.create_database import load_database
 from estimagic.logging.read_database import read_last_iterations
 
@@ -17,31 +19,90 @@ def monitoring_app(doc, database_path):
         database_path (str or pathlib.Path): path to the database
     """
     database = load_database(database_path)
+    data_dict = read_last_iterations(
+        database=database,
+        tables=["criterion_history", "params_history"],
+        n=-1,
+        return_type="bokeh",
+    )
+
     # sachen extrahieren, die ich brauch
     #   lad db_options mit read_scalar_field
     #   lad params -> für "_internal_free" Parameter, Gruppe ... -> ANSCHAUEN
 
     # alle Tabellen auf einmal laden -> ColumnDataSources updaten!
 
-    tab1 = _setup_convergence_tab(database=database)
+    tab1 = _setup_convergence_tab(data=data_dict)
     tabs = Tabs(tabs=[tab1])
     doc.add_root(tabs)
 
 
-def _setup_convergence_tab(database):
+def _setup_convergence_tab(data):
     """Create the figures and plot available time series of the criterion and parameters.
 
     Args:
-        database (sqlalchemy.MetaData)
+        data (dict): dictionary containing ColumnDataSources with the time series of
+            the criterion values and parameter values.
 
+    Returns:
+        tab (bokeh.Panel)
     """
-    criterion_values = read_last_iterations(
-        database=database, tables="criterion_history", n=-1, return_type="bokeh"
-    )
 
-    fitness_plot = plot_time_series(
-        data=criterion_values, x_name="iteration", y_keys=["value"], title="Criterion",
+    criterion_plot = _plot_time_series(
+        data=data["criterion_history"],
+        x_name="iteration",
+        y_keys=["value"],
+        y_names=["criterion"],
+        title="Criterion",
     )
-    plots = [Row(fitness_plot)]
+    plots = [Row(criterion_plot)]
     tab = Panel(child=Column(*plots), title="Convergence Tab")
     return tab
+
+
+def _plot_time_series(data, y_keys, x_name, title, y_names=None):
+    """
+    Plot time series linking the *y_keys* to a common *x_name* variable.
+
+    Args:
+        data (ColumnDataSource):
+            data that contain the y_keys and x_name
+        y_keys (list):
+            list of the entries in the data that are to be plotted
+        x_name (str):
+            name of the entry in the data that will be on the x axis
+        title (str):
+            title of the plot
+        y_names (list):
+            if given these replace the y keys for the names of the lines
+
+    """
+    if y_names is None:
+        y_names = y_keys
+
+    plot = create_wide_figure(title=title)
+
+    colors = get_color_palette(nr_colors=len(y_keys))
+    for color, y_key, y_name in zip(colors, y_keys, y_names):
+        line_glyph = plot.line(
+            source=data,
+            x=x_name,
+            y=y_key,
+            line_width=2,
+            legend_label=y_name,
+            color=color,
+            muted_color=color,
+            muted_alpha=0.2,
+        )
+    tooltips = [(x_name, "@" + x_name)]
+    tooltips += [("param_name", y_name), ("param_value", "@" + y_key)]
+    hover = HoverTool(renderers=[line_glyph], tooltips=tooltips)
+    plot.tools.append(hover)
+
+    if len(y_key) == 1:
+        plot.legend.visible = False
+    else:
+        plot.legend.click_policy = "mute"
+        plot.legend.location = "top_left"
+
+    return plot
