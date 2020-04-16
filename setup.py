3e6f0d5baff9bb9f973901e75e85023bc11839dc
Janos Gabler <janos.gabler@gmail.com>
2020-04-16 09:29:17 +0200

    Bump version.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="estimagic",
-    version="0.0.28",
+    version="0.0.29",
     description="Tools for the estimation of (structural) econometric models.",
     long_description="""
         Estimagic is a Python package that helps to build high-quality and user
