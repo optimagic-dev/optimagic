2a33cbe3ea33fa63796d0b6b6558aaba7c41753f
mariam petrosyan <mpetorsyan@uni-bonn.de>
2020-07-04 17:09:12 +0200

    use more format() in generate_notes_html

--- estimagic/visualization/estimation_table.py
+++ estimagic/visualization/estimation_table.py
@@ -665,13 +665,12 @@ def _generate_notes_html(append_notes, notes_label, sig_levels, custom_notes, df
                 )
                 if len(custom_notes) > 1:
                     for i in range(1, len(custom_notes)):
-                        notes_text += "<tr>"
-                        notes_text += '<td></td><td colspan="'
-                        notes_text += str(n_columns + n_levels - 1)
-                        notes_text += '" style="text-align: right">'
-                        notes_text += custom_notes[i]
-                        notes_text += "</td></tr>"
-
+                        notes_text += """
+                        <tr><td></td><td colspan="{}"style="text-align: right">
+                        {}</td></tr>
+                        """.format(
+                            n_columns + n_levels - 1, custom_notes[i]
+                        )
             elif isinstance(custom_notes, str):
                 notes_text += '<td></td><td colspan="'
                 notes_text += str(n_columns + n_levels - 1)
