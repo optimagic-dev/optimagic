78fdf28f8e636e87f44e3322063ea87432b8b803
Mariam Petrosyan <44179698+mpetrosian@users.noreply.github.com>
2020-07-05 09:46:16 +0200

    Estimation table (#160)

--- estimagic/config.py
+++ estimagic/config.py
@@ -1,3 +1,10 @@
+from pathlib import Path
+
 DEFAULT_DATABASE_NAME = "logging.db"
 DEFAULT_SEED = 5471
 MAX_CRITERION_PENALTY = 1e200
+
+
+TEST_DIR = Path(__file__).parent / "tests"
+
+DOCS_DIR = Path(__file__).parent.parent / "docs"
