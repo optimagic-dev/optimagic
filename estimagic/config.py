44523d0befef8e90f3d69bb9e842da04eb4d7edc
Janos Gabler <janos.gabler@gmail.com>
2020-09-07 14:00:47 +0200

    Sort imports.

--- estimagic/config.py
+++ estimagic/config.py
@@ -2,7 +2,6 @@ from pathlib import Path
 
 DEFAULT_DATABASE_NAME = "logging.db"
 DEFAULT_SEED = 5471
-MAX_CRITERION_PENALTY = 1e200
 
 TEST_DIR = Path(__file__).parent / "tests"
 
--- estimagic/tests/conftest.py
+++ estimagic/tests/conftest.py
@@ -1,5 +1,6 @@
-from pathlib import Path
 import os
+from pathlib import Path
+
 import numpy as np
 import pandas as pd
 import pytest
