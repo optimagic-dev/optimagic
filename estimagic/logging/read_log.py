20efd1eab89ca3ed8a100cf38b8272e52b5bec2c
Janos Gabler <janos.gabler@gmail.com>
2020-09-08 14:00:27 +0200

    Add high level log reading function.

---
+++ estimagic/logging/read_log.py
@@ -0,0 +1,83 @@
+"""Functions to read data from the database used for logging.
+
+
+The functions in the module are meant for end users of estimagic. They do not require
+any knowledge of databases. The downside is that they are slower than what one could
+achieve when directly working with the more low-level database utilities.
+
+Thus, they should not be used internally (e.g. in the dashboard) but only to read the
+log interactively.
+
+"""
+from estimagic.logging.database_utilities import load_database
+from estimagic.logging.database_utilities import read_last_rows
+from estimagic.logging.database_utilities import read_specific_row
+
+
+def read_optimization_iteration(path, iteration, include_internals=False):
+    """Get information about an optimization iteration.
+
+    Args:
+        path (str or pathlib.Path): Path to the sqlite database file used for logging.
+            Typically, those have the file extension ``.db``.
+        iteration (int): The index of the iteration that should be retrieved. The row_id
+            behaves as Python list indices, i.e. 0 identifies the first iteration,
+            -1 the last one, etc.
+        include_internals (bool): Whether internally used quantities like the
+            internal parameter vector and the corresponding derivative etc. are included
+             in the result. Default False. This should only be used by advanced users.
+
+    Returns:
+        dict: The logged information corresponding to the iteration. The keys correspond
+            to database columns.
+
+    Raises:
+        KeyError if the iteration is out of bounds.
+
+    """
+    database = load_database(path=path, fast_logging=False)
+    optimization_problem = read_last_rows(
+        database=database,
+        table_name="optimization_problem",
+        n_rows=1,
+        return_type="list_of_dicts",
+    )
+    start_params = optimization_problem[0]["params"]
+
+    if iteration >= 0:
+        rowid = iteration + 1
+    else:
+        last_iteration = read_last_rows(
+            database=database,
+            table_name="optimization_iterations",
+            n_rows=1,
+            return_type="list_of_dicts",
+        )
+        highest_rowid = last_iteration[0]["rowid"]
+
+        rowid = highest_rowid + iteration + 1
+
+    data = read_specific_row(
+        database=database,
+        table_name="optimization_iterations",
+        rowid=rowid,
+        return_type="list_of_dicts",
+    )
+
+    if len(data) == 0:
+        raise IndexError(f"Invalid iteration requested: {iteration}")
+    else:
+        data = data[0]
+
+    params = start_params.copy()
+    params["value"] = data.pop("external_params")
+    data["params"] = params
+
+    to_remove = ["distance_origin", "distance_ones"]
+    if not include_internals:
+        to_remove += ["internal_params", "internal_derivative"]
+    for key in to_remove:
+        if key in data:
+            del data[key]
+
+    return data
---
+++ estimagic/tests/logging/test_read_log.py
@@ -0,0 +1,42 @@
+import numpy as np
+import pandas as pd
+from pandas.testing import assert_frame_equal
+
+from estimagic.logging.database_utilities import append_row
+from estimagic.logging.database_utilities import load_database
+from estimagic.logging.database_utilities import make_optimization_iteration_table
+from estimagic.logging.database_utilities import make_optimization_problem_table
+from estimagic.logging.read_log import read_optimization_iteration
+
+
+def test_read_optimization_iteration(tmp_path):
+    path = tmp_path / "test.db"
+    database = load_database(path=path)
+
+    # add the optimization_iterations table
+    make_optimization_iteration_table(database, first_eval={"output": 0.5})
+    iteration_data = [
+        {"external_params": np.array([0])},
+        {"external_params": np.array([1])},
+        {"external_params": np.array([2])},
+    ]
+
+    for data in iteration_data:
+        append_row(data, "optimization_iterations", database, path, False)
+
+    # add the optimization_problem table
+    make_optimization_problem_table(database)
+    problem_data = {"params": pd.DataFrame(data=[10], columns=["value"])}
+    append_row(problem_data, "optimization_problem", database, path, False)
+
+    first_row_calc = read_optimization_iteration(path, 0)
+    assert first_row_calc["rowid"] == 1
+    calculated_params = first_row_calc["params"]
+    expected_params = pd.DataFrame(data=[0], columns=["value"])
+    assert_frame_equal(calculated_params, expected_params)
+
+    last_row_calc = read_optimization_iteration(path, -1)
+    assert last_row_calc["rowid"] == 3
+    calculated_params = last_row_calc["params"]
+    expected_params = pd.DataFrame(data=[2], columns=["value"])
+    assert_frame_equal(calculated_params, expected_params)
