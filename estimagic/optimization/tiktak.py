62064fa16b493abc6e19b681e6ce2ec3fba6f94e
Andrew Souther <asouther@fordham.edu>
2021-03-29 09:00:15 -0400

    adding first draft of tiktak and test_tiktak

---
+++ estimagic/optimization/tiktak.py
@@ -0,0 +1,132 @@
+import numpy as np
+import pandas as pd
+import chaospy
+from estimagic.optimization.optimize import minimize
+
+def TikTakOptimize(criterion,
+                   bounds,
+                   local_search_algorithm,
+                   num_points,
+                   num_restarts,
+                   shrink_after,
+                   *,
+                   sampling=None,
+                   algo_options=None,
+                   logging=False):
+    """
+    Minimize a function using the TikTak algorithm. 
+
+    TikTak (`Arnoud, Guvenen, and Kleineberg 
+    <https://fguvenendotcom.files.wordpress.com/2019/09/agk2019-september-nber-submit.pdf>`_) 
+    is an algorithm for solving global optimization problems. 
+    It performs local searches from a set of carefully-selected points in the parameter space. 
+
+    First implemented in Python by Alisdair McKay (`GitHub Repository <https://github.com/amckay/TikTak>`_)
+
+    Args:
+        criterion (Callable): A function that takes a pandas DataFrame (see
+            :ref:`params`) as first argument and returns a scalar floating point. 
+        bounds (pandas.DataFrame): A DataFrame with one column called "lower_bounds" and 
+            another called "upper_bounds," with an entry in each column for every dimension
+            of the optimization problem.  
+        local_search_algorithm (str or callable): Specifies the optimization algorithm. 
+            For supported algorithms this is a string with the name of the algorithm. 
+            Otherwise it can be a callable with the estimagic algorithm interface. See 
+            :ref:`algorithms`.
+        num_points (int): the number of initial points to sample in the parameter space.
+        num_restarts (int): the number of initial sample points from which to perform
+            local optimization. this value must be smaller than num_points. 
+        shtrink_after (int): after this many local optimization searches, the global 
+            algorithm starts to adjust the weight of previous and future points in the
+            search process.
+        sampling (str): specifies the procedure for random or quasi-random sampling. 
+            See chaospy documentation of  an overview of possible rules: 
+            https://chaospy.readthedocs.io/en/master/reference/sampling.html#low-discrepancy-sequences
+        algo_options (dict): Algorithm specific configuration of the local optimization. 
+            See :ref:`list_of_algorithms` for supported options of each algorithm.
+        logging #TODO
+    """
+
+    lower_bounds = bounds["lower_bounds"]
+    lower_bounds  = lower_bounds.to_numpy()
+    lower_bounds = lower_bounds.transpose()
+
+    upper_bounds = bounds["upper_bounds"]
+    upper_bounds  = upper_bounds.to_numpy()
+    upper_bounds = upper_bounds.transpose()
+
+    def df_wrapper(array):
+        df = pd.DataFrame(data=array, 
+                          columns=["value"], 
+                          index=[f"x_{i}" for i in range(len(array))])
+        return df
+    
+    nparam = len(lower_bounds)
+
+    #the default sampling method depends on nparam
+    if sampling==None:
+        if nparam <= 15:
+            sampling = 'sobol'
+        else: 
+            sampling = 'random'
+    
+    #start the global search for initial points
+    distribution = chaospy.Iid(chaospy.Uniform(0,1), nparam)
+    xstarts = distribution.sample(num_points, rule=sampling) #generate a sample based on the sampling rule
+    xstarts = np.transpose(xstarts) #transpose the array of samples
+    xstarts = lower_bounds[np.newaxis,:] + (upper_bounds-lower_bounds)[np.newaxis,:]*xstarts #spread out the sample within the bounds 
+
+    # --- evaluate the criterion function on each starting point ----
+    dfxstarts= [df_wrapper(point) for point in xstarts]
+    
+    y = np.array([criterion(point) for point in dfxstarts])
+    # sort the results
+    I = np.argsort(y)
+    xstarts = xstarts[I]
+    y = y[I]
+
+    # take the best Imax
+    xstarts = list(xstarts[:num_restarts])
+    
+
+    #define some parameters for the local searches
+    best_so_far = {"solution_criterion": 1e10}
+    result_trackers = []
+    num_jobs = len(xstarts)
+    num_func_evals = 0
+
+    for i in range(num_jobs):
+        ishrink = shrink_after #local_search_options["shrink_after"]
+        new_task = xstarts.pop()
+
+        if i >= ishrink: # shrink towards best so far
+            #TODO: implement theta that's more closely aligned with Guvenen paper
+            theta = 0.02 + (0.98-0.02)*float(i-ishrink)/float(num_jobs-ishrink)  # when i = ishrink, place 2% weight on best, when i = Imax place 98% weight on max
+            new_task = theta*best_so_far['solution_x'] + (1-theta)*new_task
+
+        #params dataframe
+        params = df_wrapper(new_task)
+        
+        result = minimize(
+            criterion = criterion,
+            params = params, 
+            algorithm = local_search_algorithm,
+            algo_options = algo_options,
+            logging=logging
+        )
+        
+        #if the new x returns the lowest function val yet, it's the best so far
+        if result['solution_criterion'] < best_so_far['solution_criterion'] :
+            print( 'best so far %f' % result['solution_criterion'])
+            best_so_far = result
+
+        result_trackers.append(result)
+        num_func_evals += result['n_criterion_evaluations']
+
+    res = {
+        "solution_x": best_so_far["solution_x"],
+        "solution_criterion": best_so_far["solution_criterion"],
+        "n_criterion_evaluations": num_func_evals
+    }
+
+    return res
---
+++ estimagic/tests/optimization/test_tiktak.py
@@ -0,0 +1,55 @@
+"""
+Test the TikTak optimization algorithm
+"""
+from estimagic.optimization.tiktak import TikTakOptimize 
+import numpy as np
+import pandas as pd
+
+
+#define a criterion function for testing TikTak
+def Griewank(params):
+   column = params["value"]
+   array = column.to_numpy()
+   X = np.transpose(array)
+   d = X.shape[0]
+   i = np.arange(1, d+1)
+   res = 2 + np.sum(X**2/200) - np.prod(np.cos(X/np.sqrt(i)))
+   return res
+
+domain = (-100,100) #domain of the Griewank function
+true_min = 1 #the real min. value of the Griewank function 
+n = 10 # the dimension of our problem
+   
+lower_bounds = [domain[0] for i in range(n)]  # lower bounds on each dimension
+upper_bounds = [domain[1] for i in range(n)]  # upper bounds on each dimension
+bounds_dict = {"lower_bounds": lower_bounds,
+                  "upper_bounds": upper_bounds}
+
+## define all the arguments for our optimization
+criterion = Griewank
+bounds = pd.DataFrame(bounds_dict)
+local_search_algorithm = "scipy_neldermead"
+num_points = 1000
+num_restarts = 100
+shrink_after = 30
+algo_options = {"convergence_absolute_criterion_tolerance": 1e-8}
+logging = False
+   
+#run the algorithm
+solution = TikTakOptimize(
+   criterion=criterion,
+   bounds=bounds,
+   local_search_algorithm=local_search_algorithm,
+   num_points=num_points,
+   num_restarts=num_restarts,
+   shrink_after=shrink_after,
+   algo_options=algo_options                                 
+)
+
+x = solution['solution_x']
+value = solution['solution_criterion']
+n_criterion_evals = solution['n_criterion_evaluations']
+
+print(f'The solution is {x}')
+print(f'The criterion value at the min is {value}')
+print(f'Number of function evaluations was {n_criterion_evals}')
