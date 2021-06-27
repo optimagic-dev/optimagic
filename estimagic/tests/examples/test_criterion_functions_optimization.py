a2bb85d427aaed99f2f3d9757c5beede9a31a3be
Madhu Chandra <c12.madhu@gmail.com>
2021-06-27 16:53:23 +0200

    Add remaining tests for criterion function implementations.

--- estimagic/tests/examples/test_criterion_functions_optimization.py
+++ estimagic/tests/examples/test_criterion_functions_optimization.py
@@ -1,10 +1,15 @@
-from unittest import TestCase
-
 import numpy as np
 import pandas as pd
 import pytest
 from numpy.testing import assert_allclose
+from numpy.testing import assert_array_equal
 
+from estimagic.examples.criterion_functions_optimization_tests import (
+    rosenbrock_criterion_and_gradient,
+)
+from estimagic.examples.criterion_functions_optimization_tests import (
+    rosenbrock_dict_criterion,
+)
 from estimagic.examples.criterion_functions_optimization_tests import (
     rosenbrock_gradient,
 )
@@ -13,6 +18,9 @@ from estimagic.examples.criterion_functions_optimization_tests import (
 )
 from estimagic.examples.criterion_functions_optimization_tests import (
     rosenbrock_scalar_criterion,
+)
+from estimagic.examples.criterion_functions_optimization_tests import (
+    rotated_hyper_ellipsoid_criterion_and_gradient,
 )
 from estimagic.examples.criterion_functions_optimization_tests import (
     rotated_hyper_ellipsoid_dict_criterion,
@@ -41,7 +49,7 @@ from estimagic.examples.criterion_functions_optimization_tests import (
 )
 
 
-# Fix input params to test all criterion functions
+# Fix input params to test every criterion function
 @pytest.fixture
 def input_params():
     params = pd.DataFrame({"value": [9, 9, 6, 7, -5]})
@@ -68,29 +76,32 @@ def test_trid_scalar_criterion(input_params, trid_criterion_output):
 
 def test_trid_gradient(input_params, trid_gradient_output):
     out = trid_gradient(input_params)
-    assert_allclose(out, trid_gradient_output)
+    assert_array_equal(out, trid_gradient_output)
     assert isinstance(out, np.ndarray)
 
 
 def test_trid_pandas_gradient(input_params, trid_gradient_output):
     out = trid_pandas_gradient(input_params)
-    assert_allclose(out, trid_gradient_output)
+    assert_array_equal(out, trid_gradient_output)
     assert isinstance(out, pd.Series)
 
 
 def test_trid_dict_criterion(input_params):
-    out = trid_dict_criterion(input_params)
-    assert isinstance(out, dict)
-    assert out == {"value": 83}
+    out_dict = trid_dict_criterion(input_params)
+    assert isinstance(out_dict, dict)
+    assert out_dict == {"value": 83}
 
 
 def test_trid_criterion_and_gradient(
     input_params, trid_gradient_output, trid_criterion_output
 ):
     out = trid_criterion_and_gradient(input_params)
-    res = trid_criterion_output, trid_gradient_output
-    # res = 83, np.array([7, 1, -6, 11, -19]
-    assert_allclose(out, res)
+    expected = trid_criterion_output, trid_gradient_output
+    assert isinstance(out, tuple)
+    assert len(out) == 2
+    assert out[0] == expected[0]
+    assert isinstance(out[1], np.ndarray)
+    assert_array_equal(out[1], expected[1])
 
 
 # Fixtures for rhe function output
@@ -113,27 +124,44 @@ def test_rhe_scalar_criterion(input_params, rhe_criterion_output):
 
 def test_rhe_gradient(input_params, rhe_gradient_output):
     out = rotated_hyper_ellipsoid_gradient(input_params)
-    assert_allclose(out, rhe_gradient_output)
+    assert_array_equal(out, rhe_gradient_output)
     assert isinstance(out, np.ndarray)
 
 
 def test_rhe_pandas_gradient(input_params, rhe_gradient_output):
     out = rotated_hyper_ellipsoid_pandas_gradient(input_params)
-    assert_allclose(out, rhe_gradient_output)
+    assert_array_equal(out, rhe_gradient_output)
     assert isinstance(out, pd.Series)
 
 
 def test_rhe_dict_criterion(input_params):
-    out = rotated_hyper_ellipsoid_dict_criterion(input_params)
-    assert isinstance(out, dict)
-    expected = {
+    out_dict = rotated_hyper_ellipsoid_dict_criterion(input_params)
+    expected_dict = {
         "value": 960,
         "contributions": np.array([81, 162, 198, 247, 272]),
         "root_contributions": np.array(
             [9, 12.72792206, 14.07124728, 15.71623365, 16.4924225]
         ),
     }
-    TestCase().assertDictEqual(out, expected)
+    assert isinstance(out_dict, dict)
+    assert len(out_dict) == 3
+    assert isinstance(out_dict["contributions"], np.ndarray)
+    assert isinstance(out_dict["root_contributions"], np.ndarray)
+
+    for key in expected_dict.keys():
+        assert_allclose(out_dict[key], expected_dict[key])
+
+
+def test_rhe_criterion_and_gradient(
+    input_params, rhe_gradient_output, rhe_criterion_output
+):
+    out = rotated_hyper_ellipsoid_criterion_and_gradient(input_params)
+    expected = rhe_criterion_output, rhe_gradient_output
+    assert isinstance(out, tuple)
+    assert len(out) == 2
+    assert isinstance(out[1], np.ndarray)
+    assert out[0] == expected[0]
+    assert_array_equal(out[1], expected[1])
 
 
 # Fixtures for rosenbrock function output
@@ -156,11 +184,41 @@ def test_rosenbrock_scalar_criterion(input_params, rosenbrock_criterion_output):
 
 def test_rosenbrock_gradient(input_params, rosenbrock_gradient_output):
     out = rosenbrock_gradient(input_params)
-    assert_allclose(out, rosenbrock_gradient_output)
     assert isinstance(out, np.ndarray)
+    assert_array_equal(out, rosenbrock_gradient_output)
 
 
 def test_rosenbrock_pandas_gradient(input_params, rosenbrock_gradient_output):
     out = rosenbrock_pandas_gradient(input_params)
-    assert_allclose(out, rosenbrock_gradient_output)
     assert isinstance(out, pd.Series)
+    assert_array_equal(out, rosenbrock_gradient_output)
+
+
+def test_rosenbrock_criterion_and_gradient(
+    input_params, rosenbrock_gradient_output, rosenbrock_criterion_output
+):
+    out = rosenbrock_criterion_and_gradient(input_params)
+    expected = rosenbrock_criterion_output, rosenbrock_gradient_output
+    assert isinstance(out, tuple)
+    assert len(out) == 2
+    assert isinstance(out[1], np.ndarray)
+    assert out[0] == expected[0]
+    assert_array_equal(out[1], expected[1])
+
+
+def test_rosenbrock_dict_criterion(input_params):
+    out_dict = rosenbrock_dict_criterion(input_params)
+    expected_dict = {
+        "value": 1456789,
+        "contributions": np.array([518464, 562564, 84125, 291636, 0]),
+        "root_contributions": np.array(
+            [720.04444307, 750.04266545, 290.04310025, 540.0333323, 0]
+        ),
+    }
+    assert isinstance(out_dict, dict)
+    assert len(out_dict) == 3
+    assert isinstance(out_dict["contributions"], np.ndarray)
+    assert isinstance(out_dict["root_contributions"], np.ndarray)
+
+    for key in expected_dict.keys():
+        assert_allclose(out_dict[key], expected_dict[key])
