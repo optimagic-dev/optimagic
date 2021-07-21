bfb8cef8fb277608f989db16356bd8aac5dc2d21
Klara Roehrl <klara.roehrl@gmail.com>
2021-07-21 18:29:12 +0200

    Add additional standard options.

--- estimagic/optimization/cyipopt_optimizers.py
+++ estimagic/optimization/cyipopt_optimizers.py
@@ -2,6 +2,8 @@
 import functools
 
 from estimagic.config import IS_CYIPOPT_INSTALLED
+from estimagic.optimization.algo_options import CONVERGENCE_RELATIVE_CRITERION_TOLERANCE
+from estimagic.optimization.algo_options import STOPPING_MAX_ITERATIONS
 from estimagic.optimization.scipy_optimizers import get_scipy_bounds
 from estimagic.optimization.scipy_optimizers import process_scipy_result
 
@@ -22,6 +24,9 @@ def ipopt(
     x,
     lower_bounds,
     upper_bounds,
+    *,
+    convergence_relative_criterion_tolerance=CONVERGENCE_RELATIVE_CRITERION_TOLERANCE,
+    stopping_max_iterations=STOPPING_MAX_ITERATIONS,
 ):
     """Minimize a scalar function using the Interior Point Optimizer.
 
@@ -29,6 +34,16 @@ def ipopt(
     :cite:`Waechter2005a`, :cite:`Waechter2005b`, :cite:`Nocedal2009`) relies on
     `cyipopt <https://cyipopt.readthedocs.io/en/latest/index.html>`_, a Python wrapper
     for the `Ipopt optimization package <https://coin-or.github.io/Ipopt/index.html>`_.
+
+    - convergence.relative_criterion_tolerance (float): The algorithm terminates
+      successfully, if the (scaled) non linear programming error becomes smaller than
+      this value.
+    - stopping.max_iterations (int):  If the maximum number of iterations is reached,
+      the optimization stops, but we do not count this as successful convergence. The
+      difference to ``max_criterion_evaluations`` is that one iteration might need
+      several criterion evaluations, for example in a line search or to determine if the
+      trust region radius has to be shrunk.
+
 
     """
     if not IS_CYIPOPT_INSTALLED:
@@ -40,14 +55,29 @@ def ipopt(
     algo_info = DEFAULT_ALGO_INFO.copy()
     algo_info["name"] = "ipopt"
 
+    gradient = functools.partial(
+        criterion_and_derivative, task="derivative", algorithm_info=algo_info
+    )
+
     func = functools.partial(
         criterion_and_derivative,
         task="criterion",
         algorithm_info=algo_info,
     )
 
+    options = {
+        "acceptable_iter": 0,  # disable the "acceptable" heuristic
+        "max_iter": stopping_max_iterations,
+    }
+
     raw_res = cyipopt.minimize_ipopt(
-        fun=func, x0=x, bounds=get_scipy_bounds(lower_bounds, upper_bounds)
+        fun=func,
+        x0=x,
+        bounds=get_scipy_bounds(lower_bounds, upper_bounds),
+        jac=gradient,
+        constraints=(),
+        tol=convergence_relative_criterion_tolerance,
+        options=options,
     )
 
     res = process_scipy_result(raw_res)
