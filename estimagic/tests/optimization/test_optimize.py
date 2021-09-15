9d5f53d5caeafee9d2920d64e9f2ac011bc999e8
Janos Gabler <janos.gabler@gmail.com>
2021-09-15 19:17:53 +0200

    Add warnings if old bounds names are used.

--- estimagic/tests/optimization/test_optimize.py
+++ estimagic/tests/optimization/test_optimize.py
@@ -1,6 +1,7 @@
 """Tests for (almost) algorithm independent properties of maximize and minimize."""
 import numpy as np
 import pandas as pd
+import pytest
 
 from estimagic.optimization.optimize import maximize
 
@@ -15,3 +16,18 @@ def test_sign_is_switched_back_after_maximization():
     )
 
     assert np.allclose(res["solution_criterion"], 1)
+
+
+def test_warnings_with_old_bounds_names():
+    base_params = pd.DataFrame()
+    base_params["value"] = [1, 2, 3]
+
+    for wrong_name in "lower", "upper":
+        params = base_params.copy()
+        params[wrong_name] = 0
+        with pytest.warns(UserWarning):
+            maximize(
+                lambda params: 1 - params["value"] @ params["value"],
+                params=params,
+                algorithm="scipy_lbfgsb",
+            )
