bbe76a47cc7b7cbc1a6e5e05565e2a1abc296aaf
Janos Gabler <janos.gabler@gmail.com>
2021-09-15 13:07:47 +0200

    Improve lollipop plot.

--- estimagic/visualization/lollipop_plot.py
+++ estimagic/visualization/lollipop_plot.py
@@ -12,6 +12,7 @@ def lollipop_plot(
     stripplot_kws=None,
     barplot_kws=None,
     style=("whitegrid"),
+    dodge=True,
 ):
     """Make a lollipop plot.
 
@@ -30,84 +31,82 @@ def lollipop_plot(
             via the barplot function. Most notably, "color" and "alpha". In contrast
             to seaborn, we allow for a "width" argument.
         style (str): A seaborn style.
+        dodge (bool): Wheter the lollipops for different datasets are plotted
+            with an offset or on top of each other.
 
     Returns:
         seaborn.PairGrid
 
     """
-    data = data.copy()
+    data, varnames = _harmonize_data(data)
+
     sns.set_style(style)
     pairgrid_kws = {} if pairgrid_kws is None else pairgrid_kws
     stripplot_kws = {} if stripplot_kws is None else stripplot_kws
     barplot_kws = {} if barplot_kws is None else barplot_kws
 
-    dot_color = get_colors("categorical", 1)[0]
-
-    col_names = _make_string_index(data.columns)
-    row_names = _make_string_index(data.index)
-    data.columns = col_names
-    data["__name__"] = row_names
-
-    n_rows = len(data)
+    colors = get_colors("categorical", len(data))
 
     # Make the PairGrid
     pairgrid_kws = {
-        "height": n_rows,
         "aspect": 0.5,
         **pairgrid_kws,
     }
 
     g = sns.PairGrid(
         data,
-        x_vars=col_names,
+        x_vars=varnames,
         y_vars=["__name__"],
+        hue="__hue__",
         **pairgrid_kws,
     )
 
     # Draw a dot plot using the stripplot function
-    stripplot_kws = {
-        "size": 10,
+    combined_stripplot_kws = {
+        "size": 8,
         "orient": "h",
         "jitter": False,
-        "color": dot_color,
-        "edgecolor": dot_color,
+        "palette": colors,
+        "edgecolor": "#0000ffff",
+        "dodge": dodge,
         **stripplot_kws,
     }
 
-    g.map(sns.stripplot, **stripplot_kws)
+    g.map(sns.stripplot, **combined_stripplot_kws)
 
     if plot_bar:
         # Draw lines to the plot using the barplot function
-        barplot_kws = {
-            "color": dot_color,
+        combined_barplot_kws = {
+            "palette": colors,
             "alpha": 0.5,
-            "width": 0.05,
+            "width": 0.1,
+            "dodge": dodge,
             **barplot_kws,
         }
-        bar_height = barplot_kws.pop("width")
-        g.map(sns.barplot, **barplot_kws)
+        bar_height = combined_barplot_kws.pop("width")
+        g.map(sns.barplot, **combined_barplot_kws)
 
-        # Adjust the width of the bars which seaborn.barplot does not allow
-        for ax in g.axes.flat:
-            for patch in ax.patches:
-                current_height = patch.get_height()
-                diff = current_height - bar_height
-                # we change the bar width
-                patch.set_height(bar_height)
-                # we recenter the bar
-                patch.set_y(patch.get_y() + diff * 0.5)
+    # Adjust the width of the bars which seaborn.barplot does not allow
+    for ax in g.axes.flat:
+        for patch in ax.patches:
+            current_height = patch.get_height()
+            diff = current_height - bar_height
+            # we change the bar width
+            patch.set_height(bar_height)
+            # we recenter the bar
+            patch.set_y(patch.get_y() + diff * 0.5)
 
     # Use the same x axis limits on all columns and add better labels
     if sharex:
-        lower_candidate = data[col_names].min().min()
-        upper_candidate = data[col_names].max().max()
+        lower_candidate = data[varnames].min().min()
+        upper_candidate = data[varnames].max().max()
         padding = (upper_candidate - lower_candidate) / 10
         lower = lower_candidate - padding
         upper = upper_candidate + padding
         g.set(xlim=(lower, upper), xlabel=None, ylabel=None)
 
     # Use semantically meaningful titles for the columns
-    for ax, title in zip(g.axes.flat, col_names):
+    for ax, title in zip(g.axes.flat, varnames):
 
         # Set a different title for each axes
         ax.set(title=title)
@@ -118,6 +117,26 @@ def lollipop_plot(
 
     sns.despine(left=False, bottom=False)
     return g
+
+
+def _harmonize_data(data):
+    if not isinstance(data, list):
+        data = [data]
+
+    to_concat = []
+    for i, df in enumerate(data):
+        df = df.copy()
+        df.columns = _make_string_index(df.columns)
+        df.index = _make_string_index(df.index)
+        df["__name__"] = df.index
+        df["__hue__"] = i
+        to_concat.append(df)
+
+    combined = pd.concat(to_concat)
+
+    varnames = [col for col in combined.columns if col not in ["__hue__", "__name__"]]
+
+    return combined, varnames
 
 
 def _make_string_index(ind):
