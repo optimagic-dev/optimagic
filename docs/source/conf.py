188e16f6eef00fa392fdd65eb166e8ba87cc52ea
Janos Gabler <janos.gabler@gmail.com>
2021-09-28 19:37:13 +0200

    Fix docs.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -20,7 +20,7 @@ import os
 import sys
 
 
-sys.path.insert(0, os.path.abspath("../.."))
+sys.path.insert(0, os.path.abspath("../../src/"))
 
 # Set variable so that todos are shown in local build
 on_rtd = os.environ.get("READTHEDOCS") == "True"
