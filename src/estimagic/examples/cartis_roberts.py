51c573a60976d482e53f2e400c766a102930b6b3
mariam petrosyan <>
2021-11-12 14:30:46 +0100

    add more medium scale tests

--- src/estimagic/examples/cartis_roberts.py
+++ src/estimagic/examples/cartis_roberts.py
@@ -11,8 +11,13 @@ Implementation is based either on sources cited in the SIF files or
 where available, on AMPL implementaions available here:
 - https://vanderbei.princeton.edu/ampl/nlmodels/cute/index.html
 
+
 """
+from functools import partial
+
 import numpy as np
+
+from estimagic.examples.more_wild import brown_almost_linear
 
 
 def argtrig(x):
@@ -59,22 +64,12 @@ def bdvalues(x):
     return fvec
 
 
-def get_start_points_bdvalues(n):
-    h = 1 / (n + 1)
-    x = np.zeros(n)
-    for i in range(n):
-        x[i] = (i + 1) * h * ((i + 1) * h - 1)
-    return x
-
-
-def bratu_2d(x):
+def bratu_2d(x, alpha):
     p = x.shape[0] + 2
     h = 1 / (p - 1)
-    c = h ** 2 * 4
-    xvec = np.concatenate([np.zeros((x.shape[0], 1)), x], axis=1)
-    xvec = np.concatenate([np.zeros((1, xvec.shape[1])), xvec])
-    xvec = np.concatenate([xvec, np.zeros((1, xvec.shape[1]))])
-    xvec = np.concatenate([xvec, np.zeros((xvec.shape[0], 1))], axis=1)
+    c = h ** 2 * alpha
+    xvec = np.zeros((x.shape[0] + 2, x.shape[1] + 2))
+    xvec[1 : x.shape[0] + 1, 1 : x.shape[1] + 1] = x
     fvec = np.zeros_like(x)
     for i in range(2, p):
         for j in range(2, p):
@@ -87,6 +82,44 @@ def bratu_2d(x):
                 - c * np.exp(xvec[i - 1, j - 1])
             )
     return fvec.flatten()
+
+
+def bratu_3d(x, alpha):
+    p = x.shape[0] + 2
+    h = 1 / (p - 1)
+    c = h ** 2 * alpha
+    xvec = np.zeros((x.shape[0] + 2, x.shape[1] + 2, x.shape[2] + 2))
+    xvec[1 : x.shape[0] + 1, 1 : x.shape[1] + 1, 1 : x.shape[2] + 1] = x
+    fvec = np.zeros_like(x)
+    for i in range(2, p):
+        for j in range(2, p):
+            for k in range(2, p):
+                fvec[i - 2, j - 2, k - 2] = (
+                    6 * xvec[i - 1, j - 1, k - 1]
+                    - xvec[i, j - 1, k - 1]
+                    - xvec[i - 2, j - 1, k - 1]
+                    - xvec[i - 1, j, k - 1]
+                    - xvec[i - 1, j - 2, k - 1]
+                    - xvec[i - 1, j - 1, k]
+                    - xvec[i - 1, j - 1, k - 2]
+                    - c * np.exp(xvec[i, j, k])
+                )
+    return fvec.flatten()
+
+
+def broydn_3d(x):
+    xvec = np.zers(len(x) + 2)
+    xvec[1 : len(x) + 1] = x
+    fvec = (3 - 2 * xvec[1:-1]) * xvec[1:-1] - xvec[:-2] - 2 * xvec[2:] + 1
+    return fvec
+
+
+def get_start_points_bdvalues(n):
+    h = 1 / (n + 1)
+    x = np.zeros(n)
+    for i in range(n):
+        x[i] = (i + 1) * h * ((i + 1) * h - 1)
+    return x
 
 
 CARTIS_ROBERTS_PROBLEMS = {
@@ -119,10 +152,31 @@ CARTIS_ROBERTS_PROBLEMS = {
         "solution_criterion": 0,
     },
     "bratu_2d": {
-        "criterion": bratu_2d,
+        "criterion": partial(bratu_2d, alpha=4),
         "start_x": np.zeros((8, 8)),
         "solution_x": None,
         "start_criterion": 0.1560738,
+        "solution_criterion": 0,
+    },
+    "bratu_2d_t": {
+        "criterion": partial(bratu_2d, alpha=6.80812),
+        "start_x": np.zeros((8, 8)),
+        "solution_x": None,
+        "start_criterion": 0.4521311,
+        "solution_criterion": 1.853474e-5,
+    },
+    "bratu_3d": {
+        "criterion": partial(bratu_3d, alpha=6.80812),
+        "start_x": np.zeros((3, 3, 3)),
+        "solution_x": None,
+        "start_criterion": 4.888529,
+        "solution_criterion": 0,
+    },
+    "brownale": {
+        "criterion": brown_almost_linear,
+        "start_x": 0.5 * np.ones(100),
+        "solution_x": None,
+        "start_criterion": 2.524757e5,
         "solution_criterion": 0,
     },
 }
