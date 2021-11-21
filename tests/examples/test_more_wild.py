b227172f994770168ce8e73fad3e2169f3c888d1
Janos Gabler <janos.gabler@gmail.com>
2021-11-21 15:39:16 +0100

    Add info on optimal parameters for more_wild problems and improve precision of optimal criterion.

--- tests/examples/test_more_wild.py
+++ tests/examples/test_more_wild.py
@@ -13,6 +13,13 @@ def test_more_wild_function_at_start_x(name, specification):
     expected = specification["start_criterion"]
     assert np.allclose(calculated, expected)
 
+    if specification.get("solution_x") is not None:
+        _x = specification["solution_x"]
+        _contributions = _criterion(_x)
+        calculated = _contributions @ _contributions
+        expected = specification["solution_criterion"]
+        assert np.allclose(calculated, expected, rtol=1e-8, atol=1e-8)
+
 
 def test_get_start_points_mancino():
     expected = (np.array([102.4824, 96.3335, 90.4363, 84.7852, 79.3747]),)
