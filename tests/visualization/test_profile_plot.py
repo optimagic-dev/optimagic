8c8e1ccaf50c4c201e7f2a2d292114d760fcfaf3
Klara Roehrl <klara.roehrl@gmail.com>
2021-11-21 12:54:45 +0100

    Test creation_solution_times with n_evaluations.

--- tests/visualization/test_profile_plot.py
+++ tests/visualization/test_profile_plot.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import pytest
+from estimagic.visualization.profile_plot import _create_solution_times
 from estimagic.visualization.profile_plot import _determine_alpha_grid
 from estimagic.visualization.profile_plot import _find_switch_points
 
@@ -24,3 +25,41 @@ def test_determine_alpha_grid(performance_ratios):
     res = _determine_alpha_grid(performance_ratios)
     expected = np.array([1.0 + 1e-10, 1.25, 1.5, 2.75, 4.0, 4.0 * 1.025, 4.0 * 1.05])
     np.testing.assert_array_almost_equal(res, expected)
+
+
+def test_create_solution_times():
+    df = pd.DataFrame(
+        columns=["problem", "algorithm", "n_evaluations"],
+        data=[
+            ["prob1", "algo1", 0],
+            ["prob1", "algo1", 1],
+            #
+            ["prob1", "algo2", 2],
+            ["prob1", "algo2", 3],
+            #
+            ["prob2", "algo1", 5],
+            #
+            ["prob2", "algo2", 0],
+            ["prob2", "algo2", 1],
+        ],
+    )
+    info = pd.DataFrame(
+        {
+            "algo1": [True, True],
+            "algo2": [True, False],
+        },
+        index=["prob1", "prob2"],
+    )
+    expected = pd.DataFrame(
+        {
+            "algo1": [1, 5],
+            "algo2": [3, np.inf],
+        },
+        index=pd.Index(["prob1", "prob2"], name="problem"),
+    )
+    expected.columns.name = "algorithm"
+
+    res = _create_solution_times(
+        df=df, runtime_measure="n_evaluations", converged_info=info
+    )
+    pd.testing.assert_frame_equal(res, expected)
