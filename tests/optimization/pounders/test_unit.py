f3d2bdcef3a792fbe632bf2c45df2e0f83a80113
Sebastian Gsell <sebastian.gsell93@gmail.com>
2021-11-26 23:47:52 +0100

    Add unit tests for 'improve_model()'.

--- tests/optimization/pounders/test_unit.py
+++ tests/optimization/pounders/test_unit.py
@@ -1,9 +1,25 @@
+from functools import partial
+
 import numpy as np
 import pandas as pd
 import pytest
 from estimagic.config import TEST_FIXTURES_POUNDERS
 from estimagic.optimization.pounders_auxiliary import find_nearby_points
+from estimagic.optimization.pounders_auxiliary import improve_model
 from numpy.testing import assert_array_almost_equal as aaae
+
+
+@pytest.fixture
+def criterion():
+    data = pd.read_csv(TEST_FIXTURES_POUNDERS / "example_data.csv")
+    endog = np.asarray(data["y"])
+    exog = np.asarray(data["t"])
+
+    def func(x: np.ndarray, exog: np.ndarray, endog: np.ndarray) -> np.ndarray:
+        """User provided residual function."""
+        return endog - np.exp(-x[0] * exog) / (x[1] + x[2] * exog)
+
+    return partial(func, exog=exog, endog=endog)
 
 
 @pytest.mark.parametrize(
@@ -19,25 +35,66 @@ from numpy.testing import assert_array_almost_equal as aaae
     ],
 )
 def test_find_nearby_points(pickle_dict):
-    data = pd.read_pickle(pickle_dict)
-    n = 3
-    theta1 = 1e-5
+    dict_ = pd.read_pickle(pickle_dict)
 
     qmat_out, model_indices_out, mpoints_out, q_is_I_out = find_nearby_points(
-        xhist=data["xhist"],
-        xmin=data["xmin"],
-        qmat=data["qmat"],
-        q_is_I=data["q_is_I"],
-        delta=data["delta"],
-        c=data["c"],
-        model_indices=data["model_indices"],
-        mpoints=data["mpoints"],
-        nhist=data["nhist"],
-        theta1=theta1,
-        n=n,
+        xhist=dict_["xhist"],
+        xmin=dict_["xmin"],
+        qmat=dict_["qmat"],
+        q_is_I=dict_["q_is_I"],
+        delta=dict_["delta"],
+        c=dict_["c"],
+        model_indices=dict_["model_indices"],
+        mpoints=dict_["mpoints"],
+        nhist=dict_["nhist"],
+        theta1=1e-5,
+        n=3,
     )
 
-    aaae(qmat_out, data["qmat_expected"])
-    aaae(model_indices_out, data["model_indices_expected"])
-    assert np.allclose(mpoints_out, data["mpoints_expected"])
+    aaae(qmat_out, dict_["qmat_expected"])
+    aaae(model_indices_out, dict_["model_indices_expected"])
+    assert np.allclose(mpoints_out, dict_["mpoints_expected"])
     assert np.allclose(q_is_I_out, 0)
+
+
+@pytest.mark.parametrize(
+    "pickle_dict",
+    [
+        TEST_FIXTURES_POUNDERS / "improve_model_i.pkl",
+        TEST_FIXTURES_POUNDERS / "improve_model_ii.pkl",
+    ],
+)
+def test_improve_model(pickle_dict, criterion):
+    dict_ = pd.read_pickle(pickle_dict)
+
+    (
+        xhist_out,
+        fhist_out,
+        fnorm_out,
+        model_indices_out,
+        mpoints_out,
+        nhist_out,
+    ) = improve_model(
+        xhist=dict_["xhist"],
+        fhist=dict_["fhist"],
+        fnorm=dict_["fnorm"],
+        jac_res=dict_["jac_res"],
+        hess_res=dict_["hess_res"],
+        qmat=dict_["qmat"],
+        model_indices=dict_["model_indices"],
+        minindex=dict_["minindex"],
+        mpoints=dict_["mpoints"],
+        nhist=dict_["nhist"],
+        delta=dict_["delta"],
+        lower_bounds=None,
+        upper_bounds=None,
+        addallpoints=1,
+        n=3,
+        criterion=criterion,
+    )
+
+    aaae(xhist_out, dict_["xhist_expected"])
+    aaae(fhist_out, dict_["fhist_expected"])
+    aaae(model_indices_out, dict_["model_indices_expected"])
+    assert np.allclose(mpoints_out, dict_["mpoints_expected"])
+    assert np.allclose(nhist_out, dict_["nhist_expected"])
