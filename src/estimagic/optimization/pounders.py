Use copy.deepcopy() when updating *fmin*. This fixes a previous bug where the solution vector converge but not the gradient norm.

src/estimagic/optimization/pounders.py
@@ -10,6 +10,7 @@ from estimagic.optimization.pounders_auxiliary import get_params_quadratic_model
 from estimagic.optimization.pounders_auxiliary import get_residuals
 from estimagic.optimization.pounders_auxiliary import improve_model
 from estimagic.optimization.pounders_auxiliary import solve_subproblem
+from estimagic.optimization.pounders_auxiliary import update_center
 from estimagic.optimization.pounders_auxiliary import update_fdiff_and_hess
 
 
@@ -152,9 +153,9 @@ def internal_solve_pounders(
     n = x0.shape[0]  # number of model parameters
     maxinterp = 2 * n + 1  # max number of interpolation points
 
-    xhist = np.zeros((maxiter + 1, n))
-    fhist = np.zeros((maxiter + 1, nobs))
-    fnorm = np.zeros(maxiter + 1)
+    xhist = np.zeros((maxiter * 2, n))
+    fhist = np.zeros((maxiter * 2, nobs))
+    fnorm = np.zeros(maxiter * 2)
     hess = np.zeros((nobs, n, n))
     model_indices = np.zeros(maxinterp, dtype=int)
 
@@ -169,6 +170,8 @@ def internal_solve_pounders(
         if np.max(x0 + delta - upper_bounds) > 1e-10:
             raise ValueError("Starting points + delta > upper bounds.")
 
+    # This provides enough information to approximate the gradient of the objective
+    # using a forward difference scheme.
     xhist[0] = x0
     fhist[0, :] = criterion(x0)
     fnorm[0] = compute_fnorm(criterion_value=fhist[0, :])
@@ -189,8 +192,8 @@ def internal_solve_pounders(
             minnorm = fnorm[i + 1]
             minindex = i + 1
 
-    xmin = xhist[minindex, :]
-    fmin = fhist[minindex, :]
+    xmin = copy.deepcopy(xhist[minindex, :])
+    fmin = copy.deepcopy(fhist[minindex, :])
     fnorm_min = minnorm
 
     # centering around new trust-region and normalize to [-1, 1]
@@ -211,6 +214,8 @@ def internal_solve_pounders(
     nhist = n + 1
     mpoints = n + 1
 
+    last_model_indices = np.zeros(maxinterp, dtype=int)
+
     while reason is True:
         niter += 1
 
@@ -230,6 +235,7 @@ def internal_solve_pounders(
 
         qmin = -rslt.fun
         xplus = xmin + rslt.x * delta
+
         xhist[nhist, :] = xplus
         fhist[nhist, :] = criterion(xhist[nhist, :])
         fnorm[nhist] = compute_fnorm(criterion_value=fhist[nhist, :])
@@ -237,22 +243,21 @@ def internal_solve_pounders(
 
         nhist += 1
 
-        # Update the center
         if (rho >= eta1) or (rho > eta0 and valid is True):
-            # Update model to reflect new base point
-            x1 = (xplus - xmin) / delta
-
-            fdiff += np.dot(hess, x1).T
-            fmin += 0.5 * np.dot(np.dot(x1, hess), x1) + np.dot(x1, fdiff)
-
-            fnorm_min += np.dot(x1, jac_res) + 0.5 * np.dot(hess_res, x1)
-            jac_res += np.dot(hess_res, x1)
-
-            minindex = nhist - 1
-            minnorm = fnorm[minindex]
-
-            # Change current center
-            xmin = xhist[minindex, :]
+            xmin, fmin, fdiff, fnorm_min, minnorm, jac_res, minindex = update_center(
+                xplus=xplus,
+                xmin=xmin,
+                xhist=xhist,
+                delta=delta,
+                fmin=fmin,
+                fdiff=fdiff,
+                fnorm=fnorm,
+                fnorm_min=fnorm_min,
+                hess=hess,
+                jac_res=jac_res,
+                hess_res=hess_res,
+                nhist=nhist,
+            )
 
         # Evaluate at a model improving point if necessary
         # Note: valid is True in first iteration
@@ -407,7 +412,7 @@ def internal_solve_pounders(
             delta_old=delta_old,
         )
 
-        fmin = fhist[minindex]
+        fmin = copy.deepcopy(fhist[minindex])
         fnorm_min = fnorm[minindex]
         jac_res, hess_res = calc_jac_and_hess_res(fdiff=fdiff, fmin=fmin, hess=hess)
 
@@ -422,7 +427,6 @@ def internal_solve_pounders(
             reason = False
 
         # Test for repeated model
-        last_model_indices = np.zeros(maxinterp, dtype=int)
         if mpoints == last_mpoints:
             same = True
         else:

src/estimagic/optimization/pounders_auxiliary.py
@@ -1,3 +1,4 @@
+import copy
 from functools import partial
 
 import numpy as np
@@ -44,7 +45,7 @@ def update_center(
     minnorm = fnorm[minindex]
 
     # Change current center
-    xmin = xhist[minindex, :]
+    xmin = copy.deepcopy(xhist[minindex, :])
 
     return xmin, fmin, fdiff, fnorm_min, minnorm, jac_res, minindex
 
@@ -295,7 +296,7 @@ def improve_model(
     """
     minindex_internal = 0
     minvalue = np.inf
-    work = np.zeros(3, dtype=np.float64)
+    work = np.zeros(3)
 
     qtmp, _ = qr_multiply(qmat, np.eye(3), mode="right")
 
@@ -388,8 +389,8 @@ def add_more_points(
         - M (np.ndarray): M matrix. Shape(*maxinterp*, *n* (*n* + 1) / 2).
         - mpoints (int): Current number of model points.
     """
-    M = np.zeros((maxinterp, n + 1), dtype=np.float64)
-    N = np.zeros((maxinterp, int(n * (n + 1) / 2)), dtype=np.float64)
+    M = np.zeros((maxinterp, n + 1))
+    N = np.zeros((maxinterp, int(n * (n + 1) / 2)))
     M[:, 0] = 1
 
     for i in range(n + 1):
@@ -509,8 +510,8 @@ def get_params_quadratic_model(
         - hess_quadratic (np.ndarray): Hessian of the quadratic model.
             Shape (*nobs*, *n*, *n*).
     """
-    jac_quadratic = np.zeros((nobs, n), dtype=np.float64)
-    hess_quadratic = np.zeros((nobs, n, n), dtype=np.float64)
+    jac_quadratic = np.zeros((nobs, n))
+    hess_quadratic = np.zeros((nobs, n, n))
 
     if mpoints == (n + 1):
         omega = np.zeros(n)
@@ -520,7 +521,6 @@ def get_params_quadratic_model(
 
     for k in range(nobs):
         if mpoints != (n + 1):
-            # Solve L'*L*Omega = Z' * RES_k
             omega = np.dot(Z[:mpoints, :].T, res[:mpoints, k])
             omega = np.linalg.solve(np.atleast_2d(L_tmp), np.atleast_1d(omega))
 
@@ -584,7 +584,7 @@ def _evaluate_phi(x):
         (np.ndarray): Phi vector. Shape (*n* (*n* + 1) / 2,)
     """
     n = x.shape[0]
-    phi = np.zeros(int(n * (n + 1) / 2), dtype=np.float64)
+    phi = np.zeros(int(n * (n + 1) / 2))
 
     j = 0
     for i in range(n):
@@ -654,7 +654,7 @@ def _add_point(
         - mpoints (int): Current number of model points.
         - nhist (int): Current number candidate solutions for x.
     """
-    # Create new vector in history: X[newidx] = X[index] + delta * X[index]
+    # Create new vector in history
     xhist[nhist] = qtmp[:, index]
     xhist[nhist] = delta * xhist[nhist] + xhist[minindex]
 