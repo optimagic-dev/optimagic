36b476606e4a6b849399efb913078dc3b2fc2a49
Sebastian Gsell <sebastian.gsell93@gmail.com>
2022-01-12 13:43:34 +0100

    Add back unit tests.

--- tests/optimization/test_pounders_unit.py
+++ tests/optimization/test_pounders_unit.py
@@ -28,29 +28,95 @@ def criterion():
     return partial(func, exog=exog, endog=endog)
 
 
-@pytest.fixture(
-    params=[
-        "model_improving_points_zero_i",
-        "model_improving_points_zero_ii",
-        "model_improving_points_zero_iii",
-        "model_improving_points_zero_iv",
-        "model_improving_points_nonzero_i",
-        "model_improving_points_nonzero_ii",
-        "model_improving_points_nonzero_iii",
-    ]
-)
-def dict_find_affine_points(request):
-    return pd.read_pickle(TEST_FIXTURES_DIR / f"find_affine_points_{request.param}.pkl")
-
-
 @pytest.fixture(params=["4", "7"])
-def dict_interpolate_f(request):
-    return pd.read_pickle(TEST_FIXTURES_DIR / f"interpolate_f_iter_{request.param}.pkl")
+def dicts_interpolate_f(request):
+    dict_ = pd.read_pickle(
+        TEST_FIXTURES_DIR / f"get_approximation_error_iter_{request.param}.pkl"
+    )
+
+    history = LeastSquaresHistory()
+    history.add_entries(
+        dict_["history_x"],
+        dict_["history_criterion"],
+    )
+
+    n = 3
+    n_obs = 214
+    n_maxinterp = 2 * n + 1
+    x_accepted = dict_["min_x"]
+    model_indices = dict_["model_indices"]
+    n_modelpoints = dict_["n_modelpoints"]
+    delta_old = dict_["delta_old"]
+
+    center_info = {"x": x_accepted, "radius": delta_old}
+    interpolation_set = history.get_centered_xs(
+        center_info, index=model_indices[:n_modelpoints]
+    )
+
+    residual_model = {
+        "intercepts": dict_["min_criterion"],
+        "linear_terms": dict_["gradient"],
+        "square_terms": dict_["hessian"],
+    }
+
+    inputs_dict = {
+        "history": history,
+        "interpolation_set": interpolation_set,
+        "residual_model": residual_model,
+        "model_indices": model_indices,
+        "n_modelpoints": n_modelpoints,
+        "n_obs": n_obs,
+        "n_maxinterp": n_maxinterp,
+    }
+
+    expected_dict = {
+        "interpolation_set_expected": dict_["xk"],
+        "f_interpolated_expected": dict_["approximation_error_expected"],
+    }
+
+    return inputs_dict, expected_dict
 
 
 @pytest.fixture
-def dict_add_more_points():
-    return pd.read_pickle(TEST_FIXTURES_DIR / "add_more_points.pkl")
+def dicts_add_more_points():
+    dict_ = pd.read_pickle(TEST_FIXTURES_DIR / "add_more_points.pkl")
+
+    history = LeastSquaresHistory()
+    history.add_entries(
+        dict_["history_x"],
+        np.zeros(dict_["history_x"].shape),
+    )
+
+    n = 3
+    n_maxinterp = 2 * n + 1
+    x_accepted = dict_["min_x"]
+    model_indices = dict_["model_indices"]
+    delta = dict_["delta"]
+    n_modelpoints = dict_["n_modelpoints"]
+    c2 = 10
+    theta2 = 1e-4
+
+    inputs_dict = {
+        "history": history,
+        "x_accepted": x_accepted,
+        "model_indices": model_indices,
+        "delta": delta,
+        "c2": c2,
+        "theta2": theta2,
+        "n": n,
+        "n_maxinterp": n_maxinterp,
+        "n_modelpoints": n_modelpoints,
+    }
+
+    expected_dict = {
+        "lower_triangular_expected": dict_["lower_triangular_expected"],
+        "basis_null_space_expected": dict_["basis_null_space_expected"],
+        "monomial_basis_expected": dict_["monomial_basis_expected"],
+        "x_sample_monomial_basis_expected": dict_["interpolation_set_expected"],
+        "n_modelpoints_expected": dict_["n_modelpoints_expected"],
+    }
+
+    return inputs_dict, expected_dict
 
 
 @pytest.fixture
@@ -112,35 +178,75 @@ def dicts_improve_model(request, criterion):
     return inputs_dict, expected_dict
 
 
-@pytest.mark.skip(reason="refactoring")
-def test_find_affine_points(dict_find_affine_points):
+@pytest.fixture(
+    params=[
+        "model_improving_points_zero_i",
+        "model_improving_points_zero_ii",
+        "model_improving_points_zero_iii",
+        "model_improving_points_zero_iv",
+        "model_improving_points_nonzero_i",
+        "model_improving_points_nonzero_ii",
+        "model_improving_points_nonzero_iii",
+    ]
+)
+def dicts_find_affine_points(request):
+    history = LeastSquaresHistory()
+    dict_ = pd.read_pickle(
+        TEST_FIXTURES_DIR / f"find_affine_points_{request.param}.pkl"
+    )
+
+    history.add_entries(
+        dict_["history_x"],
+        np.zeros(dict_["history_x"].shape),
+    )
+    x_accepted = dict_["min_x"]
+    model_improving_points = dict_["model_improving_points"]
+    project_x_onto_null = dict_["project_x_onto_null"]
+    delta = dict_["delta"]
+    c = dict_["c"]
+    model_indices = dict_["model_indices"]
+    n_modelpoints = dict_["n_modelpoints"]
+    theta1 = 1e-5
+    n = 3
+
+    inputs_dict = {
+        "history": history,
+        "x_accepted": x_accepted,
+        "model_improving_points": model_improving_points,
+        "project_x_onto_null": project_x_onto_null,
+        "delta": delta,
+        "theta1": theta1,
+        "c": c,
+        "model_indices": model_indices,
+        "n": n,
+        "n_modelpoints": n_modelpoints,
+    }
+
+    expected_dict = {
+        "model_improving_points_expected": dict_["model_improving_points_expected"],
+        "model_indices_expected": dict_["model_indices_expected"],
+        "n_modelpoints_expected": dict_["n_modelpoints_expected"],
+    }
+
+    return inputs_dict, expected_dict
+
+
+def test_find_affine_points(dicts_find_affine_points):
+    inputs_dict, expected_dict = dicts_find_affine_points
+
     (
         model_improving_points_out,
         model_indices_out,
         n_modelpoints_out,
         project_x_onto_null_out,
-    ) = find_affine_points(
-        history_x=dict_find_affine_points["history_x"],
-        min_x=dict_find_affine_points["min_x"],
-        model_improving_points=dict_find_affine_points["model_improving_points"],
-        project_x_onto_null=dict_find_affine_points["project_x_onto_null"],
-        delta=dict_find_affine_points["delta"],
-        c=dict_find_affine_points["c"],
-        model_indices=dict_find_affine_points["model_indices"],
-        n_modelpoints=dict_find_affine_points["n_modelpoints"],
-        n_history=dict_find_affine_points["n_history"],
-        theta1=1e-5,
-        n=3,
-    )
+    ) = find_affine_points(**inputs_dict)
 
     aaae(
         model_improving_points_out,
-        dict_find_affine_points["model_improving_points_expected"],
+        expected_dict["model_improving_points_expected"],
     )
-    aaae(model_indices_out, dict_find_affine_points["model_indices_expected"])
-    assert np.allclose(
-        n_modelpoints_out, dict_find_affine_points["n_modelpoints_expected"]
-    )
+    aaae(model_indices_out, expected_dict["model_indices_expected"])
+    assert np.allclose(n_modelpoints_out, expected_dict["n_modelpoints_expected"])
     assert np.allclose(project_x_onto_null_out, True)
 
 
@@ -152,76 +258,35 @@ def test_improve_model(dicts_improve_model):
     )
 
     aaae(model_indices_out, expected_dict["model_indices_expected"])
-    for index_x_added in range(inputs_dict["n"] - inputs_dict["n_modelpoints"], 0, -1):
+    for index_added in range(inputs_dict["n"] - inputs_dict["n_modelpoints"], 0, -1):
         aaae(
-            history_out.get_xs(index=-index_x_added),
-            expected_dict["history_x_expected"][-index_x_added],
+            history_out.get_xs(index=-index_added),
+            expected_dict["history_x_expected"][-index_added],
         )
 
 
-@pytest.mark.skip(reason="refactoring")
-def test_add_more_points(dict_add_more_points):
-    n = 3
-    n_maxinterp = 2 * n + 1
-
+def test_add_more_points(dicts_add_more_points):
+    inputs_dict, expected_dict = dicts_add_more_points
     (
         lower_triangular,
         basis_null_space,
         monomial_basis,
         x_sample_monomial_basis,
         n_modelpoints,
-    ) = add_more_points(
-        history_x=dict_add_more_points["history_x"],
-        min_x=dict_add_more_points["min_x"],
-        model_indices=dict_add_more_points["model_indices"],
-        index_min_x=dict_add_more_points["index_min_x"],
-        delta=dict_add_more_points["delta"],
-        n_modelpoints=dict_add_more_points["n_modelpoints"],
-        n_history=dict_add_more_points["n_history"],
-        c2=10,
-        theta2=1e-4,
-        n=n,
-        n_maxinterp=n_maxinterp,
-    )
+    ) = add_more_points(**inputs_dict)
 
-    aaae(lower_triangular, dict_add_more_points["lower_triangular_expected"])
-    aaae(basis_null_space, dict_add_more_points["basis_null_space_expected"])
-    aaae(monomial_basis, dict_add_more_points["monomial_basis_expected"])
-    aaae(x_sample_monomial_basis, dict_add_more_points["interpolation_set_expected"])
-    assert np.allclose(n_modelpoints, dict_add_more_points["n_modelpoints_expected"])
+    aaae(lower_triangular, expected_dict["lower_triangular_expected"])
+    aaae(basis_null_space, expected_dict["basis_null_space_expected"])
+    aaae(monomial_basis, expected_dict["monomial_basis_expected"])
+    aaae(x_sample_monomial_basis, expected_dict["x_sample_monomial_basis_expected"])
+    assert np.allclose(n_modelpoints, expected_dict["n_modelpoints_expected"])
 
 
-@pytest.mark.skip(reason="refactoring")
-def test_interpolate_f(dict_interpolate_f):
-    history_x = dict_interpolate_f["history_x"]
-    min_x = dict_interpolate_f["min_x"]
-    model_indices = dict_interpolate_f["model_indices"]
-    n_modelpoints = dict_interpolate_f["n_modelpoints"]
-    delta_old = dict_interpolate_f["delta_old"]
+def test_interpolate_f(dicts_interpolate_f):
+    inputs_dict, expected_dict = dicts_interpolate_f
+    f_interpolated = interpolate_f(**inputs_dict)
 
-    n = 3
-    n_obs = 214
-    n_maxinterp = 2 * n + 1
-
-    xk = (history_x[model_indices[:n_modelpoints]] - min_x) / delta_old
-
-    approximiation_error = interpolate_f(
-        xk=xk,
-        hessian=dict_interpolate_f["hessian"],
-        history_criterion=dict_interpolate_f["history_criterion"],
-        min_criterion=dict_interpolate_f["min_criterion"],
-        gradient=dict_interpolate_f["gradient"],
-        model_indices=model_indices,
-        n_modelpoints=n_modelpoints,
-        n_obs=n_obs,
-        n_maxinterp=n_maxinterp,
-    )
-
-    aaae(xk, dict_interpolate_f["xk"])
-    aaae(
-        approximiation_error,
-        dict_interpolate_f["f_interpolated_expected"],
-    )
+    aaae(f_interpolated, expected_dict["f_interpolated_expected"])
 
 
 def test_get_coefficients_residual_model(dict_get_coefficients_residual_model):
@@ -258,7 +323,7 @@ def test_update_residual_model(dict_update_residual_model):
         "square_terms": dict_update_residual_model["params_hessian"],
     }
 
-    residual_model = update_residual_model(
+    residual_model_out = update_residual_model(
         residual_model=residual_model,
         coefficients_to_add=coefficients_to_add,
         delta=dict_update_residual_model["delta"],
@@ -266,11 +331,11 @@ def test_update_residual_model(dict_update_residual_model):
     )
 
     aaae(
-        residual_model["linear_terms"],
+        residual_model_out["linear_terms"],
         dict_update_residual_model["gradient_expected"],
     )
     aaae(
-        residual_model["square_terms"],
+        residual_model_out["square_terms"],
         dict_update_residual_model["hessian_expected"],
     )
 
