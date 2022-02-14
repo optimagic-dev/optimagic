35bb88365c748e351d09fe84ab9cac545bc093fe
luward <luis.wardenbach@uni-bonn.de>
2022-02-14 18:29:49 +0100

    Added 6 new functions.

--- src/estimagic/benchmarking/scalar_functions.py
+++ src/estimagic/benchmarking/scalar_functions.py
@@ -1,4 +1,4 @@
-"""Define the More-Wild Benchmark Set.
+"""Define the Scalar Benchmark Set.
 
 This benchmark set contains 78+ test cases for single objective optimization.
 The test cases are built out of 78 functions, which originate from the python
@@ -12,25 +12,56 @@ benchmark set:
 - https://github.com/AxelThevenot/Python_Benchmark_Test_Optimization_Function_Single_Objective
 """
 import numpy as np
+from estimagic.benchmarking import more_wild as mw
 
 
-def ackley(x, a=20, b=0.2, c=2 * np.pi):
-    d = x.shape[0]
-    assert (d is None) or (
-        isinstance(d, int) and (not d < 0)
-    ), "The dimension d must be None or a positive integer"
-    res = -a * np.exp(-b * np.sqrt(np.mean(x ** 2)))
-    out = res - np.exp(np.mean(np.cos(c * x))) + a + np.exp(1)
+def ackley(x):
+    res = -20 * np.exp(-0.2 * np.sqrt(np.mean(x ** 2)))
+    out = res - np.exp(np.mean(np.cos(2 * np.pi * x))) + 20 + np.exp(1)
     return out
 
 
-def rosenbrock2(x, a=1, b=100):
-    d = x.shape[0]
-    assert (d is None) or (
-        isinstance(d, int) and (not d < 0)
-    ), "The dimension d must be None or a positive integer"
-    res = np.sum(np.abs(b * (x[1:] - x[:-1] ** 2) ** 2 + (a - x[:-1]) ** 2))
-    return res
+def ackley2(x):
+    x_1, x_2 = x
+    out = -200 * np.exp(-0.2 * np.sqrt(x_1 ** 2 + x_2 ** 2))
+    return out
+
+
+def ackley3(x):
+    x_1, x_2 = x
+    res = -200 * np.exp(-0.2 * np.sqrt(x_1 ** 2 + x_2 ** 2))
+    out = res + 5 * np.exp(np.cos(3 * x_1) + np.sin(3 * x_2))
+    return out
+
+
+def ackley4(x):
+    x_1, x_2 = x
+    out = np.sum(
+        np.exp(-0.2) * np.sqrt(x_1 ** 2 + x_2 ** 2)
+        + 3 * (np.cos(2 * x_1) + np.sin(2 * x_2))
+    )
+    return out
+
+
+def adjiman(x):
+    x_1, x_2 = x
+    out = np.cos(x_1) * np.sin(x_2) - x_1 / (x_2 ** 2 + 1)
+    return out
+
+
+def alpine1(x):
+    out = np.sum(np.abs(x * np.sin(x) + 0.1 * x))
+    return out
+
+
+def alpine2(x):
+    out = -np.prod(np.sqrt(x) * np.sin(x))
+    return out
+
+
+def rosenbrock(x):
+    out = mw.rosenbrock(x) @ mw.rosenbrock(x)
+    return out
 
 
 SCALAR_FUNCTION_PROBLEMS = {
@@ -43,13 +74,92 @@ SCALAR_FUNCTION_PROBLEMS = {
     },
     "ackley_bad_start": {
         "criterion": ackley,
-        "start_x": np.full(10, 3) * 10,
+        "start_x": np.full(10, 30),
         "solution_x": np.zeros(10),
         "start_criterion": 19.950424956466673,
         "solution_criterion": 0,
     },
+    "ackley2_good_start": {
+        "criterion": ackley2,
+        "start_x": np.full(2, 3),
+        "solution_x": np.zeros(2),
+        "start_criterion": -85.60889823804698,
+        "solution_criterion": -200,
+    },
+    "ackley2_bad_start": {
+        "criterion": ackley2,
+        "start_x": np.full(2, 25),
+        "solution_x": np.zeros(2),
+        "start_criterion": -0.1698651409438339,
+        "solution_criterion": -200,
+    },
+    "ackley3_good_start": {
+        "criterion": ackley3,
+        "start_x": np.full(2, 3),
+        # no unique solution
+        "solution_x": None,
+        "start_criterion": -82.57324651934985,
+        "solution_criterion": -170.07756299785044,
+    },
+    "ackley3_bad_start": {
+        "criterion": ackley3,
+        "start_x": np.full(2, 25),
+        # no unique solution
+        "solution_x": None,
+        "start_criterion": 8.358584120180984,
+        "solution_criterion": -170.07756299785044,
+    },
+    "ackley4_good_start": {
+        "criterion": ackley4,
+        "start_x": np.full(2, 3),
+        "solution_x": np.array([-1.51, -0.755]),
+        "start_criterion": 4.5901006651507235,
+        "solution_criterion": -4.5901006651507235,
+    },
+    "ackley4_bad_start": {
+        "criterion": ackley4,
+        "start_x": np.full(2, 25),
+        "solution_x": np.array([-1.51, -0.755]),
+        "start_criterion": 31.054276897735043,
+        "solution_criterion": -4.5901006651507235,
+    },
+    "adjiman": {
+        "criterion": adjiman,
+        "start_x": np.array([-1, 1]),
+        "solution_x": np.array([2, 0.10578]),
+        "start_criterion": 0.954648713412841,
+        "solution_criterion": -2.0218067833370204,
+    },
+    "alpine1_good_start": {
+        "criterion": alpine1,
+        "start_x": np.full(10, 2),
+        "solution_x": np.zeros(10),
+        "start_criterion": 20.18594853651364,
+        "solution_criterion": 0,
+    },
+    "alpine1_bad_start": {
+        "criterion": alpine1,
+        "start_x": np.full(10, 10),
+        "solution_x": np.zeros(10),
+        "start_criterion": 44.40211108893698,
+        "solution_criterion": 0,
+    },
+    "alpine2_good_start": {
+        "criterion": alpine2,
+        "start_x": np.full(10, 9),
+        "solution_x": np.full(10, 7.917),
+        "start_criterion": -8.345137486473694,
+        "solution_criterion": -30491.15748225926,
+    },
+    "alpine2_bad_start": {
+        "criterion": alpine2,
+        "start_x": np.ones(10),
+        "solution_x": np.full(10, 7.917),
+        "start_criterion": -0.177988299732403,
+        "solution_criterion": -30491.15748225926,
+    },
     "rosenbrock_good_start": {
-        "criterion": rosenbrock2,
+        "criterion": rosenbrock,
         "start_x": np.array([-1.2, 1]),
         "solution_x": np.ones(2),
         "start_criterion": 24.2,
