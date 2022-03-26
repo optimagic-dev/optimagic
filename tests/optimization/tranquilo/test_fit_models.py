96680b678dba87694403b511e980a2da4b99df69
timmens <tim@mensinger96.com>
2022-03-26 16:20:12 +0100

    Update test_fit_models.py

--- tests/optimization/tranquilo/test_fit_models.py
+++ tests/optimization/tranquilo/test_fit_models.py
@@ -45,7 +45,6 @@ def quadratic_case():
     square_terms = np.array(
         [[-1, 0, 0, 0], [20, -2, 0, 0], [35, 25, -3, 0], [45, 40, 30, -4]]
     )
-    square_terms += square_terms.T
 
     # random data
     x = np.array([x0 + np.random.uniform(-0.01 * x0, 0.01 * x0) for _ in range(10_000)])
@@ -65,8 +64,8 @@ def quadratic_case():
 def test_fit_ols_against_truth(quadratic_case):
     fit_ols = get_fitter("ols")
     got = fit_ols(quadratic_case["x"], quadratic_case["y"])
-    aaae(got["linear_terms"].squeeze(), quadratic_case["linear_terms_expected"])
-    aaae(got["square_terms"].squeeze(), quadratic_case["square_terms_expected"])
+    aaae(got.linear_terms.squeeze(), quadratic_case["linear_terms_expected"])
+    aaae(got.square_terms.squeeze(), quadratic_case["square_terms_expected"])
 
 
 @pytest.mark.parametrize("model", ["ols", "ridge"])
@@ -74,8 +73,9 @@ def test_fit_ols_against_gradient(model, quadratic_case):
     fit_ols = get_fitter(model, {"l2_penalty_square": 0})
     got = fit_ols(quadratic_case["x"], quadratic_case["y"])
 
-    a = got["linear_terms"].squeeze()
-    grad = a + got["square_terms"].squeeze() @ quadratic_case["x0"]
+    a = got.linear_terms.squeeze()
+    hess = got.square_terms.squeeze() + got.square_terms.squeeze().T
+    grad = a + hess @ quadratic_case["x0"]
 
     gradient = first_derivative(quadratic_case["func"], quadratic_case["x0"])
     aaae(gradient["derivative"], grad, case="gradient")
@@ -89,4 +89,5 @@ def test_fit_ols_against_hessian(model, options, quadratic_case):
     fit_ols = get_fitter(model, options)
     got = fit_ols(quadratic_case["x"], quadratic_case["y"])
     hessian = second_derivative(quadratic_case["func"], quadratic_case["x0"])
-    aaae(hessian["derivative"], got["square_terms"].squeeze(), case="hessian")
+    hess = got.square_terms.squeeze() + got.square_terms.squeeze().T
+    aaae(hessian["derivative"], hess, case="hessian")
