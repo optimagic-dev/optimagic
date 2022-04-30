38e793cc2b57438b2bd16be070fc5128a200b5b5
timmens <tim@mensinger96.com>
2022-04-30 15:47:47 +0200

    Adjust lhs_sampling

--- tests/optimization/tranquilo/test_lhs_sampling.py
+++ tests/optimization/tranquilo/test_lhs_sampling.py
@@ -1,45 +1,47 @@
-from itertools import product
-
 import numpy as np
 import pytest
-from estimagic.optimization.tranquilo.lhs_sampling import _create_upscaled_lhs_sample
-from estimagic.optimization.tranquilo.lhs_sampling import _extend_upscaled_lhs_sample
+from estimagic.optimization.tranquilo.lhs_sampling import _create_upscaled_sample
+from estimagic.optimization.tranquilo.lhs_sampling import _extend_upscaled_sample
 from estimagic.optimization.tranquilo.lhs_sampling import _get_empty_bin_info
 from estimagic.optimization.tranquilo.lhs_sampling import _scale_down_points
 from estimagic.optimization.tranquilo.lhs_sampling import _scale_up_points
-from estimagic.optimization.tranquilo.lhs_sampling import compute_optimality_criterion
-from estimagic.optimization.tranquilo.lhs_sampling import get_existing_points
-from estimagic.optimization.tranquilo.lhs_sampling import (
-    get_next_trust_region_points_latin_hypercube,
-)
+from estimagic.optimization.tranquilo.lhs_sampling import calculate_criterion
+from estimagic.optimization.tranquilo.lhs_sampling import lhs_sampler
 from numpy.testing import assert_array_almost_equal as aaae
 
 
 def test_scaling_bijection():
     params = {
+        "n_params": 20,
         "n_points": 100,
-        "n_dim": 20,
         "n_designs": 1,
+        "seed": 0,
+        "dtype": np.uint8,
     }
-    center = np.ones(params["n_dim"])
-    radius = 0.1
+    lower_bounds = np.ones(params["n_params"])
+    upper_bounds = 3 + lower_bounds
 
-    points = _create_upscaled_lhs_sample(**params)
+    points = _create_upscaled_sample(**params)
     points = np.squeeze(points)
-    downscaled = _scale_down_points(points, center, radius, params["n_points"])
-    upscaled = _scale_up_points(downscaled, center, radius, params["n_points"])
+    downscaled = _scale_down_points(
+        points, params["n_points"], lower_bounds, upper_bounds
+    )
+    upscaled = _scale_up_points(
+        downscaled, params["n_points"], lower_bounds, upper_bounds
+    )
 
     aaae(points, upscaled)
 
 
 def test_scale_down_points():
     points = np.array([[0, 1], [1, 2], [2, 0]])
-    n_points, n_dim = points.shape
+    n_points, n_params = points.shape
 
-    center = 0.5 * np.ones(n_dim)
-    radius = 0.1
+    # center [0.5, ..., 0.5] with radius 0.1
+    lower_bounds = 0.4 * np.ones(n_params)
+    upper_bounds = 0.6 * np.ones(n_params)
 
-    downscaled = _scale_down_points(points, center, radius, n_points)
+    downscaled = _scale_down_points(points, n_points, lower_bounds, upper_bounds)
     expected = np.array(
         [[0.4, 0.46666667], [0.46666667, 0.53333333], [0.53333333, 0.4]]
     )
@@ -57,114 +59,64 @@ def test_scale_up_points():
     )
     points = expected / 3
 
-    n_points, n_dim = points.shape
+    n_points, n_params = points.shape
 
-    center = 0.5 * np.ones(n_dim)
-    radius = 0.5
+    # center [0.5, ..., 0.5] with radius 0.5
+    lower_bounds = np.zeros(n_params)
+    upper_bounds = np.ones(n_params)
 
-    upscaled = _scale_up_points(points, center, radius, n_points)
+    upscaled = _scale_up_points(points, n_points, lower_bounds, upper_bounds)
     aaae(expected, upscaled)
 
 
-def test_get_existing_points():
-    old_sample = np.array(
-        [
-            [0.1, 0.1],
-            [0.1, 0.2],
-            [0.2, 0.2],
-            [0.2, 0.3],
-        ]
-    )
-    new_center = 0.3 * np.ones(2)
-    new_radius = 0.1
-
-    expected = np.array(
-        [
-            [0.2, 0.2],
-            [0.2, 0.3],
-        ]
-    )
-
-    got = get_existing_points(old_sample, new_center, new_radius)
-    aaae(expected, got)
-
-
-def test_latin_hypercube_property():
+@pytest.mark.parametrize("n_params", [2, 5, 10, 50, 100])
+def test_latin_hypercube_property(n_params):
     """Check that for each single dimension the points are uniformly distributed."""
-    n_dim, n_points = np.random.randint(2, 100, size=2)
-    sample = _create_upscaled_lhs_sample(n_dim, n_points, n_designs=1)
+    n_points = 50
+    sample = _create_upscaled_sample(
+        n_params, n_points, n_designs=1, seed=0, dtype=np.uint8
+    )
     index = np.arange(n_points)
-    for j in range(n_dim):
+    for j in range(n_params):
         aaae(index, np.sort(sample[0][:, j]))
 
 
 @pytest.mark.parametrize(
-    "center, radius, n_points, optimality_criterion, lhs_design, target, n_iter",
-    product(
-        [np.ones(d) for d in (2, 5)],
-        [0.05, 0.1],
-        [10, 100],
-        ["a-optimal", "e-optimal", "d-optimal", "g-optimal", "maximin"],
-        ["centered", "random"],
-        ["linear", "quadratic"],
-        [1, 100],
-    ),
-)
-def test_get_next_trust_region_points_latin_hypercube_single_use(
-    center, radius, n_points, optimality_criterion, lhs_design, target, n_iter
-):
-    """Check that function can be called with all arguments and that the resulting
-    sample fulfills some basic Latin-Hypercube condition.
-
-    """
-    sample = get_next_trust_region_points_latin_hypercube(
-        center=center,
-        radius=radius,
-        n_points=n_points,
-        n_iter=n_iter,
-        lhs_design=lhs_design,
-        optimality_criterion=optimality_criterion,
-    )["points"]
-
-    decimal = 8 if lhs_design == "centered" else 2
-
-    assert sample.shape == (n_points, len(center))
-    aaae(sample.mean(axis=0), center, decimal=decimal)
-
-
-@pytest.mark.parametrize(
-    "optimality_criterion",
+    "criterion",
     ["a-optimal", "e-optimal", "d-optimal", "g-optimal", "maximin"],
 )
-def test_get_next_trust_region_points_latin_hypercube_optimality_criterion(
-    optimality_criterion,
+def test_optimality_criterion(
+    criterion,
 ):
     """Check that the optimal sample is actually optimal."""
-    sample = get_next_trust_region_points_latin_hypercube(
-        center=np.ones(5),
-        radius=0.1,
-        n_points=10,
+    sample = lhs_sampler(
+        lower_bounds=0.4 * np.ones(5),
+        upper_bounds=0.6 * np.ones(5),
+        target_size=10,
+        centered=True,
+        criterion=criterion,
+        target="linear",
         n_iter=1,
-        lhs_design="centered",
-        optimality_criterion=optimality_criterion,
-        target="linear",
+        return_crit_vals=True,
     )
 
-    optimized_sample = get_next_trust_region_points_latin_hypercube(
-        center=np.ones(5),
-        radius=0.1,
-        n_points=10,
-        n_iter=50_000,
-        lhs_design="centered",
-        optimality_criterion=optimality_criterion,
+    optimized_sample = lhs_sampler(
+        lower_bounds=0.4 * np.ones(5),
+        upper_bounds=0.6 * np.ones(5),
+        target_size=10,
+        centered=True,
+        criterion=criterion,
         target="linear",
-    )
-    crit_val_optimized_sample = compute_optimality_criterion(
-        optimized_sample["points"], optimality_criterion, target="linear"
+        n_iter=50_000,  # random search through 50_000 examples
+        return_crit_vals=True,
     )
 
-    if optimality_criterion != "maximin":
-        # by design of the criterion only few values are possible
+    crit_val_optimized_sample = calculate_criterion(
+        optimized_sample["points"], criterion, target="linear"
+    )
+
+    if criterion != "maximin":
+        # by design of the maximin criterion only few values are possible
         assert len(np.unique(optimized_sample["crit_vals"])) > 10_000
     assert sample["crit_vals"] > np.min(optimized_sample["crit_vals"])
     assert crit_val_optimized_sample == np.min(optimized_sample["crit_vals"])
@@ -178,20 +130,21 @@ def test_extend_upscaled_lhs_sample():
 
     """
     n_points = 2
-    second_center = 0.4 * np.ones(2)
-    second_radius = 0.1
+    lower_bounds = 0.3 * np.ones(2)
+    upper_bounds = 0.5 * np.ones(2)
 
     existing_points = np.array([[0.45, 0.45]])
 
     existing_scaled = _scale_up_points(
-        existing_points, second_center, second_radius, n_points
+        existing_points, n_points, lower_bounds, upper_bounds
     )
-    empty_bins = _get_empty_bin_info(existing_scaled, n_points)
-    new_points = _extend_upscaled_lhs_sample(
-        empty_bins, n_points, n_designs=1, dtype=np.uint8
-    )[0]
+    empty_bins = _get_empty_bin_info(existing_scaled, n_points, dtype=np.uint8)
+    new_points = _extend_upscaled_sample(
+        empty_bins, n_points, n_designs=1, seed=0, dtype=np.uint8
+    )
+    new_points = np.squeeze(new_points)
 
-    assert np.all(new_points == np.array([[0, 0]]))
+    assert np.all(new_points == np.array([[0, 0]], dtype=np.uint8))
 
 
 def test_get_empty_bin_info():
@@ -205,10 +158,12 @@ def test_get_empty_bin_info():
 
     empty_bins = []
     for existing_upscaled in existing_upscaled_list:
-        empty_bins.append(_get_empty_bin_info(existing_upscaled, n_points=3))
+        empty_bins.append(
+            _get_empty_bin_info(existing_upscaled, n_points=3, dtype=np.uint8)
+        )
 
     for empty_bin in empty_bins:
-        assert np.all(empty_bin == np.array([[2, 2]]))
+        assert np.all(empty_bin == np.array([[2, 2]], dtype=np.uint8))
 
 
 def test_get_empty_bin_info_multiple_points():
@@ -224,7 +179,7 @@ def test_get_empty_bin_info_multiple_points():
             [0.75, 0],
         ]
     )
-    expected = np.array([[1, 1], [2, 2]])
-    got = _get_empty_bin_info(existing_upscaled, n_points=3)
+    expected = np.array([[1, 1], [2, 2]], dtype=np.uint8)
+    got = _get_empty_bin_info(existing_upscaled, n_points=3, dtype=np.uint8)
 
     assert np.all(expected == got)
