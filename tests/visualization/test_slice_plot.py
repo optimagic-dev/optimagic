23720f0a2f67d01fc7a9f24dab28e4352be49474
Mariam <mpetrosyan@uni-bonn.de>
2022-05-27 13:36:50 +0200

    Update test_slice_plot

--- tests/visualization/test_slice_plot.py
+++ tests/visualization/test_slice_plot.py
@@ -3,7 +3,6 @@ import itertools
 import numpy as np
 import pandas as pd
 import pytest
-from estimagic.visualization.plotting_utilities import combine_plots
 from estimagic.visualization.slice_plot import slice_plot
 
 
@@ -25,21 +24,16 @@ def problem():
 
 
 @pytest.mark.parametrize(
-    "n_gridpoints, n_random_values, plots_per_row, combine_plots_in_grid",
-    itertools.product([21, 41], [2, 3], [1, 2], [True, False]),
+    "n_gridpoints, n_random_values, plots_per_row, return_dict",
+    itertools.product([21, 41], [2, 3], [1, 2], [False, True]),
 )
-def test_slice_plot(
-    problem, n_gridpoints, n_random_values, plots_per_row, combine_plots_in_grid
-):
+def test_slice_plot(problem, n_gridpoints, n_random_values, plots_per_row, return_dict):
 
-    plots_dict = slice_plot(
+    slice_plot(
         criterion=problem["criterion"],
         params=problem["params"],
         n_gridpoints=n_gridpoints,
         n_random_values=n_random_values,
+        return_dict=return_dict,
+        plots_per_row=plots_per_row,
     )
-    if combine_plots_in_grid:
-        combine_plots(
-            plots=list(plots_dict.values()),
-            plots_per_row=plots_per_row,
-        )
