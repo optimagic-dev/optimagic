Implement correct jacobian of constraints

src/estimagic/parameters/nonlinear_constraints.py
@@ -4,6 +4,7 @@ import numpy as np
 import pandas as pd
 from estimagic.differentiation.derivatives import first_derivative
 from estimagic.exceptions import InvalidFunctionError
+from estimagic.parameters.block_trees import block_tree_to_matrix
 from estimagic.parameters.tree_registry import get_registry
 from pybaum import tree_just_flatten
 from pybaum import tree_unflatten
@@ -14,6 +15,10 @@ CONVERGENCE_ABSOLUTE_CONSTRAINT_TOLERANCE = 1e-5
 considered 'feasible'.
 
 """
+
+# ======================================================================================
+# Process nonlinear constraints
+# ======================================================================================
 
 
 def process_nonlinear_constraints(
@@ -46,8 +51,10 @@ def process_nonlinear_constraints(
         list[dict]: List of processed constraints.
 
     """
-    for c in nonlinear_constraints:
-        _check_validity_nonlinear_constraint(c, params=params, skip_checks=skip_checks)
+    constraint_evals = [
+        _check_validity_and_return_evaluation(c, params, skip_checks)
+        for c in nonlinear_constraints
+    ]
 
     _processor = partial(
         _process_nonlinear_constraint,
@@ -56,22 +63,34 @@ def process_nonlinear_constraints(
         numdiff_options=numdiff_options,
     )
 
-    processed = [_processor(c) for c in nonlinear_constraints]
+    processed = [
+        _processor(c, e) for c, e in zip(nonlinear_constraints, constraint_evals)
+    ]
     return processed
 
 
-def _process_nonlinear_constraint(c, params, converter, numdiff_options):
+def _process_nonlinear_constraint(
+    c, constraint_eval, params, converter, numdiff_options
+):
     """Process a single nonlinear constraint."""
 
-    constraint_fun = c["func"]
+    # ==================================================================================
+    # Process selectors for params (functional) and flat_params (indices)
+    # ==================================================================================
+
+    external_selector = _process_selector(c)  # functional selector
+
+    selection_indices, n_params = _get_selection_indices(params, external_selector)
 
     # ==================================================================================
-    # Process selectors for external and internal params
+    # Evaluate constraint function and selector
     # ==================================================================================
-    external_selector = _process_selector(c)
-    internal_selector_mask = _get_internal_selector_index(
-        params, external_selector, converter
-    )
+    selected = external_selector(params)
+
+    constraint_func = c["func"]
+
+    if constraint_eval is None:
+        constraint_eval = constraint_func(selected)
 
     # ==================================================================================
     # Retrieve number of constraints
@@ -79,17 +98,17 @@ def _process_nonlinear_constraint(c, params, converter, numdiff_options):
     if "n_constr" in c:
         _n_constr = c["n_constr"]
     else:
-        constraint_value = constraint_fun(external_selector(params))
+        constraint_value = constraint_func(external_selector(params))
         _n_constr = len(np.atleast_1d(constraint_value))
 
     # ==================================================================================
     # Consolidate and transform jacobian
     # ==================================================================================
 
-    # Process numdiff_options
-    numdiff_options = numdiff_options.copy()
-    numdiff_options["lower_bounds"] = external_selector(numdiff_options["lower_bounds"])
-    numdiff_options["upper_bounds"] = external_selector(numdiff_options["upper_bounds"])
+    # process numdiff_options for numerical derivative
+    options = numdiff_options.copy()
+    options.pop("lower_bounds", None)
+    options.pop("upper_bounds", None)
 
     if "derivative" in c:
         if not callable(c["derivative"]):
@@ -100,20 +119,29 @@ def _process_nonlinear_constraint(c, params, converter, numdiff_options):
         # use finite-differences if no closed-form jacobian is defined
         def jacobian(p):
             return first_derivative(
-                constraint_fun,
+                constraint_func,
                 p,
-                **numdiff_options,
+                **options,
             )["derivative"]
 
     def _jacobian_from_internal(x):
+        """Return Jacobian of constraint at internal parameters.
+
+        The constraint function is written to be evaluated on a selection of the
+        external parameters. The optimizer, however, only works on internal parameters.
+        These can be significantly different from the external parameters, due to
+        estimagic's reparametrization features. In this function we compute the Jacobian
+        of the constraint at the internal parameters using information on the Jacobian
+        of the constraint at the selected external parameters.
+
+        """
         params = converter.params_from_internal(x)
         select = external_selector(params)
-        value = np.zeros((_n_constr, len(x)))
-        # here we could return a sparse jacobian
-        value[:, internal_selector_mask] = np.atleast_1d(jacobian(select)).reshape(
-            _n_constr, -1
-        )
-        return value
+        jac = jacobian(select)
+        jac_matrix = block_tree_to_matrix(jac, constraint_eval, selected)
+        jac_sparse = _extend_jacobian(jac_matrix, selection_indices, n_params)
+        jac_internal = converter.derivative_to_internal(jac_sparse, x, jac_is_flat=True)
+        return np.atleast_2d(jac_internal)
 
     # ==================================================================================
     # Transform constraint function and derive bounds
@@ -133,7 +161,7 @@ def _process_nonlinear_constraint(c, params, converter, numdiff_options):
         def constraint_from_internal(x):
             params = converter.params_from_internal(x)
             select = external_selector(params)
-            out = np.atleast_1d(constraint_fun(select)) - _value
+            out = np.atleast_1d(constraint_func(select)) - _value
             return out
 
         jacobian_from_internal = _jacobian_from_internal
@@ -152,7 +180,7 @@ def _process_nonlinear_constraint(c, params, converter, numdiff_options):
         def _constraint_from_internal(x):
             params = converter.params_from_internal(x)
             select = external_selector(params)
-            return np.atleast_1d(constraint_fun(select))
+            return np.atleast_1d(constraint_func(select))
 
         lower_bounds = c.get("lower_bounds", 0)
         upper_bounds = c.get("upper_bounds", np.inf)
@@ -178,6 +206,11 @@ def _process_nonlinear_constraint(c, params, converter, numdiff_options):
     }
 
     return internal_constr
+
+
+# ======================================================================================
+# Transform equality constraints to inequality constraints
+# ======================================================================================
 
 
 def equality_as_inequality_constraints(nonlinear_constraints):
@@ -211,8 +244,70 @@ def _equality_to_inequality(c):
     return out
 
 
+# ======================================================================================
+# Helper Functions
+# ======================================================================================
+
+
+def _process_selector(c):
+    if "selector" in c:
+        selector = c["selector"]
+    elif "loc" in c:
+
+        def selector(params):
+            return params.loc[c["loc"]]
+
+    elif "query" in c:
+
+        def selector(params):
+            return params.query(c["query"])
+
+    else:
+        selector = _identity
+    return selector
+
+
 def _compose_funcs(f, g):
     return lambda x: g(f(x))
+
+
+def _identity(x):
+    return x
+
+
+# ======================================================================================
+# Jacobian helper functions
+# ======================================================================================
+
+
+def _extend_jacobian(jac_mat, selection_indices, n_params):
+    """Extend Jacobian on selected parameters to full params.
+
+    Jacobian of constraints is defined on a selection of the parameters, however, we
+    need the Jacobian on the full params. Since the Jacobian is trivially zero at the
+    non-selected params we can simply fill a zero matrix.
+
+    """
+    jac_sparse = np.zeros((jac_mat.shape[0], n_params))
+    jac_sparse[:, selection_indices] = jac_mat
+    return jac_sparse
+
+
+def _get_selection_indices(params, selector):
+    """Get index of selected flat params and number of flat params."""
+    registry = get_registry(extended=True)
+    flat_params = tree_just_flatten(params, registry=registry)
+    n_params = len(flat_params)
+    flat_params_indices = np.arange(n_params, dtype=int)
+    params_indices = tree_unflatten(params, flat_params_indices, registry=registry)
+    selected = selector(params_indices)
+    indices = np.array(tree_just_flatten(selected, registry=registry), dtype=int)
+    return indices, n_params
+
+
+# ======================================================================================
+# Transformation helper functions
+# ======================================================================================
 
 
 def _get_transformation(lower_bounds, upper_bounds):
@@ -259,38 +354,19 @@ def _get_transformation_type(lower_bounds, upper_bounds):
     return _transformation_type
 
 
-def _process_selector(c):
-    if "selector" in c:
-        selector = c["selector"]
-    elif "loc" in c:
-
-        def selector(params):
-            return params.loc[c["loc"]]
-
-    elif "query" in c:
-
-        def selector(params):
-            return params.query(c["query"])
-
-    else:
-        selector = _identity
-    return selector
+# ======================================================================================
+# Checks
+# ======================================================================================
 
 
-def _get_internal_selector_index(params, selector, converter):
-    registry = get_registry(extended=True)
-    x = converter.params_to_internal(params)
-    flat_params_indices = np.arange(len(x))
-    params_indices = tree_unflatten(params, flat_params_indices, registry=registry)
-    index = tree_just_flatten(selector(params_indices), registry=registry)
-    return index
+def _check_validity_and_return_evaluation(c, params, skip_checks):
+    """Check that nonlinear constraints are valid.
 
+    Returns:
+        constaint_eval: Evaluation of constraint at params, if skip_checks if False,
+            else None.
 
-def _identity(x):
-    return x
-
-
-def _check_validity_nonlinear_constraint(c, params, skip_checks):
+    """
     # ==================================================================================
     # check functions
     # ==================================================================================
@@ -376,13 +452,17 @@ def _check_validity_nonlinear_constraint(c, params, skip_checks):
     # check that constraints can be evaluated
     # ==================================================================================
 
+    constraint_eval = None
+
     if not skip_checks:
 
         selector = _process_selector(c)
 
         try:
-            c["func"](selector(params))
+            constraint_eval = c["func"](selector(params))
         except Exception:
             raise InvalidFunctionError(
                 f"Error when evaluating function of constraint {c}."
             )
+
+    return constraint_eval

tests/optimization/test_with_nonlinear_constraints.py
@@ -125,7 +125,7 @@ def test_nonlinear_optimization(nlc_2d_example, algorithm, constr_type):
 
     """
     if "equality" in constr_type and algorithm == "nlopt_mma":
-        pytest.skip(msg="Very slow and low accuracy.")
+        pytest.skip(reason="Very slow and low accuracy.")
 
     solution_x, kwargs = nlc_2d_example
     if algorithm == "scipy_cobyla":
@@ -157,8 +157,8 @@ def criterion(params):
 
 @pytest.mark.parametrize("algorithm", NLC_ALGORITHMS)
 def test_documentation_example(algorithm):
-    if algorithm == "nlopt_mma":
-        pytest.skip(msg="Very slow and low accuracy.")
+    if algorithm in ("nlopt_mma", "ipopt"):
+        pytest.skip(reason="Slow.")
 
     kwargs = {
         "lower_bounds": np.zeros(6),
@@ -181,3 +181,68 @@ def test_documentation_example(algorithm):
         },
         **kwargs
     )
+
+
+# ======================================================================================
+# Test: selection + reparametrization constraint + nonlinear constraint
+# ======================================================================================
+
+
+@pytest.fixture()
+def general_example():
+
+    params = {"a": np.array([0.1, 0.3, 0.4, 0.2]), "b": np.array([1.5, 2])}
+
+    def criterion(params):
+        weights = np.array([0, 1, 2, 3])
+        _crit = params["a"] @ weights
+        _crit += params["b"].sum()
+        return _crit
+
+    def selector_probability_constraint(params):
+        return params["a"]
+
+    def selector_nonlinear_constraint(params):
+        return {"probs": params["a"][:3][::-1], "unnecessary": params["b"]}
+
+    def constraint(selected):
+        return selected["probs"] @ selected["probs"]
+
+    constraints = [
+        {"type": "probability", "selector": selector_probability_constraint},
+        {
+            "type": "nonlinear",
+            "selector": selector_nonlinear_constraint,
+            "upper_bounds": 0.8,
+            "func": constraint,
+            "tol": 0.01,
+        },
+    ]
+
+    lower_bounds = {"b": np.array([0, 0])}
+    upper_bounds = {"b": np.array([2, 2])}
+
+    kwargs = {
+        "criterion": criterion,
+        "params": params,
+        "constraints": constraints,
+        "lower_bounds": lower_bounds,
+        "upper_bounds": upper_bounds,
+    }
+    return kwargs
+
+
+@pytest.mark.parametrize("algorithm", ["ipopt"])
+def test_general_example(general_example, algorithm):
+
+    kwargs = general_example
+
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")
+        res = minimize(algorithm=algorithm, **kwargs)
+
+    optimal_p1 = 0.5 + np.sqrt(3 / 20)  # can be derived analytically
+    optimal_p2 = 1 - optimal_p1
+
+    aaae(res.params["a"], np.array([optimal_p1, optimal_p2, 0, 0]), decimal=4)
+    aaae(res.params["b"], np.array([0.0, 0]), decimal=5)

tests/parameters/test_nonlinear_constraints.py
@@ -5,9 +5,9 @@ import numpy as np
 import pandas as pd
 import pytest
 from estimagic.parameters.nonlinear_constraints import (
-    _check_validity_nonlinear_constraint,
+    _check_validity_and_return_evaluation,
 )
-from estimagic.parameters.nonlinear_constraints import _get_internal_selector_index
+from estimagic.parameters.nonlinear_constraints import _get_selection_indices
 from estimagic.parameters.nonlinear_constraints import _get_transformation
 from estimagic.parameters.nonlinear_constraints import _get_transformation_type
 from estimagic.parameters.nonlinear_constraints import _process_selector
@@ -15,6 +15,7 @@ from estimagic.parameters.nonlinear_constraints import (
     equality_as_inequality_constraints,
 )
 from estimagic.parameters.nonlinear_constraints import process_nonlinear_constraints
+from estimagic.parameters.tree_registry import get_registry
 from numpy.testing import assert_array_equal
 from pandas.testing import assert_frame_equal
 from pybaum import tree_just_flatten
@@ -26,7 +27,8 @@ class Converter:
         return x
 
     def params_to_internal(self, params):
-        return np.array(tree_just_flatten(params))
+        registry = get_registry(extended=True)
+        return np.array(tree_just_flatten(params, registry=registry))
 
 
 # ======================================================================================
@@ -72,20 +74,20 @@ def test_get_positivity_transform(lower_bounds, upper_bounds, case, expected):
 
 
 # ======================================================================================
-# _get_internal_selector_index
+# _get_selection_indices
 # ======================================================================================
 
 
-def test_get_internal_selector_index():
+def test_get_selection_indices():
 
-    converter = Converter()
     params = {"a": [0, 1, 2], "b": [3, 4, 5]}
     selector = lambda p: p["a"]
 
-    expected = [0, 1, 2]
-    got = _get_internal_selector_index(params, selector, converter)
+    expected = np.array([0, 1, 2], dtype=int)
+    got_index, got_n_params = _get_selection_indices(params, selector)
 
-    assert expected == got
+    assert got_n_params == 6
+    assert_array_equal(got_index, expected)
 
 
 # ======================================================================================
@@ -146,7 +148,7 @@ TEST_CASES = list(
 @pytest.mark.parametrize("constraint, params", TEST_CASES)
 def test_check_validity_nonlinear_constraint(constraint, params):
     with pytest.raises(ValueError):
-        _check_validity_nonlinear_constraint(constraint, params, skip_checks=False)
+        _check_validity_and_return_evaluation(constraint, params, skip_checks=False)
 
 
 def test_check_validity_nonlinear_constraint_correct_example():
@@ -156,7 +158,9 @@ def test_check_validity_nonlinear_constraint_correct_example():
         "lower_bounds": np.arange(4),
         "selector": lambda x: x[:1],
     }
-    _check_validity_nonlinear_constraint(constr, params=np.arange(4), skip_checks=False)
+    _check_validity_and_return_evaluation(
+        constr, params=np.arange(4), skip_checks=False
+    )
 
 
 # ======================================================================================