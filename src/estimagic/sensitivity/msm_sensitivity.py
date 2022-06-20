9b678c66b151085183b5dc610b3c2160db220f21
timmens <tim@mensinger96.com>
2022-06-20 16:17:56 +0200

    Remove broken link

--- src/estimagic/sensitivity/msm_sensitivity.py
+++ src/estimagic/sensitivity/msm_sensitivity.py
@@ -1,7 +1,7 @@
 """Implement local sensitivity measures for method of moments.
 
 measures:
-m1: Andrews, Gentzkow & Shapiro (https://doi.org/10.1093/qje/qjx023).
+m1: Andrews, Gentzkow & Shapiro (2017, Quarterly Journal of Economics)
 
 epsilon 2-6: Honore, Jorgensen & de Paula
 (https://onlinelibrary.wiley.com/doi/full/10.1002/jae.2779)
