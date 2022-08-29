41daf40fd8eae9377eef43eec35a34f1027e6889
Sebastian Gsell <sebastian.gsell93@gmail.com>
2022-08-29 17:41:58 +0200

    Implement alternative algorithm for constrained case.

--- src/estimagic/optimization/bhhh.py
+++ src/estimagic/optimization/bhhh.py
@@ -19,24 +19,35 @@ def bhhh(
     convergence_relative_gradient_tolerance=1e-8,
     stopping_max_iterations=200,
 ):
-    """Minimize a likelihood function using the bounded BHHH algorithm.
+    """Minimize a likelihood function using the box-constrained BHHH algorithm.
 
     For details, see :ref:`_own_algorithms`.
     """
-    result_dict = bhhh_internal(
-        criterion_and_derivative,
-        x=x,
-        lower_bounds=lower_bounds,
-        upper_bounds=upper_bounds,
-        convergence_absolute_gradient_tolerance=convergence_absolute_gradient_tolerance,
-        convergence_relative_gradient_tolerance=convergence_relative_gradient_tolerance,
-        stopping_max_iterations=stopping_max_iterations,
-    )
+    if np.isinf(lower_bounds).all() and np.isinf(upper_bounds).all():
+        result_dict = bhhh_unconstrained(
+            criterion_and_derivative,
+            x,
+            lower_bounds,
+            upper_bounds,
+            convergence_absolute_gradient_tolerance,
+            convergence_relative_gradient_tolerance,
+            stopping_max_iterations,
+        )
+    else:
+        result_dict = bhhh_box_constrained(
+            criterion_and_derivative,
+            x,
+            lower_bounds,
+            upper_bounds,
+            convergence_absolute_gradient_tolerance,
+            convergence_relative_gradient_tolerance,
+            stopping_max_iterations,
+        )
 
     return result_dict
 
 
-def bhhh_internal(
+def bhhh_unconstrained(
     criterion_and_derivative,
     x,
     lower_bounds,
@@ -94,7 +105,9 @@ def bhhh_internal(
     )
 
     gradient_reduced = gradient[inactive_set]
-    hessian_reduced = jacobian[:, inactive_set].T @ jacobian[:, inactive_set]
+    hessian_approx = jacobian.T @ jacobian
+    hessian_reduced = hessian_approx[inactive_set[:, np.newaxis], inactive_set]
+
     direction = determine_descent_direction(
         gradient, gradient_reduced, hessian_reduced, inactive_set, n_params
     )
@@ -111,7 +124,8 @@ def bhhh_internal(
         criterion_candidate, jacobian = criterion_and_derivative(x_candidate)
 
         if step_size == initial_step_size:
-            hessian_reduced = jacobian[:, inactive_set].T @ jacobian[:, inactive_set]
+            hessian_approx = jacobian.T @ jacobian
+            hessian_reduced = hessian_approx[inactive_set[:, np.newaxis], inactive_set]
 
         if np.sum(criterion_candidate) > np.sum(criterion_accepted):
             step_size /= 2
@@ -145,9 +159,10 @@ def bhhh_internal(
 
             if norm_pg < 0:
                 gradient_reduced = gradient[inactive_set]
-                hessian_reduced = (
-                    jacobian[:, inactive_set].T @ jacobian[:, inactive_set]
-                )
+                hessian_approx = jacobian.T @ jacobian
+                hessian_reduced = hessian_approx[
+                    inactive_set[:, np.newaxis], inactive_set
+                ]
 
                 direction = determine_descent_direction(
                     gradient,
@@ -198,7 +213,7 @@ def estimate_epsilon_inactive_set(x, norm_gradient, lower_bounds, upper_bounds):
 def determine_descent_direction(
     gradient_candidate, gradient_reduced, hessian_reduced, inactive_set, n_params
 ):
-    """Determine the new descent serach direction."""
+    """Determine the new descent (search) direction."""
     direction = np.linalg.solve(hessian_reduced, gradient_reduced)
 
     direction_active = gradient_candidate.copy()
@@ -210,3 +225,147 @@ def determine_descent_direction(
     direction_all = direction_active + direction_projected
 
     return direction_all
+
+
+def bhhh_box_constrained(
+    criterion_and_derivative,
+    x,
+    lower_bounds,
+    upper_bounds,
+    convergence_absolute_gradient_tolerance,
+    stopping_max_iterations,
+):
+    """Minimize a likelihood function using the box-constrained BHHH algorithm.
+
+    Args:
+        criterion_and_derivative (callable): A function returning the tuple:
+            - criterion (np.ndarray): Likelihood contributions of shape (n_obs,)
+            - derivative (np.ndarray): Jacobian matrix of shape (n_obs, n_params)
+        x (np.ndarray): Initial guess of the parameter vector x (starting points).
+        lower_bounds (np.ndarray): 1d array of shape (n_params,) with lower bounds
+            for the parameter vector x.
+        upper_bounds (np.ndarray): 1d array of shape (n_params,) with upper bounds
+            for the parameter vector x
+        convergence_absolute_gradient_tolerance (float): Stopping criterion for the
+            absolute gradient tolerance.
+        convergence_relative_gradient_tolerance (float): Stopping criterion for the
+            relative gradient tolerance.
+        stopping_max_iterations (int): Maximum number of iterations. If reached,
+            terminate.
+
+    Returns:
+        (dict) Result dictionary containing:
+
+        - solution_x (np.ndarray): Solution vector of shape (n_params,).
+        - solution_criterion (np.ndarray): Likelihood contributions at the solution.
+            Shape (n_obs,).
+        - solution_loglikelihood (float): Value of the log-likelihood at the solution.
+        - n_iterations (int): Number of iterations the algorithm ran before finding a
+            solution vector or reaching stopping_max_iterations.
+        - message (str): Message to the user. Currently it says: "Under development."
+    """
+    _zero_threshold = np.finfo(float).eps
+    n_params = len(x)
+    step_min = 1e-6
+
+    critic_limit = convergence_absolute_gradient_tolerance
+
+    crit = 1
+    step_len_optimal = 1
+
+    jacobian = criterion_and_derivative(x, task="derivative")
+    gradient = np.sum(jacobian, axis=0)
+
+    norm_pg = np.linalg.norm(x - np.clip(x - gradient, lower_bounds, upper_bounds))
+    inactive_set = estimate_epsilon_inactive_set(x, norm_pg, lower_bounds, upper_bounds)
+
+    for _n_iter in range(stopping_max_iterations):
+        jacobian = criterion_and_derivative(x, task="derivative")
+        gradient = np.sum(jacobian, axis=0)
+
+        gradient_reduced = gradient[inactive_set]
+        hessian_approx = jacobian.T @ jacobian
+        hessian_reduced = hessian_approx[inactive_set[:, np.newaxis], inactive_set]
+
+        direction_projected = determine_descent_direction(
+            gradient, gradient_reduced, hessian_reduced, inactive_set, n_params
+        )
+
+        step_len_optimal = find_optimal_step_len(
+            x,
+            direction_projected,
+            lower_bounds,
+            upper_bounds,
+            step_min,
+            criterion_and_derivative,
+        )
+
+        x_candidate = np.clip(
+            x + step_len_optimal * direction_projected, lower_bounds, upper_bounds
+        )
+
+        crit = np.max(
+            np.abs(((x_candidate - x) + _zero_threshold) / (x + _zero_threshold))
+            / step_len_optimal
+        )
+
+        x = x_candidate
+
+        if crit < critic_limit:
+            break
+
+        norm_pg = np.linalg.norm(x - np.clip(x - gradient, lower_bounds, upper_bounds))
+        inactive_set = estimate_epsilon_inactive_set(
+            x, norm_pg, lower_bounds, upper_bounds
+        )
+
+    solution_criterion = criterion_and_derivative(x, task="criterion")
+
+    result_dict = {
+        "solution_x": x,
+        "solution_criterion": solution_criterion,
+        "solution_loglikelihood": np.sum(solution_criterion),
+        "n_iterations": _n_iter,
+        "message": "Under develpment",
+    }
+
+    return result_dict
+
+
+def find_optimal_step_len(
+    x,
+    direction_projected,
+    lower_bounds,
+    upper_bounds,
+    step_min,
+    criterion_and_derivative,
+):
+    """Find optimal step length."""
+    step_len_trial = 2
+    step_len_optimal = 1
+
+    loglike_higher = 1
+    loglike_lower = 0
+
+    while (loglike_higher > loglike_lower) & (step_len_trial >= step_min):
+        step_len_trial = step_len_trial / 2
+
+        criterion_candidate_higher = criterion_and_derivative(
+            np.clip(
+                x + step_len_trial * direction_projected, lower_bounds, upper_bounds
+            ),
+            task="criterion",
+        )
+        criterion_candidate_lower = criterion_and_derivative(
+            np.clip(
+                x + step_len_trial * direction_projected / 2, lower_bounds, upper_bounds
+            ),
+            task="criterion",
+        )
+
+        loglike_higher = np.sum(criterion_candidate_higher)
+        loglike_lower = np.sum(criterion_candidate_lower)
+
+        step_len_optimal = step_len_trial
+
+    return step_len_optimal
--- tests/optimization/test_bhhh.py
+++ tests/optimization/test_bhhh.py
@@ -2,9 +2,12 @@
 from functools import partial
 
 import numpy as np
+import pandas as pd
 import pytest
 import statsmodels.api as sm
-from estimagic.optimization.bhhh import bhhh_internal
+from estimagic.config import TEST_FIXTURES_DIR
+from estimagic.optimization.bhhh import bhhh_box_constrained
+from estimagic.optimization.bhhh import bhhh_unconstrained
 from estimagic.utilities import get_rng
 from numpy.testing import assert_array_almost_equal as aaae
 from scipy.stats import norm
@@ -23,6 +26,20 @@ def generate_test_data():
     exog = simulated_exog
     intercept = np.ones((exog.shape[0], 1))
     exog = np.hstack((intercept, exog))
+
+    return endog, exog
+
+
+def ibm_test_data():
+    df = pd.read_csv(TEST_FIXTURES_DIR / "telco_churn_clean.csv")
+
+    exog = df.drop(columns="Churn").to_numpy()
+    intercept = np.ones((exog.shape[0], 1))
+    exog = np.hstack((intercept, exog))
+    endog = df[["Churn"]].to_numpy()
+    endog = endog.reshape(
+        len(endog),
+    )
 
     return endog, exog
 
@@ -62,38 +79,132 @@ def get_score_probit(endog, exog, x):
     return derivative_loglikelihood[:, None] * exog
 
 
-def criterion_and_derivative_logit(x):
+def criterion_and_derivative_logit(x, task="criterion_and_derivative"):
     """Return Logit criterion and derivative.
 
     Args:
         x (np.ndarray): Parameter vector of shape (n_obs,).
+        task (str): If task=="criterion", compute log-likelihood.
+            If task=="derivative", compute derivative.
+            If task="criterion_and_derivative", compute both.
 
     Returns:
-        tuple: first entry is the criterion, second entry is the score
-
+        (np.ndarray or tuple): If task=="criterion" it returns the output of
+            criterion, which is a 1d numpy array.
+            If task=="derivative" it returns the first derivative of criterion,
+            which is 2d numpy array.
+            If task=="criterion_and_derivative" it returns both as a tuple.
     """
     endog, exog = generate_test_data()
-    score = partial(get_score_logit, endog, exog)
-    loglike = partial(get_loglikelihood_logit, endog, exog)
+    result = ()
 
-    return -loglike(x), score(x)
+    if "criterion" in task:
+        loglike = partial(get_loglikelihood_logit, endog, exog)
+        result += (-loglike(x),)
+    if "derivative" in task:
+        score = partial(get_score_logit, endog, exog)
+        result += (score(x),)
+
+    if len(result) == 1:
+        (result,) = result
+
+    return result
 
 
-def criterion_and_derivative_probit(x):
+def criterion_and_derivative_logit_ibm(x, task="criterion_and_derivative"):
+    """Return Logit criterion and derivative.
+
+    Args:
+        x (np.ndarray): Parameter vector of shape (n_obs,).
+        task (str): If task=="criterion", compute log-likelihood.
+            If task=="derivative", compute derivative.
+            If task="criterion_and_derivative", compute both.
+
+    Returns:
+        (np.ndarray or tuple): If task=="criterion" it returns the output of
+            criterion, which is a 1d numpy array.
+            If task=="derivative" it returns the first derivative of criterion,
+            which is 2d numpy array.
+            If task=="criterion_and_derivative" it returns both as a tuple.
+    """
+    endog, exog = ibm_test_data()
+    result = ()
+
+    if "criterion" in task:
+        loglike = partial(get_loglikelihood_logit, endog, exog)
+        result += (-loglike(x),)
+    if "derivative" in task:
+        score = partial(get_score_logit, endog, exog)
+        result += (score(x),)
+
+    if len(result) == 1:
+        (result,) = result
+
+    return result
+
+
+def criterion_and_derivative_probit(x, task="criterion_and_derivative"):
     """Return Probit criterion and derivative.
 
     Args:
         x (np.ndarray): Parameter vector of shape (n_obs,).
+        task (str): If task=="criterion", compute log-likelihood.
+            If task=="derivative", compute derivative.
+            If task="criterion_and_derivative", compute both.
 
     Returns:
-        tuple: first entry is the criterion, second entry is the score
+        (np.ndarray or tuple): If task=="criterion" it returns the output of
+            criterion, which is a 1d numpy array.
+            If task=="derivative" it returns the first derivative of criterion,
+            which is 2d numpy array.
+            If task=="criterion_and_derivative" it returns both as a tuple.
     """
     endog, exog = generate_test_data()
+    result = ()
 
-    score = partial(get_score_probit, endog, exog)
-    loglike = partial(get_loglikelihood_probit, endog, exog)
+    if "criterion" in task:
+        loglike = partial(get_loglikelihood_probit, endog, exog)
+        result += (-loglike(x),)
+    if "derivative" in task:
+        score = partial(get_score_probit, endog, exog)
+        result += (score(x),)
 
-    return -loglike(x), score(x)
+    if len(result) == 1:
+        (result,) = result
+
+    return result
+
+
+def criterion_and_derivative_probit_ibm(x, task="criterion_and_derivative"):
+    """Return Probit criterion and derivative.
+
+    Args:
+        x (np.ndarray): Parameter vector of shape (n_obs,).
+        task (str): If task=="criterion", compute log-likelihood.
+            If task=="derivative", compute derivative.
+            If task="criterion_and_derivative", compute both.
+
+    Returns:
+        (np.ndarray or tuple): If task=="criterion" it returns the output of
+            criterion, which is a 1d numpy array.
+            If task=="derivative" it returns the first derivative of criterion,
+            which is 2d numpy array.
+            If task=="criterion_and_derivative" it returns both as a tuple.
+    """
+    endog, exog = ibm_test_data()
+    result = ()
+
+    if "criterion" in task:
+        loglike = partial(get_loglikelihood_probit, endog, exog)
+        result += (-loglike(x),)
+    if "derivative" in task:
+        score = partial(get_score_probit, endog, exog)
+        result += (score(x),)
+
+    if len(result) == 1:
+        (result,) = result
+
+    return result
 
 
 # =====================================================================================
@@ -106,7 +217,7 @@ def result_logit_unbounded():
     endog, exog = generate_test_data()
     result_unbounded = sm.Logit(endog, exog).fit(disp=True)
 
-    return result_unbounded.params
+    return result_unbounded
 
 
 @pytest.fixture
@@ -114,52 +225,96 @@ def result_probit_unbounded():
     endog, exog = generate_test_data()
     result_unbounded = sm.Probit(endog, exog).fit(disp=True)
 
-    return result_unbounded.params
+    return result_unbounded
 
 
 @pytest.fixture
 def result_logit_bounded():
-    return np.array([-1.866, -1.5223, 2.7153])
+    endog, exog = generate_test_data()
+    result_bounded = sm.Logit(endog, exog).fit(
+        method="lbfgs", bounds=((-5, np.inf), (-10, 10), (-10, 10)), disp=False
+    )
+
+    return result_bounded
+
+
+@pytest.fixture
+def result_probit_bounded():
+    endog, exog = generate_test_data()
+    result_bounded = sm.Logit(endog, exog).fit(
+        method="lbfgs", bounds=((-5, np.inf), (-10, 10), (-10, 10)), disp=False
+    )
+
+    return result_bounded
+
+
+@pytest.fixture
+def result_logit_ibm():
+    endog, exog = ibm_test_data()
+    result_unbounded = sm.Logit(endog, exog).fit(disp=False)
+
+    return result_unbounded
+
+
+@pytest.fixture
+def result_probit_ibm():
+    endog, exog = ibm_test_data()
+    result_unbounded = sm.Probit(endog, exog).fit(disp=False)
+
+    return result_unbounded
 
 
 # =====================================================================================
 # Tests
 # =====================================================================================
 
-TEST_CASES = [
+TEST_CASES_UNBOUNDED = [
     (
         criterion_and_derivative_logit,
-        np.array([-np.inf, -np.inf, -np.inf]),
-        np.array([np.inf, np.inf, np.inf]),
+        np.zeros(3),
+        -np.ones(3) * np.inf,
+        np.ones(3) * np.inf,
         "result_logit_unbounded",
     ),
     (
         criterion_and_derivative_probit,
-        np.array([-np.inf, -np.inf, -np.inf]),
-        np.array([np.inf, np.inf, np.inf]),
+        np.zeros(3),
+        -np.ones(3) * np.inf,
+        np.ones(3) * np.inf,
         "result_probit_unbounded",
     ),
     (
-        criterion_and_derivative_logit,
-        np.array([-5, -1_000, -1_000]),
-        np.array([1_000, 1_000, 1_000]),
-        "result_logit_bounded",
+        criterion_and_derivative_logit_ibm,
+        np.zeros(9),
+        -np.ones(9) * np.inf,
+        np.ones(9) * np.inf,
+        "result_logit_ibm",
+    ),
+    (
+        criterion_and_derivative_probit_ibm,
+        np.zeros(9),
+        -np.ones(9) * np.inf,
+        np.ones(9) * np.inf,
+        "result_probit_ibm",
     ),
 ]
 
 
 @pytest.mark.parametrize(
-    "criterion_and_derivative, lower_bounds, upper_bounds, result_statsmodels",
-    TEST_CASES,
+    "criterion_and_derivative, x0, lower_bounds, upper_bounds, expected",
+    TEST_CASES_UNBOUNDED,
 )
 def test_maximum_likelihood(
-    criterion_and_derivative, lower_bounds, upper_bounds, result_statsmodels, request
+    criterion_and_derivative,
+    x0,
+    lower_bounds,
+    upper_bounds,
+    expected,
+    request,
 ):
-    params_expected = request.getfixturevalue(result_statsmodels)
+    params_expected = request.getfixturevalue(expected)
 
-    x0 = np.zeros(3)
-
-    result_bhhh = bhhh_internal(
+    result_bhhh = bhhh_unconstrained(
         criterion_and_derivative,
         x=x0,
         lower_bounds=lower_bounds,
@@ -169,4 +324,51 @@ def test_maximum_likelihood(
         stopping_max_iterations=200,
     )
 
-    aaae(result_bhhh["solution_x"], params_expected, decimal=4)
+    aaae(result_bhhh["solution_x"], params_expected.params, decimal=4)
+
+
+TEST_CASES_BOUNDED = [
+    (
+        criterion_and_derivative_logit,
+        np.zeros(3),
+        np.array([-5, -100, -100]),
+        np.array([100, 100, 100]),
+        "result_logit_bounded",
+        4,
+    ),
+    (
+        criterion_and_derivative_probit,
+        np.zeros(3),
+        np.array([-5, -np.inf, -np.inf]),
+        np.array([10, np.inf, np.inf]),
+        "result_probit_bounded",
+        0,
+    ),
+]
+
+
+@pytest.mark.parametrize(
+    "criterion_and_derivative, x0, lower_bounds, upper_bounds, expected, digits",
+    TEST_CASES_BOUNDED,
+)
+def test_maximum_likelihood_bounded(
+    criterion_and_derivative,
+    x0,
+    lower_bounds,
+    upper_bounds,
+    expected,
+    decimals,
+    request,
+):
+    params_expected = request.getfixturevalue(expected)
+
+    result_bhhh = bhhh_box_constrained(
+        criterion_and_derivative,
+        x=x0,
+        lower_bounds=lower_bounds,
+        upper_bounds=upper_bounds,
+        convergence_absolute_gradient_tolerance=1e-4,
+        stopping_max_iterations=200,
+    )
+
+    aaae(result_bhhh["solution_x"], params_expected.params, decimal=decimals)
