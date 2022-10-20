1e4ddd320dfbae07abf2172f146043f39798fa74
Sebastian Gsell <sebastian.gsell93@gmail.com>
2022-10-20 11:06:07 +0200

    More tests and fix.

--- src/estimagic/optimization/tranquilo/filter_points.py
+++ src/estimagic/optimization/tranquilo/filter_points.py
@@ -51,35 +51,48 @@ def drop_collinear(xs, indices, state):
 
 def drop_collinear_pounders(xs, indices, state):
     """Drop collinear points using pounders filtering."""
+    if xs.shape[0] <= xs.shape[1] + 1:
+        filtered_xs, filtered_indices = xs, indices
+    else:
+        filtered_xs, filtered_indices = _drop_collinear_pounders(xs, indices, state)
+
+    return filtered_xs, filtered_indices
+
+
+def _drop_collinear_pounders(xs, indices, state):
     theta2 = 1e-4
     n_samples, n_params = xs.shape
     n_poly_terms = n_params * (n_params + 1) // 2
 
-    indices = np.flip(indices)
+    indices_reverse = indices[::-1]
+    indexer_reverse = np.arange(n_samples)[::-1]
 
     radius = state.radius
     center = state.x
-    index_center = indices[state.index]
+    index_center = int(np.where(indices_reverse == state.index)[0])
     centered_xs = (xs - center) / radius
 
     (
         linear_features,
         square_features,
-        index_list_additional,
+        idx_list_n_plus_1,
         index,
     ) = _get_polynomial_feature_matrices(
-        centered_xs, indices, index_center, n_params, n_samples, n_poly_terms
+        centered_xs,
+        indexer_reverse,
+        index_center,
+        n_params,
+        n_samples,
+        n_poly_terms,
     )
 
-    filtered_indices = np.ones(n_samples, dtype=np.int64) * -999
-    filtered_indices[0] = indices[index_center]
-    filtered_indices[1 : n_params + 1] = indices[index_list_additional]
+    indexer_filtered = indexer_reverse[idx_list_n_plus_1].tolist()
+    _index_center = indexer_reverse[index_center]
 
     counter = n_params + 1
 
     while (counter < n_samples) and (index >= 0):
-
-        if index == indices[index_center]:
+        if index == _index_center:
             index -= 1
             continue
 
@@ -96,23 +109,24 @@ def drop_collinear_pounders(xs, indices, state):
         beta = np.linalg.svd(n_z_mat.T[n_params + 1 :], compute_uv=False)
 
         if beta[min(counter - n_params, n_poly_terms) - 1] > theta2:
-            filtered_indices[counter] = index
+            indexer_filtered += [index]
             counter += 1
 
         index -= 1
 
-    filtered_indices = filtered_indices[filtered_indices >= 0]
+    filtered_indices = indices[indexer_filtered]
+    filtered_xs = xs[indexer_filtered]
 
-    return xs[filtered_indices], filtered_indices
+    return filtered_xs, filtered_indices
 
 
 def _get_polynomial_feature_matrices(
-    centered_xs, indices, index_center, n_params, n_samples, n_poly_terms
+    centered_xs, indexer, index_center, n_params, n_samples, n_poly_terms
 ):
     linear_features = np.zeros((n_samples, n_params + 1))
     square_features = np.zeros((n_samples, n_poly_terms))
 
-    linear_features[0, 1:] = centered_xs[indices[0]]
+    linear_features[0, 1:] = centered_xs[indexer[index_center]]
     square_features[0, :] = _square_features(linear_features[0, 1:]).flatten()
 
     _is_center_in_head = index_center < n_params
@@ -120,14 +134,14 @@ def _get_polynomial_feature_matrices(
     idx_list_n_plus_1 = [index_center] + idx_list_n
 
     linear_features[:, 0] = 1
-    linear_features[: n_params + 1, 1:] = centered_xs[indices[idx_list_n_plus_1]]
+    linear_features[: n_params + 1, 1:] = centered_xs[indexer[idx_list_n_plus_1]]
     square_features[: n_params + 1, :] = _square_features(
         linear_features[: n_params + 1, 1:]
     )
 
     idx = n_samples - _is_center_in_head - len(idx_list_n) - 1
 
-    return linear_features, square_features, idx_list_n, idx
+    return linear_features, square_features, idx_list_n_plus_1, idx
 
 
 @njit
