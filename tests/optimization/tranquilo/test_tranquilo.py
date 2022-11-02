1e557d9d06ea616eceea111df5b78ebb0339819d
Tim <tim@mensinger96.com>
2022-11-02 13:52:13 +0100

    Improve construction of test cases in test_tranquilo.py

--- tests/optimization/tranquilo/test_tranquilo.py
+++ tests/optimization/tranquilo/test_tranquilo.py
@@ -1,4 +1,4 @@
-from itertools import product
+import itertools
 
 import numpy as np
 import pytest
@@ -8,63 +8,56 @@ from estimagic.optimization.tranquilo.tranquilo import tranquilo_ls
 from numpy.testing import assert_array_almost_equal as aaae
 
 
+def _product(sample_filter, fitter, surrogate_model, sample_size):
+    # is used to create products of test cases
+    return list(itertools.product(sample_filter, fitter, surrogate_model, sample_size))
+
+
 # ======================================================================================
 # Scalar Tranquilo
 # ======================================================================================
 
-_sample_filter = ["discard_all", "keep_all"]
-_fitter = ["ols"]
-_surrogate_model = ["quadratic"]
-_sample_size = ["quadratic"]
-ols = list(product(_sample_filter, _fitter, _surrogate_model, _sample_size))
+TEST_CASES = {
+    "ols": {
+        "sample_filter": ["discard_all", "keep_all"],
+        "fitter": ["ols"],
+        "surrogate_model": ["quadratic"],
+        "sample_size": ["quadratic"],
+    },
+    "ols_keep_all": {
+        "sample_filter": ["keep_all"],
+        "fitter": ["ols"],
+        "surrogate_model": ["quadratic"],
+        "sample_size": ["powell"],
+    },
+    "pounders_discard_all": {
+        "sample_filter": ["discard_all"],
+        "fitter": ["powell"],
+        "surrogate_model": ["quadratic"],
+        "sample_size": ["quadratic"],
+    },
+    "pounders_keep_all": {
+        "sample_filter": ["keep_all"],
+        "fitter": ["powell"],
+        "surrogate_model": ["quadratic"],
+        "sample_size": ["linear", "powell", "quadratic"],
+    },
+    "ols_pounders_filtering": {
+        "sample_filter": ["drop_pounders"],
+        "fitter": ["ols"],
+        "surrogate_model": ["quadratic"],
+        "sample_size": ["powell", "quadratic"],
+    },
+    "pounders_filtering": {
+        "sample_filter": ["drop_pounders"],
+        "fitter": ["powell"],
+        "surrogate_model": ["quadratic"],
+        "sample_size": ["linear", "powell", "quadratic"],
+    },
+}
 
-_sample_filter = ["keep_all"]
-_fitter = ["ols"]
-_surrogate_model = ["quadratic"]
-_sample_size = ["powell"]
-ols_keep_all = list(product(_sample_filter, _fitter, _surrogate_model, _sample_size))
-
-_sample_filter = ["discard_all"]
-_fitter = ["powell"]
-_surrogate_model = ["quadratic"]
-_sample_size = ["quadratic"]
-pounders_discard_all = list(
-    product(_sample_filter, _fitter, _surrogate_model, _sample_size)
-)
-
-_sample_filter = ["keep_all"]
-_fitter = ["powell"]
-_surrogate_model = ["quadratic"]
-_sample_size = ["linear", "powell", "quadratic"]
-pounders_keep_all = list(
-    product(_sample_filter, _fitter, _surrogate_model, _sample_size)
-)
-
-_sample_filter = ["drop_pounders"]
-_fitter = ["ols"]
-_surrogate_model = ["quadratic"]
-_sample_size = ["powell", "quadratic"]
-ols_pounders_filtering = list(
-    product(_sample_filter, _fitter, _surrogate_model, _sample_size)
-)
-
-_sample_filter = ["drop_pounders"]
-_fitter = ["powell"]
-_surrogate_model = ["quadratic"]
-_sample_size = ["linear", "powell", "quadratic"]
-pounders_filtering = list(
-    product(_sample_filter, _fitter, _surrogate_model, _sample_size)
-)
-
-
-TEST_CASES = (
-    ols
-    + ols_keep_all
-    + pounders_discard_all
-    + pounders_keep_all
-    + ols_pounders_filtering
-    + pounders_filtering
-)
+TEST_CASES = [_product(**kwargs) for kwargs in TEST_CASES.values()]
+TEST_CASES = itertools.chain.from_iterable(TEST_CASES)
 
 
 @pytest.mark.parametrize(
@@ -88,25 +81,27 @@ def test_internal_tranquilo_scalar_sphere_defaults(
 # Imprecise options for scalar tranquilo
 # ======================================================================================
 
-_sample_filter = ["keep_all"]
-_fitter = ["ols"]
-_surrogate_model = ["quadratic"]
-_sample_size = ["linear"]
-ols_keep_all = list(product(_sample_filter, _fitter, _surrogate_model, _sample_size))
+TEST_CASES = {
+    "ls_keep": {
+        "sample_filter": ["keep_all"],
+        "fitter": ["ols"],
+        "surrogate_model": ["quadratic"],
+        "sample_size": ["linear"],
+    },
+    "pounders_discard_all": {
+        "sample_filter": ["discard_all"],
+        "fitter": ["powell"],
+        "surrogate_model": ["quadratic"],
+        "sample_size": ["powell"],
+    },
+}
 
-_sample_filter = ["discard_all"]
-_fitter = ["powell"]
-_surrogate_model = ["quadratic"]
-_sample_size = ["powell"]
-pounders_discard_all = list(
-    product(_sample_filter, _fitter, _surrogate_model, _sample_size)
-)
-
-TEST_CASES_IMPRECISE = ols_keep_all + pounders_discard_all
+TEST_CASES = [_product(**kwargs) for kwargs in TEST_CASES.values()]
+TEST_CASES = itertools.chain.from_iterable(TEST_CASES)
 
 
 @pytest.mark.parametrize(
-    "sample_filter, fitter, surrogate_model, sample_size", TEST_CASES_IMPRECISE
+    "sample_filter, fitter, surrogate_model, sample_size", TEST_CASES
 )
 def test_internal_tranquilo_scalar_sphere_imprecise_defaults(
     sample_filter, fitter, surrogate_model, sample_size
@@ -141,22 +136,24 @@ def test_external_tranquilo_scalar_sphere_defaults():
 # Least-squares Tranquilo
 # ======================================================================================
 
-_sample_filter = ["keep_all", "discard_all"]
-_fitter = ["ols"]
-_surrogate_model = ["linear"]
-_sample_size = ["linear"]
-ols = list(product(_sample_filter, _fitter, _surrogate_model, _sample_size))
 
+TEST_CASES = {
+    "ols": {
+        "sample_filter": ["keep_all", "discard_all"],
+        "fitter": ["ols"],
+        "surrogate_model": ["linear"],
+        "sample_size": ["linear"],
+    },
+    "pounders_filtering": {
+        "sample_filter": ["drop_pounders"],
+        "fitter": ["ols"],
+        "surrogate_model": ["linear"],
+        "sample_size": ["linear"],
+    },
+}
 
-_sample_filter = ["drop_pounders"]
-_fitter = ["ols"]
-_surrogate_model = ["linear"]
-_sample_size = ["linear"]
-pounders_filtering = list(
-    product(_sample_filter, _fitter, _surrogate_model, _sample_size)
-)
-
-TEST_CASES = ols + pounders_filtering
+TEST_CASES = [_product(**kwargs) for kwargs in TEST_CASES.values()]
+TEST_CASES = itertools.chain.from_iterable(TEST_CASES)
 
 
 @pytest.mark.parametrize(
