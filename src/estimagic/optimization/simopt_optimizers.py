f1c725ac342f95a9db52885a7bb02170bed16ce6
Tim <tim@mensinger96.com>
2022-11-09 16:28:10 +0100

    Add simoprt optimizers

--- src/estimagic/optimization/simopt_optimizers.py
+++ src/estimagic/optimization/simopt_optimizers.py
@@ -1,4 +1,5 @@
 """Implement simopt optimizers."""
+import numpy as np
 from estimagic.config import IS_SIMOPT_INSTALLED
 from estimagic.decorators import mark_minimizer
 
@@ -14,8 +15,25 @@ except ImportError:
     needs_scaling=True,
     is_available=IS_SIMOPT_INSTALLED,
 )
-def simopt_adam():
-    pass
+def simopt_astrodf(
+    criterion,
+    derivative,
+    x,
+    lower_bounds,
+    upper_bounds,
+    *,
+    budget=None,
+):
+    out = _minimize_simopt(
+        name="ADAM",
+        criterion=criterion,
+        derivative=derivative,
+        x=x,
+        lower_bounds=lower_bounds,
+        upper_bounds=upper_bounds,
+        budget=budget,
+    )
+    return out
 
 
 @mark_minimizer(
@@ -24,8 +42,25 @@ def simopt_adam():
     needs_scaling=True,
     is_available=IS_SIMOPT_INSTALLED,
 )
-def simopt_aloe():
-    pass
+def simopt_astrodf(
+    criterion,
+    derivative,
+    x,
+    lower_bounds,
+    upper_bounds,
+    *,
+    budget=None,
+):
+    out = _minimize_simopt(
+        name="ALOE",
+        criterion=criterion,
+        derivative=derivative,
+        x=x,
+        lower_bounds=lower_bounds,
+        upper_bounds=upper_bounds,
+        budget=budget,
+    )
+    return out
 
 
 @mark_minimizer(
@@ -34,8 +69,25 @@ def simopt_aloe():
     needs_scaling=True,
     is_available=IS_SIMOPT_INSTALLED,
 )
-def simopt_astrodf():
-    pass
+def simopt_astrodf(
+    criterion,
+    derivative,
+    x,
+    lower_bounds,
+    upper_bounds,
+    *,
+    budget=None,
+):
+    out = _minimize_simopt(
+        name="ASTRODF",
+        criterion=criterion,
+        derivative=derivative,
+        x=x,
+        lower_bounds=lower_bounds,
+        upper_bounds=upper_bounds,
+        budget=budget,
+    )
+    return out
 
 
 @mark_minimizer(
@@ -45,28 +97,18 @@ def simopt_astrodf():
     is_available=IS_SIMOPT_INSTALLED,
 )
 def simopt_neldmd():
-    pass
-
-
-@mark_minimizer(
-    name="simopt_randomsearch",
-    primary_criterion_entry="value",
-    needs_scaling=True,
-    is_available=IS_SIMOPT_INSTALLED,
-)
-def simopt_randomsearch(
-    criterion,
-    x,
-    lower_bounds,
-    upper_bounds,
-    *,
-    population_size=None,
-    batch_evaluator=None,
-    n_cores=1,
-    seed=None,
-    discard_start_params=False,
-):
-    pass
+    problem = MinimzerClass(
+        criterion=criterion,
+        gradient=derivative,
+        x=x,
+        lower_bounds=lower_bounds,
+        upper_bounds=upper_bounds,
+        budget=budget,
+    )
+    solver = so.experiment_base.ProblemSolver(solver_name="ASTRODF", problem=problem)
+    solver.run(n_macroreps=1)
+    out = solver.all_recommended_xs[0][-1]
+    return out
 
 
 @mark_minimizer(
@@ -76,7 +118,18 @@ def simopt_randomsearch(
     is_available=IS_SIMOPT_INSTALLED,
 )
 def simopt_spsa():
-    pass
+    problem = MinimzerClass(
+        criterion=criterion,
+        gradient=derivative,
+        x=x,
+        lower_bounds=lower_bounds,
+        upper_bounds=upper_bounds,
+        budget=budget,
+    )
+    solver = so.experiment_base.ProblemSolver(solver_name="ASTRODF", problem=problem)
+    solver.run(n_macroreps=1)
+    out = solver.all_recommended_xs[0][-1]
+    return out
 
 
 @mark_minimizer(
@@ -86,4 +139,125 @@ def simopt_spsa():
     is_available=IS_SIMOPT_INSTALLED,
 )
 def simopt_strong():
-    pass
+    problem = MinimzerClass(
+        criterion=criterion,
+        gradient=derivative,
+        x=x,
+        lower_bounds=lower_bounds,
+        upper_bounds=upper_bounds,
+        budget=budget,
+    )
+    solver = so.experiment_base.ProblemSolver(solver_name="ASTRODF", problem=problem)
+    solver.run(n_macroreps=1)
+    out = solver.all_recommended_xs[0][-1]
+    return out
+
+
+# ======================================================================================
+# Template
+# ======================================================================================
+
+
+def _minimize_simopt(
+    name,
+    criterion,
+    derivative,
+    x,
+    lower_bounds,
+    upper_bounds,
+    *,
+    budget=None,
+):
+    budget = 1_000 if budget is None else budget
+
+    problem = MinimzerClass(
+        criterion=criterion,
+        gradient=derivative,
+        x=x,
+        lower_bounds=lower_bounds,
+        upper_bounds=upper_bounds,
+        budget=budget,
+    )
+    solver = so.experiment_base.ProblemSolver(solver_name=name, problem=problem)
+    solver.run(n_macroreps=1)
+    out = solver.all_recommended_xs[0][-1]
+    return out
+
+
+# ======================================================================================
+# SimOpt Classes
+# ======================================================================================
+
+
+class CriterionClass(so.base.Model):
+    def __init__(self, criterion, gradient, x, fixed_factors=None):
+        fixed_factors = {} if fixed_factors is None else fixed_factors
+        self.n_rngs = 0
+        self.n_responses = 1
+        self.specifications = {"x": {"default": x}}
+        self.criterion = criterion
+        self.gradient = gradient
+        super().__init__(fixed_factors)
+
+    def replicate(self, rng_list):
+        x = np.array(self.factors["x"])
+        responses = {"value": self.criterion(x)}
+        gradients = {"value": {"x": self.gradient(x)}}
+        return responses, gradients
+
+
+class MinimzerClass(so.base.Problem):
+    def __init__(
+        self,
+        criterion,
+        gradient,
+        x,
+        lower_bounds,
+        upper_bounds,
+        budget,
+        name="Minimizer",
+        fixed_factors=None,
+        model_fixed_factors=None,
+    ):
+        fixed_factors = {} if fixed_factors is None else fixed_factors
+        model_fixed_factors = {} if model_fixed_factors is None else model_fixed_factors
+        self.name = name
+        self.dim = 2
+        self.n_objectives = 1
+        self.n_stochastic_constraints = 0
+        self.minmax = (-1,)
+        self.constraint_type = "box"
+        self.variable_type = "continuous"
+        self.lower_bounds = lower_bounds
+        self.upper_bounds = upper_bounds
+        self.gradient_available = True
+        self.model_default_factors = {}
+        self.model_decision_factors = {"x"}
+        self.factors = fixed_factors
+        self.specifications = {
+            "initial_solution": {"default": x},
+            "budget": {"default": budget},
+        }
+        self.check_factor_list = {
+            "initial_solution": self.check_initial_solution,
+            "budget": self.check_budget,
+        }
+        super().__init__(fixed_factors, model_fixed_factors)
+        self.model = CriterionClass(
+            criterion=criterion,
+            gradient=gradient,
+            x=x,
+            fixed_factors=self.model_fixed_factors,
+        )
+
+    def vector_to_factor_dict(self, vector):
+        factor_dict = {"x": vector[:]}
+        return factor_dict
+
+    def factor_dict_to_vector(self, factor_dict):
+        vector = factor_dict["x"]
+        return vector
+
+    def response_dict_to_objectives(self, response_dict):
+        objectives = (response_dict["value"],)
+        return objectives
