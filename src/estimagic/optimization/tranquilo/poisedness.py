3884b279f497fe1c4d6ffa6bf95ae89948b3bf9a
Sebastian Gsell <sebastian.gsell93@gmail.com>
2022-12-02 13:58:45 +0100

    New implementation. Follow Conn et al (2009).

--- src/estimagic/optimization/tranquilo/poisedness.py
+++ src/estimagic/optimization/tranquilo/poisedness.py
@@ -1,176 +1,77 @@
 import numpy as np
-from estimagic.optimization.subsolvers._trsbox_quadratic import minimize_trust_trsbox
 
 
-def get_lambda_poisedness_constant(sample, lower_bounds, upper_bounds):
-    """Calculate the lambda poisedness constant of a sample.
+def get_lagrange_poly_matrix(sample):
+    """Construct matrix of lagrange polynomials.
 
-    Lambda-poisedness is a concept to measure how well a set of points is dispersed
-    through a region of interest; here the trust-region. The poisedness of a sample
-    reflects how well the sample “spans” the region of interest.
-
-    In general, if the sample is lambda-poised with a small lambda, the sample is
-    said to have "good" geometry. As lambda grows, the system represented by these
-    "points", i.e. vectors, becomes increasingly linearly dependent. The smallest
-    possible lambda a sample can achieve is 1: lambda >= 1.
-
-    Formal definition:
-    A sample Y is said to be lambda-poised on a region of interest if it is linearly
-    independent and the Lagrange polynomials L(i) of points i through N in Y satisfy:
-
-        lambda >= max_i max_x | L(i) |      (1)
-
-    i.e. for each point i in the sample, we maximize the absolute criterion value
-    of its lagrange polynomial L(i); we then take the maximum over all these
-    criterion values as the lambda constant for this particular sample.
-
-    When we compare different samples on the same trust-region, we are usually
-    interested in keeping the sample with the least lambda, so that (1) holds.
-
-    For more details, see Conn et al. (:cite:`Conn2010`).
-
-    Note that the trust-region is centered around the origin, and its radius is
-    normalized to 1.
+    See :cite:`Conn2009`, Chapter 4.2, p. 60.
 
     Args:
-        sample (np.ndarray): The data sample of shape (p, n), where p is the number
-            of points in the sample and n the number of parameters.
-        lower_bounds (np.ndarray): 1d array of shape (n,) with lower bounds for the
-            parameter vector x.
-        upper_bounds (np.ndarray): 1d array of shape (n,) with upper bounds for the
-            parameter vector x.
+        sample (np.ndarry): Array of shape (n_samples, n_params).
 
     Returns:
-        float: The lambda poisedness constant, which is >=1. A small lambda means
-            the points in the sample have "good" geometry.
+        np.ndarray: Matrix of lagrange polynomials of shape
+            (n_samples, n_params * (n_params + 1) // 2).
 
     """
-    n_samples, n_params = sample.shape
+    basis_mat = _scaled_polynomial_features(sample)
+    lagrange_mat = basis_mat @ np.linalg.inv(basis_mat.T @ basis_mat)
 
-    interpolation_mat = build_interpolation_matrix(sample.T, n_params, n_samples)
-    lambda_ = -999
-
-    for index in range(n_samples):
-        linear_terms, square_terms = get_lagrange_polynomial(
-            sample=sample,
-            interpolation_mat=interpolation_mat,
-            index=index,
-        )
-
-        x_argmax = minimize_trust_trsbox(
-            linear_terms,
-            square_terms,
-            trustregion_radius=1,
-            lower_bounds=lower_bounds,
-            upper_bounds=upper_bounds,
-        )
-        critval_min = abs(_evaluate_scalar_model(x_argmax, linear_terms, square_terms))
-
-        x_argmin = minimize_trust_trsbox(
-            -linear_terms,
-            -square_terms,
-            trustregion_radius=1,
-            lower_bounds=lower_bounds,
-            upper_bounds=upper_bounds,
-        )
-        critval_max = abs(_evaluate_scalar_model(x_argmin, linear_terms, square_terms))
-
-        if critval_min >= critval_max:
-            current_critval = critval_min
-        else:
-            current_critval = critval_max
-
-        if current_critval > lambda_:
-            lambda_ = current_critval
-
-    return lambda_
+    return lagrange_mat
 
 
-def get_lagrange_polynomial(
-    sample,
-    interpolation_mat,
-    index,
-):
-    n_samples, n_params = sample.shape
+def _scaled_polynomial_features(x):
+    """Construct linear terms, interactions and scaled square terms.
 
-    if n_samples == n_params:
-        rhs = np.zeros(n_params)
-        rhs[index] = 1.0
-    elif n_samples == (n_params + 1) * (n_params + 2) // 2 - 1:
-        rhs = np.zeros(n_samples)
-        rhs[index] = 1.0
-    else:
-        rhs = np.zeros(n_samples + n_params)
-        rhs[index] = 1.0
+    The square terms are scaled by 1 / 2.
 
-    coef, *_ = np.linalg.lstsq(interpolation_mat, rhs, rcond=None)
+    Args:
+        x (np.ndarray): Array of shape (n_samples, n_params).
 
-    if n_samples == n_params:
-        linear_terms = coef
-        square_terms = np.zeros((n_params, n_params))
-    elif n_samples == (n_params + 1) * (n_params + 2) // 2 - 1:
-        linear_terms = coef[:n_params]
-        square_terms = _reshape_coef_to_square_terms(coef[n_params:], n_params)
-    else:
-        linear_terms = coef[n_samples:]
-        square_terms = np.zeros((n_params, n_params))
-        for i in range(n_samples):
-            x = sample[i]
-            square_terms += coef[i] * np.outer(x, x)
+    Returns:
+        np.ndarray: Linear terms, interactions and scaled square terms.
+            Has shape (n_samples, n_params * (n_params + 1) // 2).
 
-    return linear_terms, square_terms
+    """
+    n_samples, n_params = np.atleast_2d(x).shape
+    n_poly_terms = _n_second_order_terms(n_params)
 
+    poly_terms = np.empty((n_poly_terms, n_samples), np.float64)
+    xt = x.T
 
-def build_interpolation_matrix(sample, n_params, n_samples):
-    if n_samples == n_params:
-        out = sample.T
-    elif n_samples + 1 == (n_params + 1) * (n_params + 2) // 2:
-        out = np.empty((n_samples, n_samples))
-        out[:, :n_params] = sample.T
+    idx = 0
+    for i in range(n_params):
+        poly_terms[idx] = 0.5 * xt[i] ** 2
+        idx += 1
 
-        for i in range(n_samples):
-            _mat = np.outer(sample[:, i], sample[:, i]) - 0.5 * np.diag(
-                sample[:, i] * sample[:, i]
-            )
-            out[i, n_params:] = _reshape_mat_to_upper_triangular(
-                _mat,
-                n_params,
-            )
-    else:
-        out = np.zeros((n_samples + n_params, n_samples + n_params))
-        for i in range(n_samples):
-            for j in range(n_samples):
-                out[i, j] = 0.5 * (sample[:, i] @ sample[:, j]) ** 2
-        out[:n_samples, n_samples:] = sample.T
-        out[n_samples:, :n_samples] = sample
+        for j in range(i + 1, n_params):
+            poly_terms[idx] = xt[i] * xt[j]
+            idx += 1
 
-    return out
+    intercept = np.ones((1, n_samples), x.dtype)
+    out = np.concatenate((intercept, xt, poly_terms), axis=0)
+
+    return out.T
 
 
-def _reshape_coef_to_square_terms(vec, n_params):
+def _n_second_order_terms(dim):
+    """Number of free second order terms in a quadratic model."""
+    return dim * (dim + 1) // 2
+
+
+def _reshape_coef_to_square_terms(coef, n_params):
+    """Reshape coefficients to matrix of square terms."""
     mat = np.empty((n_params, n_params))
     idx = -1
 
     for j in range(n_params):
         for i in range(j + 1):
             idx += 1
-            mat[i, j] = vec[idx]
-            mat[j, i] = vec[idx]
+            mat[i, j] = coef[idx]
+            mat[j, i] = coef[idx]
 
     return mat
 
 
-def _reshape_mat_to_upper_triangular(mat, n_params):
-    triu = np.empty(n_params * (n_params + 1) // 2)
-    idx = -1
-
-    for j in range(n_params):
-        for i in range(j + 1):
-            idx += 1
-            triu[idx] = mat[i, j]
-
-    return triu
-
-
-def _evaluate_scalar_model(x, linear_terms, square_terms):
-    return linear_terms.T @ x + 0.5 * x.T @ square_terms @ x
+def _evaluate_scalar_model(x, intercept, linear_terms, square_terms):
+    return intercept + linear_terms.T @ x + 0.5 * x.T @ square_terms @ x
--- tests/optimization/tranquilo/test_poisedness.py
+++ tests/optimization/tranquilo/test_poisedness.py
@@ -1,222 +1,93 @@
 import numpy as np
 import pytest
-from estimagic.optimization.tranquilo.poisedness import build_interpolation_matrix
-from estimagic.optimization.tranquilo.poisedness import get_lagrange_polynomial
-from estimagic.optimization.tranquilo.poisedness import get_lambda_poisedness_constant
-from numpy.testing import assert_allclose
-from numpy.testing import assert_array_almost_equal
-
-
-def aaae(x, y, case=None):
-    tolerance = {
-        None: 7,
-        "hessian": 3,
-    }
-    assert_array_almost_equal(x, y, decimal=tolerance[case])
-
-
-TEST_CASES = [
-    (np.array([[1, 0.0], [0.0, 1]]), 1.0),
-    (
-        np.array(
-            [
-                [0.024, -0.4994],
-                [-0.468, -0.177],
-                [-0.313, 0.39],
-                [0.482, -0.132],
-            ]
-        ),
-        3.226777830135947,
-    ),
-]
-
-
-@pytest.mark.parametrize("sample, expected", TEST_CASES)
-def test_poisedness_constant(sample, expected):
-    n_params = 2
-
-    lower_bounds = -1 * np.ones(n_params)
-    upper_bounds = 1 * np.ones(n_params)
-
-    got = get_lambda_poisedness_constant(sample, lower_bounds, upper_bounds)
-
-    assert_allclose(got, expected, rtol=1e-2, atol=1e-8)
+from estimagic.optimization.tranquilo.poisedness import _evaluate_scalar_model
+from estimagic.optimization.tranquilo.poisedness import _reshape_coef_to_square_terms
+from estimagic.optimization.tranquilo.poisedness import get_lagrange_poly_matrix
+from numpy.testing import assert_array_almost_equal as aaae
 
 
 TEST_CASES = [
     (
+        np.array([[0, 0], [1, 0], [0, 1], [2, 0], [1, 1], [0, 2], [0.5, 0.5]]),
         np.array(
             [
-                [-0.45, -0.4],
-                [-0.4, -0.45],
-                [0.45, 0.4],
-                [0.4, 0.45],
-                [0.35, 0.35],
+                [
+                    1,
+                    -1.5,
+                    -1.5,
+                    1,
+                    1,
+                    1,
+                ],
+                [
+                    0,
+                    5 / 3,
+                    -1 / 3,
+                    -1.64705882e00,
+                    -7.64705882e-01,
+                    3.52941176e-01,
+                ],
+                [
+                    0,
+                    -1 / 3,
+                    5 / 3,
+                    3.52941176e-01,
+                    -7.64705882e-01,
+                    -1.64705882e00,
+                ],
+                [
+                    0,
+                    -5 / 12,
+                    1 / 12,
+                    9.11764706e-01,
+                    -5.88235294e-02,
+                    -8.82352941e-02,
+                ],
+                [
+                    -0,
+                    -1 / 6,
+                    -1 / 6,
+                    1.76470588e-01,
+                    1.11764706e00,
+                    1.76470588e-01,
+                ],
+                [
+                    0,
+                    1 / 12,
+                    -5 / 12,
+                    -8.82352941e-02,
+                    -5.88235294e-02,
+                    9.11764706e-01,
+                ],
+                [
+                    0,
+                    2 / 3,
+                    2 / 3,
+                    -7.05882353e-01,
+                    -4.70588235e-01,
+                    -7.05882353e-01,
+                ],
             ]
         ),
-        1179.497744338786,
+        np.array([1, 0.84, 0.84, 0.99, 0.96, 0.99, 0.37]),
     )
 ]
 
 
-@pytest.mark.parametrize("sample, expected", TEST_CASES)
-def test_poisedness_constant_imprecise(sample, expected):
-    n_params = 2
+@pytest.mark.parametrize("sample, expected_lagrange_mat, expected_critval", TEST_CASES)
+def test_lagrange_poly_matrix(sample, expected_lagrange_mat, expected_critval):
+    """This test case is taken from :cite:`Conn2009` p. 62."""
+    sample = np.array([[0, 0], [1, 0], [0, 1], [2, 0], [1, 1], [0, 2], [0.5, 0.5]])
+    n_params = sample.shape[1]
 
-    lower_bounds = -1 * np.ones(n_params)
-    upper_bounds = 1 * np.ones(n_params)
+    lagrange_mat = get_lagrange_poly_matrix(sample)
+    aaae(lagrange_mat, expected_lagrange_mat)
 
-    got = get_lambda_poisedness_constant(sample, lower_bounds, upper_bounds)
+    for idx, lagrange_poly in enumerate(lagrange_mat):
+        intercept = lagrange_poly[0]
+        linear_terms = lagrange_poly[1 : n_params + 1]
+        _coef_square_terms = lagrange_poly[n_params + 1 :]
+        square_terms = _reshape_coef_to_square_terms(_coef_square_terms, n_params)
 
-    assert 1178 <= got <= 1180
-
-
-TEST_CASES = [
-    (
-        np.array([[0.0, 0.0], [3.2, -0.1]]),
-        np.array([2.2, -0.1]),
-        [
-            [np.array([-0.0, 10.0]), np.zeros((2, 2))],
-            [np.array([1.0, 22.0]), np.zeros((2, 2))],
-        ],
-    ),
-    (
-        np.array([[0.0, 0.0], [1.0, 0.0], [-0.1, 0.0]]),
-        np.array([0.1, 0.9]),
-        [
-            [
-                np.array([7.0, -2.0]),
-                np.array(
-                    [
-                        [-2.00000000e01, -7.10542736e-15],
-                        [-7.10542736e-15, 0.00000000e00],
-                    ]
-                ),
-            ],
-            [
-                np.array([0.27272727, -0.02020202]),
-                np.array([[1.81818182, 0.0], [0.0, 0.0]]),
-            ],
-            [
-                np.array([-7.27272727, 0.90909091]),
-                np.array(
-                    [[1.81818182e01, 3.55271368e-15], [3.55271368e-15, 2.84217094e-14]]
-                ),
-            ],
-        ],
-    ),
-    (
-        np.array([[0.0, 0.0], [1.0, 0.0], [-0.1, 0.0], [-0.1, 2.0]]),
-        np.array([0.1, 0.9]),
-        [
-            [
-                np.array([6.44186047, -0.46511628]),
-                np.array([[-19.96554694, -0.31007752], [-0.31007752, 6.20155039]]),
-            ],
-            [
-                np.array([0.2670895, -0.00469814]),
-                np.array([[1.81852983, -0.0031321], [-0.0031321, 0.06264192]]),
-            ],
-            [
-                np.array([-6.8794926, -0.17230444]),
-                np.array([[18.15754444, 0.21846371], [0.21846371, -4.36927414]]),
-            ],
-            [
-                np.array([-0.13953488, 0.38372093]),
-                np.array([[0.00861326, -0.07751938], [-0.07751938, 1.5503876]]),
-            ],
-        ],
-    ),
-    (
-        np.array([[0.0, 0.0], [1.0, 0.0], [-0.1, 0.0], [-0.1, 2.0], [-1.1, 1.0]]),
-        np.array([0.1, 0.9]),
-        [
-            [
-                np.array([-10.37692308, -3.52307692]),
-                np.array([[-20.0, -19.30769231], [-19.30769231, -3.38461538]]),
-            ],
-            [
-                np.array([1.02027972, 0.13224553]),
-                np.array([[1.81818182, 0.83061383], [0.83061383, 0.33877234]]),
-            ],
-            [
-                np.array([9.31433566, 2.77202797]),
-                np.array([[18.18181818, 18.43006993], [18.43006993, 4.13986014]]),
-            ],
-            [
-                np.array([0.03461538, 0.41538462]),
-                np.array(
-                    [[1.23788147e-15, 3.84615385e-02], [3.84615385e-02, 9.23076923e-01]]
-                ),
-            ],
-            [
-                np.array([-0.76153846, -0.13846154]),
-                np.array(
-                    [
-                        [5.11125253e-16, -8.46153846e-01],
-                        [-8.46153846e-01, -3.07692308e-01],
-                    ]
-                ),
-            ],
-        ],
-    ),
-]
-
-
-@pytest.mark.parametrize("sample, center, expected", TEST_CASES)
-def test_get_lagrange_polynomial(sample, center, expected):
-
-    n_samples, n_params = sample.shape
-    sample_centered = sample - center
-
-    interpolation_mat = build_interpolation_matrix(
-        sample_centered.T, n_params, n_samples
-    )
-
-    for index in range(n_samples):
-        linear_terms, square_terms = get_lagrange_polynomial(
-            sample,
-            interpolation_mat,
-            index,
-        )
-        aaae(linear_terms, expected[index][0])
-        aaae(square_terms, expected[index][1])
-
-
-TEST_CASES = [
-    (np.array([[-2.2, 0.1], [1.0, 0.0]]), np.array([[-2.2, 0.1], [1.0, 0.0]])),
-    (
-        np.array([[-0.1, -0.9], [0.9, -0.9], [-0.2, -0.9]]),
-        np.array(
-            [
-                [0.3362, 0.2592, 0.34445, -0.1, -0.9],
-                [0.2592, 1.3122, 0.19845, 0.9, -0.9],
-                [0.34445, 0.19845, 0.36125, -0.2, -0.9],
-                [-0.1, 0.9, -0.2, 0.0, 0.0],
-                [-0.9, -0.9, -0.9, 0.0, 0.0],
-            ]
-        ),
-    ),
-    (
-        np.array([[-0.1, -0.9], [0.9, -0.9], [-0.2, -0.9], [-0.2, 1.1]]),
-        np.array(
-            [
-                [0.3362, 0.2592, 0.34445, 0.47045, -0.1, -0.9],
-                [0.2592, 1.3122, 0.19845, 0.68445, 0.9, -0.9],
-                [0.34445, 0.19845, 0.36125, 0.45125, -0.2, -0.9],
-                [0.47045, 0.68445, 0.45125, 0.78125, -0.2, 1.1],
-                [-0.1, 0.9, -0.2, -0.2, 0.0, 0.0],
-                [-0.9, -0.9, -0.9, 1.1, 0.0, 0.0],
-            ]
-        ),
-    ),
-]
-
-
-@pytest.mark.parametrize("sample, expected", TEST_CASES)
-def test_get_interpolation_matrix(sample, expected):
-    sample_t = sample.T
-    n_params, n_samples = sample_t.shape
-
-    mat = build_interpolation_matrix(sample_t, n_params, n_samples)
-    aaae(mat, expected)
+        got = _evaluate_scalar_model(sample[idx], intercept, linear_terms, square_terms)
+        aaae(got, expected_critval[idx], decimal=2)
