ce894b4b54a8959b0585ee41c7d0ef0f0bee24f1
Janos Gabler <janos.gabler@gmail.com>
2023-01-27 11:24:19 +0100

    Fix pre-commits.

--- tests/optimization/tranquilo/test_clustering.py
+++ tests/optimization/tranquilo/test_clustering.py
@@ -9,9 +9,12 @@ def test_cluster_lollipop():
     radius = 0.05
 
     x = np.array(
-        [center]
-        + (center + rng.uniform(low=-radius, high=radius, size=(6, 2))).tolist()
-        + [[0.5, 0.5], [0.75, 0.75]]
+        [
+            center,
+            *(center + rng.uniform(low=-radius, high=radius, size=(6, 2))).tolist(),
+            [0.5, 0.5],
+            [0.75, 0.75],
+        ],
     )
 
     clusters, centers = cluster(x, epsilon=0.1)
