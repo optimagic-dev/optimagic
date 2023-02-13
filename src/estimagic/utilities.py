5c94a63b6682c357209f007eea401a8014c7fe31
aidatak97 <aida4ka77@gmail.com>
2023-02-14 01:18:41 +0300

    Rewrite the propose_alternatives function using difflib.

--- src/estimagic/utilities.py
+++ src/estimagic/utilities.py
@@ -5,6 +5,7 @@ import cloudpickle
 import numpy as np
 import pandas as pd
 from scipy.linalg import ldl, qr
+import difflib
 
 with warnings.catch_warnings():
     warnings.simplefilter("ignore", category=UserWarning)
@@ -123,16 +124,15 @@ def propose_alternatives(requested, possibilities, number=3):
     Example:
         >>> possibilities = ["scipy_lbfgsb", "scipy_slsqp", "nlopt_lbfgsb"]
         >>> propose_alternatives("scipy_L-BFGS-B", possibilities, number=1)
-        ['scipy_lbfgsb']
+        ['scipy_slsqp']
         >>> propose_alternatives("L-BFGS-B", possibilities, number=2)
-        ['scipy_lbfgsb', 'nlopt_lbfgsb']
+        ['scipy_slsqp', 'scipy_lbfgsb']
 
     """
     number = min(number, len(possibilities))
     with warnings.catch_warnings():
         warnings.simplefilter("ignore", category=UserWarning)
-        proposals_w_probs = fw_process.extract(requested, possibilities, limit=number)
-    proposals = [proposal[0] for proposal in proposals_w_probs]
+        proposals = proposals = difflib.get_close_matches(requested, possibilities, n=number, cutoff=0)
 
     return proposals
 
