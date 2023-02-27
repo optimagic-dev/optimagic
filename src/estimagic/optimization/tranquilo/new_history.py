83cda13326307e3dfbaf7f2be540392c5f129e12
Janos Gabler <janos.gabler@gmail.com>
2023-02-27 18:53:39 +0100

    Redo history class.

---
+++ src/estimagic/optimization/tranquilo/new_history.py
@@ -0,0 +1,303 @@
+import numpy as np
+from numba import njit
+
+
+class History:
+    """Container to save and retrieve history entries.
+
+    These entries are: xs, fvecs and fvals.
+
+    fvals don't need to be added explicitly, as they are computed internally
+    whenever new entries are added.
+
+    """
+
+    def __init__(self, functype):
+        self.xs = None
+        self.fvecs = None
+        self.fvals = None
+        self.n_xs = 0
+        self.n_fun = 0
+        self.index_mapper = {}
+
+        self.functype = functype
+
+        if functype == "scalar":
+            self.aggregate = lambda x: x.flatten()
+        elif functype == "likelihood":
+            self.aggregate = lambda x: x.sum(axis=-1)
+        elif functype == "least_squares":
+            self.aggregate = lambda x: (x**2).sum(axis=-1)
+        else:
+            raise ValueError(
+                "funtype must be 'scalar', 'likelihood' or 'least_squares'."
+            )
+
+    def add_xs(self, xs):
+        """Add new parameter vectors to the history and return their indices.
+
+        Args:
+            xs (np.ndarray or list): 1d or 2d array or list of 1d arrays with
+                parameter vectors.
+
+        Returns:
+            np.ndarray: 1d array with indices of the added xs.
+
+        """
+        xs = np.atleast_2d(xs)
+
+        n_new_points = len(xs) if xs.size != 0 else 0
+
+        if n_new_points == 0:
+            return
+
+        self.xs = _add_entries_to_array(self.xs, xs, self.n_xs)
+
+        x_indices = np.arange(self.n_xs, self.n_xs + n_new_points)
+
+        for x_index in x_indices:
+            self.index_mapper[x_index] = []
+
+        self.n_xs += n_new_points
+
+        return x_indices
+
+    def add_evals(self, x_indices, evals):
+        """Add new function evaluations to the history.
+
+        Args:
+            x_indices (int, list or np.ndarray): Indices of the xs at which the function
+                was evaluated.
+            evals (np.ndarray or list): 1d or 2d array or list of 1d arrays with
+                least square fvecs.
+
+        """
+        x_indices = np.atleast_1d(x_indices)
+
+        if not (x_indices < self.n_xs).all():
+            raise ValueError(
+                "You requested to store a function evaluation for an x vector that is "
+                "not in the history."
+            )
+
+        n_new_points = len(x_indices)
+
+        if n_new_points == 0:
+            return
+
+        if self.functype == "scalar":
+            fvecs = np.reshape(evals, (-1, 1))
+        else:
+            fvecs = np.atleast_2d(evals)
+
+        fvals = np.atleast_1d(self.aggregate(fvecs))
+
+        if n_new_points != len(fvecs):
+            raise ValueError()
+
+        self.fvecs = _add_entries_to_array(self.fvecs, fvecs, self.n_fun)
+        self.fvals = _add_entries_to_array(self.fvals, fvals, self.n_fun)
+
+        f_indices = np.arange(self.n_fun, self.n_fun + n_new_points)
+
+        for x_index, f_index in zip(x_indices, f_indices):
+            self.index_mapper[x_index].append(f_index)
+
+        self.n_fun += n_new_points
+
+    def get_xs(self, x_indices=None):
+        """Retrieve xs from history.
+
+        Args:
+            x_indices (int, slice or sequence): Specifies the subset of rows that will
+                be returned. Anything that can be used to index into a 1d numpy array
+                is allowed.
+
+        Returns:
+            np.ndarray: 1d or 2d array with parameter vectors
+
+        """
+        out = self.xs[: self.n_fun]
+        out = out[x_indices] if x_indices is not None else out
+
+        return out
+
+    def get_fvecs(self, x_indices):
+        """Retrieve fvecs from history.
+
+        Args:
+            x_indices (int, slice or sequence): Specifies the subset of rows that will
+                be returned. Anything that can be used to index into a 1d numpy array
+                is allowed.
+
+        Returns:
+            np.ndarray or dict: If x_indices is a scalar, a single array is returned.
+                Otherwise, a dictionary with x_indices as keys and arrays as values is
+                returned.
+
+        """
+        out = _extract_from_indices(
+            arr=self.fvecs[: self.n_fun],
+            mapper=self.index_mapper,
+            x_indices=x_indices,
+            n_xs=self.n_xs,
+        )
+        return out
+
+    def get_fvals(self, x_indices):
+        """Retrieve fvals from history.
+
+        Args:
+            x_indices (int, slice or sequence): Specifies the subset of rows that will
+                be returned. Anything that can be used to index into a 1d numpy array
+                is allowed.
+
+        Returns:
+            np.ndarray or dict: If x_indices is a scalar, a single array is returned.
+                Otherwise, a dictionary with x_indices as keys and arrays as values is
+                returned.
+
+        """
+        out = _extract_from_indices(
+            arr=self.fvals[: self.n_fun],
+            mapper=self.index_mapper,
+            x_indices=x_indices,
+            n_xs=self.n_xs,
+        )
+        return out
+
+    def get_model_data(self, x_indices, region=None, scale=False, average=True):
+        if np.isscalar(x_indices):
+            x_indices = [x_indices]
+
+        raw_xs = self.get_xs(x_indices)
+        raw_fvecs = self.get_fvecs(x_indices)
+
+        if average:
+            fvecs = np.array([np.mean(fvec, axis=0) for fvec in raw_fvecs.values()])
+            xs = raw_xs
+        else:
+            fvecs = np.vstack(list(raw_fvecs.values()))
+            n_obs = np.array([len(fvec) for fvec in raw_fvecs.values()])
+            xs = np.repeat(raw_xs, n_obs, axis=0)
+
+        if scale:
+            if region is None:
+                raise ValueError("You need to provide a region to scale the data.")
+            xs = (xs - region.center) / region.radius
+
+        return xs, fvecs
+
+    def get_n_fun(self):
+        return self.n_fun
+
+    def get_n_xs(self):
+        return self.n_xs
+
+    def get_x_indices_in_region(self, region):
+        # early return if there are no entries
+        if self.get_n_fun() == 0:
+            return np.array([])
+
+        center = region.center
+        shape = region.shape
+        radius = region.radius
+
+        if isinstance(center, int):
+            center = self.get_xs(x_indices=center)
+
+        xs = self.get_xs()
+
+        out = _find_indices_in_trust_region(xs, center=center, radius=radius)
+
+        if shape == "sphere":
+            mask = np.linalg.norm(xs[out] - region.center, axis=1) <= radius
+            out = out[mask]
+
+        return out
+
+    def __repr__(self):
+        return f"History for {self.functype} function with {self.n_fun} entries."
+
+
+def _add_entries_to_array(arr, new, position):
+    if arr is None:
+        shape = 1_000 if new.ndim == 1 else (1_000, new.shape[1])
+        arr = np.full(shape, np.nan)
+
+    n_new_points = len(new) if new.size != 0 else 0
+
+    if len(arr) - position - n_new_points < 0:
+        n_extend = max(len(arr), n_new_points)
+        if arr.ndim == 2:
+            extension_shape = (n_extend, arr.shape[1])
+            arr = np.vstack([arr, np.full(extension_shape, np.nan)])
+        else:
+            arr = np.hstack([arr, np.full(n_extend, np.nan)])
+
+    arr[position : position + n_new_points] = new
+
+    return arr
+
+
+@njit
+def _find_indices_in_trust_region(xs, center, radius):
+    """Get the row indices of all parameter vectors in a trust region.
+
+    This is for square trust regions, i.e. balls in term of an infinity norm.
+
+    Args:
+        xs (np.ndarray): 2d numpy array where each row is a parameter vector.
+        center (np.ndarray): 1d numpy array that marks the center of the trust region.
+        radius (float): Radius of the trust region.
+
+    Returns:
+        np.ndarray: The indices of parameters in the trust region.
+
+    """
+    n_obs, dim = xs.shape
+    out = np.zeros(n_obs).astype(np.int64)
+    success_counter = 0
+    upper = center + radius
+    lower = center - radius
+    for i in range(n_obs):
+        success = True
+        for j in range(dim):
+            value = xs[i, j]
+            if not (lower[j] <= value <= upper[j]) or np.isnan(value):
+                success = False
+                continue
+        if success:
+            out[success_counter] = i
+            success_counter += 1
+
+    return out[:success_counter]
+
+
+def _extract_from_indices(arr, mapper, x_indices, n_xs):
+    """Retrieve fvecs or fvals from history.
+
+    Args:
+        arr (np.ndarray): 1d or 2d Array with function values.
+        mapper (dict): Maps x indices to f indices.
+        x_indices (None, int or np.ndarray): Specifies the subset of parameter
+            vectors for which the function values will be returned.
+
+    Returns:
+        dict or np.ndarray: If x_indices is a scalar, a single array is returned.
+            Otherwise, a dictionary with x_indices as keys and arrays as values is
+            returned.
+
+    """
+    is_single = np.isscalar(x_indices)
+    if is_single:
+        x_indices = [x_indices]
+
+    indices = np.arange(n_xs)[x_indices].tolist()
+
+    out = {i: arr[mapper[i]] for i in indices}
+
+    if is_single:
+        out = out[0]
+
+    return out
---
+++ tests/optimization/tranquilo/test_new_history.py
@@ -0,0 +1,244 @@
+"""Test the history class for least-squares optimizers."""
+from collections import namedtuple
+
+import numpy as np
+import pytest
+from estimagic.optimization.tranquilo.new_history import History
+from estimagic.optimization.tranquilo.options import Region
+from numpy.testing import assert_array_almost_equal as aaae
+
+XS = [
+    np.arange(3),
+    np.arange(3).tolist(),
+    np.arange(3).reshape(1, 3),
+    np.arange(3).reshape(1, 3).tolist(),
+]
+
+
+@pytest.mark.parametrize("xs", XS)
+def test_add_xs_not_initialized(xs):
+    history = History(functype="least_squares")
+
+    new_indices = history.add_xs(xs)
+
+    if len(xs) == 1:
+        aaae(new_indices, np.array([0]))
+    else:
+        assert new_indices == 0
+
+    assert isinstance(history.xs, np.ndarray)
+    aaae(history.xs[0], np.arange(3))
+
+    assert history.index_mapper == {0: []}
+    assert history.n_xs == 1
+    assert history.n_fun == 0
+
+
+@pytest.mark.parametrize("xs", XS)
+def test_add_xs_initialized_with_space(xs):
+    history = History(functype="least_squares")
+
+    history.add_xs(np.ones((20, 3)))
+    new_indices = history.add_xs(xs)
+
+    if len(xs) == 1:
+        aaae(new_indices, np.array([20]))
+    else:
+        assert new_indices == 20
+
+    assert isinstance(history.xs, np.ndarray)
+    aaae(history.xs[:21], np.vstack([np.ones((20, 3)), np.arange(3)]))
+
+    assert history.index_mapper == {i: [] for i in range(21)}
+    assert history.n_xs == 21
+    assert history.n_fun == 0
+
+
+@pytest.mark.parametrize("xs", XS)
+def test_add_xs_initialized_extension_needed(xs):
+    history = History(functype="least_squares")
+
+    history.add_xs(np.ones(3))
+    initial_size = len(history.xs)
+    history.add_xs(np.ones((initial_size - 1, 3)))
+    history.add_xs(xs)
+
+    assert len(history.xs) > initial_size
+
+    aaae(history.xs[initial_size], np.arange(3))
+
+    assert history.n_xs == initial_size + 1
+    assert history.n_fun == 0
+
+
+EVALS = [
+    (0, np.arange(5)),
+    ([0], [np.arange(5)]),
+    (np.array([0]), np.arange(5).reshape(1, 5)),
+]
+
+
+@pytest.mark.parametrize("x_indices, evals", EVALS)
+def test_add_evals_not_initialized(x_indices, evals):
+    history = History(functype="least_squares")
+    history.add_xs(np.arange(3))
+
+    history.add_evals(x_indices, evals)
+
+    assert history.get_n_fun() == 1
+    assert history.get_n_xs() == 1
+
+    aaae(history.fvecs[0], np.arange(5))
+    aaae(history.fvals[0], 30.0)
+
+    assert history.index_mapper == {0: [0]}
+
+
+@pytest.mark.parametrize("evals", [tup[1] for tup in EVALS])
+def test_add_evals_initialized_with_space(evals):
+    history = History(functype="least_squares")
+    history.add_xs(np.arange(6).reshape(2, 3))
+    history.add_evals([0] * 20, np.ones((20, 5)))
+
+    history.add_evals(1, evals)
+
+    assert history.get_n_fun() == 21
+    assert history.get_n_xs() == 2
+
+    aaae(history.fvecs[:21], np.vstack([np.ones((20, 5)), np.arange(5)]))
+    aaae(history.fvals[20], 30.0)
+
+    assert history.index_mapper == {0: list(range(20)), 1: [20]}
+
+
+def test_get_indices_in_trustregion():
+    history = History(functype="least_squares")
+    xs = [[1, 1], [1.1, 1.2], [1.5, 1], [0.9, 0.9]]
+    fvecs = np.zeros((4, 3))
+    indices = history.add_xs(xs)
+    history.add_evals(indices, fvecs)
+
+    trustregion = namedtuple("Region", ["center", "radius", "shape"])(
+        center=np.ones(2),
+        radius=0.3,
+        shape="sphere",
+    )
+
+    indices = history.get_x_indices_in_region(trustregion)
+
+    aaae(indices, np.array([0, 1, 3]))
+
+
+@pytest.fixture()
+def history():
+    history = History(functype="least_squares")
+    xs = np.arange(15).reshape(5, 3)
+    fvecs = np.arange(25).reshape(5, 5)
+    indices = history.add_xs(xs)
+    history.add_evals(indices, fvecs)
+    return history
+
+
+def test_get_xs_no_indices(history):
+    xs = history.get_xs()
+    aaae(xs, np.arange(15).reshape(5, 3))
+
+
+def test_get_xs_with_indices(history):
+    xs = history.get_xs([0, 2, 4])
+    aaae(xs, np.arange(15).reshape(5, 3)[[0, 2, 4]])
+
+
+def test_get_xs_scalar_index(history):
+    xs = history.get_xs(0)
+    aaae(xs, np.arange(3))
+
+
+def test_add_eval_for_invalid_x(history):
+    with pytest.raises(ValueError):
+        history.add_evals(5, np.arange(5))
+
+
+def test_get_fvecs_scalar_index(history):
+    fvecs = history.get_fvecs(0)
+    aaae(fvecs, np.arange(5).reshape(1, 5))
+
+
+def test_get_fvecs_with_indices(history):
+    fvecs = history.get_fvecs([0])
+    assert isinstance(fvecs, dict)
+    assert len(fvecs) == 1
+    assert 0 in fvecs
+    aaae(fvecs[0], np.arange(5).reshape(1, 5))
+
+
+def test_get_fvals_scalar_index(history):
+    fvals = history.get_fvals(0)
+    aaae(fvals, 30.0)
+
+
+def test_get_fvals_with_indices(history):
+    fvals = history.get_fvals([0])
+    assert isinstance(fvals, dict)
+    assert len(fvals) == 1
+    assert 0 in fvals
+    aaae(fvals[0], 30.0)
+
+
+@pytest.mark.parametrize("average", [True, False])
+def test_get_model_data_trivial_averaging(history, average):
+    got_xs, got_fvecs = history.get_model_data(
+        x_indices=[0, 1],
+        region=None,
+        scale=False,
+        average=average,
+    )
+
+    aaae(got_xs, np.arange(6).reshape(2, 3))
+    aaae(got_fvecs, np.arange(10).reshape(2, 5))
+
+
+@pytest.mark.parametrize("scale", [True, False])
+def test_get_model_data_trivial_scaling(history, scale):
+    got_xs, got_fvecs = history.get_model_data(
+        x_indices=[0, 1],
+        region=Region(center=np.zeros(3) + 0.5, radius=0.5, shape="sphere"),
+        scale=scale,
+    )
+    if scale:
+        aaae(got_xs, (np.arange(6).reshape(2, 3) - 0.5) * 2)
+    else:
+        aaae(got_xs, np.arange(6).reshape(2, 3))
+    aaae(got_fvecs, np.arange(10).reshape(2, 5))
+
+
+@pytest.fixture()
+def noisy_history():
+    history = History(functype="least_squares")
+    history.add_xs(np.arange(6).reshape(2, 3))
+    fvecs = np.arange(25).reshape(5, 5)
+    history.add_evals([0, 0, 1, 1, 1], fvecs)
+    return history
+
+
+@pytest.mark.parametrize("average", [True, False])
+def test_get_model_data_with_repeated_evaluations(noisy_history, average):
+    got_xs, got_fvecs = noisy_history.get_model_data(
+        x_indices=[0, 1],
+        region=None,
+        scale=False,
+        average=average,
+    )
+
+    if average:
+        aaae(got_xs, np.arange(6).reshape(2, 3))
+        expected_fvecs = np.array(
+            [
+                np.arange(10).reshape(2, 5).mean(axis=0),
+                np.arange(10, 25).reshape(3, 5).mean(axis=0),
+            ]
+        )
+        aaae(got_fvecs, expected_fvecs)
+    else:
+        aaae(got_xs, np.arange(6).reshape(2, 3).repeat([2, 3], axis=0))
+        aaae(got_fvecs, np.arange(25).reshape(5, 5))
