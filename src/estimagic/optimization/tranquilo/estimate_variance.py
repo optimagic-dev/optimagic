Fix in variance estimation.

src/estimagic/optimization/tranquilo/estimate_variance.py
@@ -49,17 +49,17 @@ def _estimate_variance_classic(
     weights = np.array([n_ for idx, n_ in n_evals.items() if idx in valid_indices])
     weights = weights / weights.sum()
 
-    samples = list(history.get_fvecs(valid_indices).values())
-
-    dim = samples[0].shape[1]
-
-    cov = np.zeros((dim, dim))
-    for weight, sample in zip(weights, samples):
-        cov += weight * np.cov(sample, rowvar=False, ddof=1)
-
     if model_type == "scalar":
-        out = cov[0, 0]
+        samples = list(history.get_fvals(valid_indices).values())
+        out = 0.0
+        for weight, sample in zip(weights, samples):
+            out += weight * np.var(sample, ddof=1)
     else:
-        out = cov
+        samples = list(history.get_fvecs(valid_indices).values())
+
+        dim = samples[0].shape[1]
+        out = np.zeros((dim, dim))
+        for weight, sample in zip(weights, samples):
+            out += weight * np.cov(sample, rowvar=False, ddof=1)
 
     return out

tests/optimization/tranquilo/test_estimate_variance.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pytest
 from estimagic.optimization.tranquilo.estimate_variance import (
     _estimate_variance_classic,
 )
@@ -7,7 +8,8 @@ from estimagic.optimization.tranquilo.tranquilo import Region
 from numpy.testing import assert_array_almost_equal as aaae
 
 
-def test_estimate_variance_classic():
+@pytest.mark.parametrize("model_type", ["scalar", "vector"])
+def test_estimate_variance_classic(model_type):
     xs = np.array(
         [
             [0.0, 0.0],  # center with multiple evaluations
@@ -29,11 +31,14 @@ def test_estimate_variance_classic():
     got = _estimate_variance_classic(
         trustregion=Region(center=np.array([0.0, 0.0]), radius=1.0, shape="sphere"),
         history=history,
-        model_type="scalar",
+        model_type=model_type,
         max_distance_factor=1.0,
         min_n_evals=4,
     )
 
-    expected = np.var(evals[:5], ddof=1)
+    if model_type == "scalar":
+        expected = np.var(evals[:5], ddof=1)
+    else:
+        expected = np.var(evals[:5], ddof=1).reshape(1, 1)
 
     aaae(got, expected)