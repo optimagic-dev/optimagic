1507083bfacb09b25b9a7bb4e280b9888e9072cb
timmens <mensingertim@gmail.com>
2023-03-28 16:36:06 +0200

    Add early return to get_x_indices_in_region

--- src/estimagic/optimization/tranquilo/history.py
+++ src/estimagic/optimization/tranquilo/history.py
@@ -197,6 +197,9 @@ class History:
         return self.n_xs
 
     def get_x_indices_in_region(self, region):
+        # early return if there are no entries
+        if self.get_n_fun() == 0:
+            return np.array([])
         xs = self.get_xs()
         mask = np.linalg.norm(xs - region.center, axis=1) <= region.radius
         out = np.arange(len(mask))[mask]
