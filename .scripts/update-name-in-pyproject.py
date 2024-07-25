5695ed89a7f91e149d86304bd9f2e49a12fea478
Tim Mensinger <mensingertim@gmail.com>
2024-07-25 15:25:38 +0200

    Fix faulty pyproject updating

--- .scripts/update-name-in-pyproject.py
+++ .scripts/update-name-in-pyproject.py
@@ -3,7 +3,10 @@ from pathlib import Path
 
 file_path = Path("pyproject.toml")
 
-with file_path.open("r+") as f:
+with file_path.open("r") as f:
     config = toml.load(f)
-    config["project"]["name"] = "estimagic"
+
+config["project"]["name"] = "estimagic"
+
+with file_path.open("w") as f:
     toml.dump(config, f)
