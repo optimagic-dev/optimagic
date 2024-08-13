a8570cdb7dbccf726a9e8132cea33c110f55444d
Kristof Schroeder <kristof_schroeder@web.de>
2024-08-13 14:39:08 +0200

    A docstrings to dataclasses and enumerations in logging.types

--- src/optimagic/logging/types.py
+++ src/optimagic/logging/types.py
@@ -15,18 +15,51 @@ from optimagic.typing import (
 
 
 class StepStatus(str, Enum):
+    """Status of a step in a process.
+
+    Attributes:
+        SCHEDULED: Indicates that the step is scheduled but not yet started.
+        RUNNING: Indicates that the step is currently in progress.
+        COMPLETE: Indicates that the step has completed successfully.
+        SKIPPED: Indicates that the step was skipped.
+
+    """
+
     SCHEDULED = "scheduled"
     RUNNING = "running"
     COMPLETE = "complete"
     SKIPPED = "skipped"
 
 
+StepStatusLiteral = Literal["scheduled", "running", "complete", "skipped"]
+
+
 class StepType(str, Enum):
+    """Type of step in a process.
+
+    Attributes:
+        OPTIMIZATION: Represents an optimization step.
+        EXPLORATION: Represents an exploration step.
+
+    """
+
     OPTIMIZATION = "optimization"
     EXPLORATION = "exploration"
 
 
+StepTypeLiteral = Literal["optimization", "exploration"]
+
+
 class ExistenceStrategy(str, Enum):
+    """Strategies to handle the existence of a database or table.
+
+    Attributes:
+        RAISE: Raise an error if the database resp. table exists.
+        EXTEND: Extend the existing database or table.
+        REPLACE: Replace the existing database or table.
+
+    """
+
     RAISE = "raise"
     EXTEND = "extend"
     REPLACE = "replace"
@@ -37,6 +70,21 @@ ExistenceStrategyLiteral = Literal["raise", "extend", "replace"]
 
 @dataclass(frozen=True)
 class CriterionEvaluationResult(DictLikeAccess):
+    """Result of a criterion evaluation.
+
+    Attributes:
+        params: The parameters used in the evaluation.
+        timestamp: The time at which the evaluation was performed.
+        value: The result value of the evaluation.
+        valid: Indicates if the evaluation is valid.
+        criterion_eval: Optional, additional evaluation information.
+        internal_derivative: Optional, derivative information used internally.
+        step: Optional, step number associated with the evaluation.
+        exceptions: Optional, exceptions encountered during evaluation.
+        hash: Optional, hash of the evaluation for identification purposes.
+
+    """
+
     params: PyTree
     timestamp: float
     value: float
@@ -50,6 +98,16 @@ class CriterionEvaluationResult(DictLikeAccess):
 
 @dataclass(frozen=True)
 class CriterionEvaluationWithId(CriterionEvaluationResult):
+    """Criterion evaluation result with an ID.
+
+    Attributes:
+        rowid: The unique ID associated with the evaluation result.
+
+    Raises:
+        ValueError: If `rowid` is None.
+
+    """
+
     rowid: int | None = None
 
     def __post_init__(self) -> None:
@@ -59,9 +117,19 @@ class CriterionEvaluationWithId(CriterionEvaluationResult):
 
 @dataclass(frozen=True)
 class StepResult(DictLikeAccess):
+    """Result of a process step.
+
+    Attributes:
+        name: The name of the step.
+        type: The type of the step, either as `StepType` or string.
+        status: The status of the step, either as `StepStatus` or string.
+        n_iterations: Optional, the number of iterations performed in the step.
+
+    """
+
     name: str
-    type: StepType | str
-    status: StepStatus | str
+    type: StepType | StepTypeLiteral
+    status: StepStatus | StepStatusLiteral
     n_iterations: int | None = None
 
     def __post_init__(self) -> None:
@@ -73,6 +141,16 @@ class StepResult(DictLikeAccess):
 
 @dataclass(frozen=True)
 class StepResultWithId(StepResult):
+    """Step result with an ID.
+
+    Attributes:
+        rowid: The unique ID associated with the step result.
+
+    Raises:
+        ValueError: If `rowid` is None.
+
+    """
+
     rowid: int | None = None
 
     def __post_init__(self) -> None:
@@ -83,12 +161,31 @@ class StepResultWithId(StepResult):
 
 @dataclass(frozen=True)
 class ProblemInitialization(DictLikeAccess):
+    """Start characteristics of an optimization problem.
+
+    Attributes:
+        direction: The direction of optimization,
+            either as `OptimizationType` or string literal.
+        params: The parameters for the initialization.
+
+    """
+
     direction: OptimizationType | OptimizationTypeLiteral
     params: PyTree
 
 
 @dataclass(frozen=True)
 class ProblemInitializationWithId(ProblemInitialization):
+    """Problem initialization with an ID.
+
+    Attributes:
+        rowid: The unique ID associated with the problem initialization.
+
+    Raises:
+        ValueError: If `rowid` is None.
+
+    """
+
     rowid: int | None = None
 
     def __post_init__(self) -> None:
@@ -98,6 +195,15 @@ class ProblemInitializationWithId(ProblemInitialization):
 
 @dataclass(frozen=True)
 class IterationHistory(DictLikeAccess):
+    """History of iterations in a process.
+
+    Attributes:
+        params: A list of parameters used in each iteration.
+        criterion: A list of criterion values obtained in each iteration.
+        runtime: A list or array of runtimes associated with each iteration.
+
+    """
+
     params: list[PyTree]
     criterion: list[float]
     runtime: list[float] | NDArray[np.float64]
@@ -105,6 +211,15 @@ class IterationHistory(DictLikeAccess):
 
 @dataclass(frozen=True)
 class MultiStartIterationHistory(TupleLikeAccess):
+    """History of multiple start iterations.
+
+    Attributes:
+        history: The main iteration history, representing the best end value.
+        local_histories: Optional, a list of local iteration histories.
+        exploration: Optional, iteration history for exploration steps.
+
+    """
+
     history: IterationHistory
     local_histories: list[IterationHistory] | None = None
     exploration: IterationHistory | None = None
