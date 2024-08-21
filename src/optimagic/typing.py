a04f32753cc5158bccdb5a628c973c466a4729ed
Janos Gabler <janos.gabler@gmail.com>
2024-08-21 09:34:39 +0200

    Wrap parallel neldermead.

--- src/optimagic/typing.py
+++ src/optimagic/typing.py
@@ -122,6 +122,7 @@ NegativeFloat = Annotated[float, Lt(0)]
 GtOneFloat = Annotated[float, Gt(1)]
 YesNoBool = Literal["yes", "no"] | bool
 DirectionLiteral = Literal["minimize", "maximize"]
+BatchEvaluatorLiteral = Literal["joblib", "pathos"]
 
 
 @dataclass(frozen=True)
