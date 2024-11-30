d18b63c0770cb9e0e27e69a4b791e5cf43dd6f89
Tim Mensinger <mensingertim@gmail.com>
2024-11-30 11:52:50 +0100

    Add attributes of HistoryArrays to History

--- src/optimagic/optimization/history.py
+++ src/optimagic/optimization/history.py
@@ -1,8 +1,11 @@
 import warnings
 from dataclasses import dataclass
+from functools import partial
 from typing import Any
 
 import numpy as np
+from numpy.typing import NDArray
+from pybaum import get_registry, tree_just_flatten
 
 from optimagic.typing import Direction, EvalTask, PyTree
 
@@ -34,12 +37,18 @@ class History:
         recover a history from a log.
 
         """
+        if direction not in [Direction.MINIMIZE, Direction.MAXIMIZE]:
+            raise ValueError(f"Invalid direction: {direction}.")
         self.direction = direction
         self._params = params if params is not None else []
         self._fun = fun if fun is not None else []
         self._time = time if time is not None else []
         self._batches = batches if batches is not None else []
         self._task = task if task is not None else []
+
+    # ==================================================================================
+    # Methods to add entries to the history
+    # ==================================================================================
 
     def add_entry(self, entry: HistoryEntry, batch_id: int | None = None) -> None:
         if batch_id is None:
@@ -67,13 +76,66 @@ class History:
         for entry, id in zip(batch, ids, strict=False):
             self.add_entry(entry, id)
 
+    def _get_next_batch_id(self) -> int:
+        if not self._batches:
+            batch = 0
+        else:
+            batch = self._batches[-1] + 1
+        return batch
+
+    # ==================================================================================
+    # Properties to access the history
+    # ==================================================================================
+
+    # Function value and monotone function value
+    # ----------------------------------------------------------------------------------
+
+    @property
+    def fun(self) -> list[float | None]:
+        return self._fun
+
+    @property
+    def fun_array(self) -> NDArray[np.float64]:
+        return np.array(self._valid_fun, dtype=np.float64)
+
+    @property
+    def _valid_fun(self) -> list[float]:
+        return [f for f in self.fun if f is not None]
+
+    @property
+    def monotone_fun(self) -> NDArray[np.float64]:
+        if self.direction == Direction.MINIMIZE:
+            return np.minimum.accumulate(self.fun_array)
+        elif self.direction == Direction.MAXIMIZE:
+            return np.maximum.accumulate(self.fun_array)
+
+    # Acceptance
+    # ----------------------------------------------------------------------------------
+
+    @property
+    def is_accepted(self) -> NDArray[np.bool_]:
+        if self.direction == Direction.MINIMIZE:
+            return self.fun_array <= self.monotone_fun
+        elif self.direction == Direction.MAXIMIZE:
+            return self.fun_array >= self.monotone_fun
+
+    # Parameters
+    # ----------------------------------------------------------------------------------
+
     @property
     def params(self) -> list[PyTree]:
         return self._params
 
     @property
-    def fun(self) -> list[float | None]:
-        return self._fun
+    def flat_params_array(self) -> NDArray[np.float64]:
+        return np.array(_flatten_params(self._valid_params), dtype=np.float64)
+
+    @property
+    def _valid_params(self) -> list[PyTree]:
+        return [p for p, f in zip(self.params, self.fun, strict=True) if f is not None]
+
+    # Time
+    # ----------------------------------------------------------------------------------
 
     @property
     def time(self) -> list[float]:
@@ -81,19 +143,26 @@ class History:
         return (arr - arr[0]).tolist()
 
     @property
+    def time_array(self) -> NDArray[np.float64]:
+        return np.array(self._valid_time, dtype=np.float64)
+
+    @property
+    def _valid_time(self) -> list[float]:
+        return [t for t, f in zip(self.time, self.fun, strict=True) if f is not None]
+
+    # Batches
+    # ----------------------------------------------------------------------------------
+
+    @property
     def batches(self) -> list[int]:
         return self._batches
+
+    # Tasks
+    # ----------------------------------------------------------------------------------
 
     @property
     def task(self) -> list[EvalTask]:
         return self._task
-
-    def _get_next_batch_id(self) -> int:
-        if not self._batches:
-            batch = 0
-        else:
-            batch = self._batches[-1] + 1
-        return batch
 
     # ==================================================================================
     # Add deprecated dict access
@@ -115,3 +184,19 @@ class History:
         msg = "dict-like access to History is deprecated. Use attribute access instead."
         warnings.warn(msg, FutureWarning)
         return getattr(self, key)
+
+
+def _flatten_params(valid_params: list[PyTree]) -> list[list[float]]:
+    is_flat = (
+        len(valid_params) > 0
+        and isinstance(valid_params[0], np.ndarray)
+        and valid_params[0].ndim == 1
+    )
+
+    if is_flat:
+        flatten = lambda x: x.tolist()
+    else:
+        registry = get_registry(extended=True)
+        flatten = partial(tree_just_flatten, registry=registry)
+
+    return [flatten(p) for p in valid_params]
