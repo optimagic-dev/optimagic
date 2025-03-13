d73a1e904acd9b622bfca0a634f533316ae89e12
gauravmanmode <gauravmanmode@gmail.com>
2025-03-13 13:41:58 +0530

    Update test_history_plots.py
    remove unused variable res

--- tests/optimagic/visualization/test_history_plots.py
+++ tests/optimagic/visualization/test_history_plots.py
@@ -183,12 +183,8 @@ def test_harmonize_inputs_to_dict_with_log_file_name():
         "test_log.db",
         if_database_exists=om.ExistenceStrategy.REPLACE,
     )
-    res = minimize(
-        fun=lambda x: x @ x,
-        params=np.arange(5),
-        algorithm="scipy_lbfgsb",
-        logging=log_options,
-    )
+    minimize(fun=lambda x: x @ x,params=np.arange(5),
+        algorithm="scipy_lbfgsb",logging=log_options,)
     assert _harmonize_inputs_to_dict(results="test_log.db", names=None) == {
         "0": "test_log.db"
     }
