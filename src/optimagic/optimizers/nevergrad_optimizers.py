389b1cc708cf3aabe7315336fce01c242832b465
gaurav <gauravmanmode@gmail.com>
2025-05-04 14:10:26 +0530

    remove executor

--- src/optimagic/optimizers/nevergrad_optimizers.py
+++ src/optimagic/optimizers/nevergrad_optimizers.py
@@ -1,8 +1,7 @@
 """Implement nevergrad optimizers."""
 
-from concurrent.futures import Future
 from dataclasses import dataclass
-from typing import Any, List
+from typing import Any
 
 import numpy as np
 from numpy.typing import NDArray
@@ -44,6 +43,7 @@ if IS_NEVERGRAD_INSTALLED:
 @dataclass(frozen=True)
 class NevergradCMAES(Algorithm):
     scale: NonNegativeFloat = 1.0
+    seed: int | None = None
     population_size: int | None = None
     elitist: bool = False
     diagonal: bool = False
@@ -67,7 +67,7 @@ class NevergradCMAES(Algorithm):
             "CMA_rankone": self.learning_rate_rank_one_update,
         }
 
-        optimizer = ng.optimizers.ParametrizedCMA(
+        raw_optimizer = ng.optimizers.ParametrizedCMA(
             scale=self.scale,
             popsize=self.population_size,
             elitist=self.elitist,
@@ -78,12 +78,13 @@ class NevergradCMAES(Algorithm):
             inopts=cma_options,
         )
 
-        res = nevergrad_internal(
+        res = _nevergrad_internal(
             problem=problem,
             x0=x0,
-            optimizer=optimizer,
+            raw_optimizer=raw_optimizer,
             stopping_maxfun=self.stopping_maxfun,
             n_cores=self.n_cores,
+            seed=self.seed,
         )
 
         return res
@@ -92,12 +93,13 @@ class NevergradCMAES(Algorithm):
 """ helper function for nevergrad"""
 
 
-def nevergrad_internal(
+def _nevergrad_internal(
     problem: InternalOptimizationProblem,
     x0: NDArray[np.float64],
     n_cores: int,
-    optimizer: Callable[..., Any],
+    raw_optimizer: Callable[..., Any],
     stopping_maxfun: int,
+    seed: int | None,
 ) -> InternalOptimizeResult:
     if not IS_NEVERGRAD_INSTALLED:
         raise NotInstalledError("Nevergrad is not installed.")
@@ -105,77 +107,35 @@ def nevergrad_internal(
     param = ng.p.Array(
         init=np.clip(x0, problem.bounds.lower, problem.bounds.upper)
     ).set_bounds(problem.bounds.lower, upper=problem.bounds.upper)
-    instrum = ng.p.Instrumentation(param)
 
-    parametrized_optimizer = optimizer(
+    instrum = ng.p.Instrumentation(param)
+    instrum.random_state.seed(12)
+    optimizer = raw_optimizer(
         parametrization=instrum, budget=stopping_maxfun, num_workers=n_cores
     )
 
-    executor = NevergradExecutor(
-        batch_fun=problem.batch_fun, n_cores=n_cores, budget=stopping_maxfun
-    )
-    recommendation = parametrized_optimizer.minimize(
-        problem.fun,
-        executor=executor,
-    )
+    while optimizer.num_ask < stopping_maxfun:
+        x_list = [
+            optimizer.ask()
+            for _ in range(min(n_cores, stopping_maxfun - optimizer.num_ask))
+        ]
+        losses = problem.batch_fun([x.value[0][0] for x in x_list], n_cores=n_cores)
+        for x, loss in zip(x_list, losses, strict=True):
+            optimizer.tell(x, loss)
+
+    recommendation = optimizer.provide_recommendation()
     best_x = recommendation.value[0][0]
+    loss = recommendation.loss
+    if loss is None:
+        loss = problem.fun(best_x)
 
     result = InternalOptimizeResult(
         x=best_x,
-        fun=problem.fun(best_x),
+        fun=loss,
         success=True,
-        n_fun_evals=parametrized_optimizer.num_ask,
+        n_fun_evals=optimizer.num_ask,
         n_jac_evals=0,
+        n_hess_evals=0,
     )
 
     return result
-
-
-""" Custom Executor class that uses batch_fun inside """
-
-
-class NevergradExecutor:
-    def __init__(
-        self,
-        batch_fun: Callable[..., list[float | NDArray[np.float64]]],
-        n_cores: int,
-        budget: int,
-    ):
-        self._batch_fun: Callable[..., list[float | NDArray[np.float64]]] = batch_fun
-        self._n_cores: int = n_cores
-        self._budget: int = budget
-        self._batch_inputs: List[NDArray[np.float64]] = []
-        self._futures: List[Future[Any]] = []
-        self._submitted_count: int = 0
-
-    def submit(
-        self, func: Callable[..., Any], *args: Any, **kwargs: Any
-    ) -> Future[Any]:
-        future: Future[Any] = Future()
-        x = args[0]
-        self._batch_inputs.append(x)
-        self._futures.append(future)
-        self._submitted_count += 1
-
-        if len(self._batch_inputs) == self._n_cores:
-            self._run_batch()
-
-        if self._submitted_count == self._budget:
-            self._flush()
-
-        return future
-
-    def _run_batch(self) -> None:
-        results = self._batch_fun(
-            x_list=self._batch_inputs, n_cores=len(self._batch_inputs)
-        )
-
-        for future, result in zip(self._futures, results, strict=False):
-            future.set_result(result)
-
-        self._batch_inputs.clear()
-        self._futures.clear()
-
-    def _flush(self) -> None:
-        if self._batch_inputs:
-            self._run_batch()
