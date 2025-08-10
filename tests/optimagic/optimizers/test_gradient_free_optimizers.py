c2958fd1ecc3f409bf02ebfeaaa1e9a1ca729621
gaurav <gauravmanmode@gmail.com>
2025-08-10 17:16:09 +0530

    pytree tests for gfo

--- tests/optimagic/optimizers/test_gradient_free_optimizers.py
+++ tests/optimagic/optimizers/test_gradient_free_optimizers.py
@@ -1,17 +1,20 @@
 import numpy as np
+import pytest
 
+from optimagic.config import IS_GRADIENT_FREE_OPTIMIZERS_INSTALLED
 from optimagic.optimization.internal_optimization_problem import (
-    SphereExampleInternalOptimizationProblem,
+    SphereExampleInternalOptimizationProblemWithConverter,
 )
 from optimagic.optimizers.gradient_free_optimizers import (
     _get_gfo_constraints,
     _get_initialize_gfo,
     _get_search_space_gfo,
+    _gfo_internal,
     _value2para,
 )
 from optimagic.parameters.bounds import Bounds
 
-problem = SphereExampleInternalOptimizationProblem()
+problem = SphereExampleInternalOptimizationProblemWithConverter()
 
 
 def test_get_gfo_constraints():
@@ -22,7 +25,9 @@ def test_get_gfo_constraints():
 
 def test_get_initialize_gfo():
     x0 = np.array([1, 0, 1])
-    x1 = [np.array([1, 2, 3])]
+    x1 = [
+        {"x0": 1, "x1": 2, "x2": 3},
+    ]
     n_init = 20
     got = _get_initialize_gfo(x0, n_init, x1, problem.converter)
     expected = {
@@ -35,24 +40,12 @@ def test_get_initialize_gfo():
     assert got == expected
 
 
-# unable to test with pytrees as SphereExampleInternalOptimizationProblem does
-# not convert pytrees
 def test_get_search_space_gfo():
-    bounds = Bounds(
-        lower=np.array(
-            [
-                -10,
-                -10,
-            ]
-        ),
-        upper=np.array(
-            [
-                10,
-                10,
-            ]
-        ),
-    )
-    n_grid_points = 4
+    bounds = Bounds(lower=np.array([-10, -10]), upper=np.array([10, 10]))
+    n_grid_points = {
+        "x0": 4,
+        "x1": 4,
+    }
     got = _get_search_space_gfo(bounds, n_grid_points, problem.converter)
     expected = {
         "x0": np.array([-10.0, -5.0, 0.0, 5.0]),
@@ -65,3 +58,30 @@ def test_get_search_space_gfo():
 
 def test_value2para():
     assert _value2para(np.array([0, 1, 2])) == {"x0": 0, "x1": 1, "x2": 2}
+
+
+@pytest.mark.skipif(
+    not IS_GRADIENT_FREE_OPTIMIZERS_INSTALLED, reason="gfo not installed"
+)
+def test_gfo_internal():
+    from gradient_free_optimizers import DownhillSimplexOptimizer
+
+    res = _gfo_internal(
+        problem=problem,
+        x0=np.full(10, 2),
+        optimizer=DownhillSimplexOptimizer,
+        warm_start=None,
+        n_init=5,
+        n_grid_points=20,
+        stopping_maxiter=1000,
+        stopping_maxtime=None,
+        stopping_funval=None,
+        convergence_iter_noimprove=100,
+        convergence_ftol_abs=None,
+        convergence_ftol_rel=None,
+        caching=False,
+        verbosity=False,
+        seed=12345,
+    )
+
+    assert np.all(res.x == np.full(10, 0))
